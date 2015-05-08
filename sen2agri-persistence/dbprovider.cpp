f22f632c8b4c4f46d157daac82594275ec27cc09
Laurentiu Nicola <lnicola@c-s.ro>
2015-05-08 15:50:20 +0300

    Add infrastructure for handling database deadlock

--- sen2agri-persistence/dbprovider.cpp
+++ sen2agri-persistence/dbprovider.cpp
@@ -12,26 +12,67 @@ ConfigurationParameterList DBProvider::GetConfigurationParameters(const QString 
 {
     QSqlDatabaseRAII db = getDatabase();
 
-    auto query = db.prepareQuery("select 'param1' as key, 'value1' as value");
-    query.setForwardOnly(true);
-    if (!query.exec()) {
-        throw_query_error(query);
-    }
+    return handleTransactionRetry(QStringLiteral("GetConfigurationParameters"), [&]() {
+        auto query = db.prepareQuery("select 'param1' as key, 'value1' as value");
+        query.setForwardOnly(true);
+        if (!query.exec()) {
+            throw_query_error(query);
+        }
 
-    ConfigurationParameterList result;
+        ConfigurationParameterList result;
 
-    auto dataRecord = query.record();
-    auto keyCol = dataRecord.indexOf(QStringLiteral("key"));
-    auto valueCol = dataRecord.indexOf(QStringLiteral("value"));
+        auto dataRecord = query.record();
+        auto keyCol = dataRecord.indexOf(QStringLiteral("key"));
+        auto valueCol = dataRecord.indexOf(QStringLiteral("value"));
 
-    while (query.next()) {
-        result.append({ query.value(keyCol).toString(), query.value(valueCol).toString() });
-    }
+        while (query.next()) {
+            result.append({ query.value(keyCol).toString(), query.value(valueCol).toString() });
+        }
 
-    return result;
+        return result;
+    });
 }
 
 QSqlDatabaseRAII DBProvider::getDatabase()
 {
     return QSqlDatabaseRAII();
+}
+
+bool DBProvider::shouldRetryTransaction(const sql_error &e)
+{
+    return e.error_code() == "40001" || e.error_code() == "40P01";
+}
+
+QString DBProvider::getErrorName(const sql_error &e)
+{
+    if (e.error_code() == "40001") {
+        return QStringLiteral("serialization failure");
+    } else if (e.error_code() == "40P01") {
+        return QStringLiteral("deadlock");
+    } else {
+        return QStringLiteral("unknown");
+    }
+}
+
+void DBProvider::warnRecoverableErrorAbort(const sql_error &e, const QString &operation)
+{
+    Q_UNUSED(QStringLiteral("A recoverable error of type %1 has been detected for "
+                            "operation %2, but the retry limit has been reached, aborting.")
+                 .arg(getErrorName(e))
+                 .arg(operation));
+}
+
+void DBProvider::warnRecoverableError(const sql_error &e,
+                                      const QString &operation,
+                                      int retryDelay,
+                                      int retryNumber,
+                                      int maxRetries)
+{
+    Q_UNUSED(QStringLiteral("A recoverable error of type %1 has been detected for "
+                            "operation %2. Retrying in %3 ms (%4/%5).")
+                 .arg(getErrorName(e))
+                 .arg(operation)
+                 .arg(retryDelay)
+                 .arg(retryNumber)
+                 .arg(maxRetries));
 }
--- sen2agri-persistence/dbprovider.hpp
+++ sen2agri-persistence/dbprovider.hpp
@@ -7,6 +7,7 @@
 
 #include "qsqldatabaseraii.hpp"
 #include "configurationparameter.hpp"
+#include "sql_error.hpp"
 
 class DBProvider
 {
@@ -18,4 +19,47 @@ public:
     DBProvider();
 
     ConfigurationParameterList GetConfigurationParameters(const QString &prefix);
+
+    bool shouldRetryTransaction(const sql_error &e);
+    QString getErrorName(const sql_error &e);
+    void warnRecoverableErrorAbort(const sql_error &e, const QString &operation);
+    void warnRecoverableError(const sql_error &e,
+                              const QString &operation,
+                              int retryDelay,
+                              int retryNumber,
+                              int maxNetries);
+
+    template <typename F>
+    auto handleTransactionRetry(const QString &operation, F &&f) -> decltype(f())
+    {
+        static const int maxTxnRetries = 10;
+        static const int initialRetryDelay = 1000;
+        static const int maxRetryDelay = 8000;
+
+        int retryDelay = 0;
+        int txnRetryNumber = 0;
+        while (true) {
+            try {
+                return f();
+            } catch (const sql_error &e) {
+                if (!shouldRetryTransaction(e)) {
+                    throw;
+                } else if (++txnRetryNumber > maxTxnRetries) {
+                    warnRecoverableErrorAbort(e, operation);
+                    throw;
+                } else {
+                    if (!retryDelay) {
+                        retryDelay = initialRetryDelay;
+                    } else if (retryDelay * 2 <= maxRetryDelay) {
+                        retryDelay *= 2;
+                    }
+
+                    warnRecoverableError(e, operation, retryDelay, txnRetryNumber,
+                                         maxTxnRetries);
+
+                    QThread::msleep(retryDelay);
+                }
+            }
+        }
+    }
 };
