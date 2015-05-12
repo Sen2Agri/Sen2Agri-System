a012ae920ac20221c896c0b8d12687f346cc0170
Florian Popescu <florian.popescu@c-s.ro>
2015-05-12 12:57:41 +0300

    Adding database objects

--- database/schema/02.config.sql
+++ database/schema/02.config.sql
@@ -2,7 +2,7 @@ CREATE TABLE config
 (
   key character varying NOT NULL,
   value character varying,
-  type t_data_types,
-  last_updated timestamp with time zone,
+  type t_data_types NOT NULL,
+  last_updated timestamp with time zone NOT NULL default now(),
   CONSTRAINT config_pkey PRIMARY KEY (key)
 )
