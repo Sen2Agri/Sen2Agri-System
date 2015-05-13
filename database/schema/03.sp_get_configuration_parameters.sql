e5e7fff1cae3f666dc624ec94c77996b7d6dc54b
Laurentiu Nicola <grayshade@gmail.com>
2015-05-13 12:45:54 +0300

    Fix _prefix concatenation

--- database/schema/03.sp_get_configuration_parameters.sql
+++ database/schema/03.sp_get_configuration_parameters.sql
@@ -3,7 +3,7 @@ IN _prefix CHARACTER VARYING DEFAULT NULL) RETURNS
 TABLE (key CHARACTER VARYING, value CHARACTER VARYING, type t_data_types) AS $$
 BEGIN
 
-RETURN QUERY SELECT config.key, config.value, config.type FROM config WHERE CASE WHEN _prefix IS NOT NULL THEN config.key like 'prefix%' ELSE 1 = 1 END;
+RETURN QUERY SELECT config.key, config.value, config.type FROM config WHERE CASE WHEN _prefix IS NOT NULL THEN config.key like _prefix || '%' ELSE 1 = 1 END;
 
 END;
 $$ LANGUAGE plpgsql;
