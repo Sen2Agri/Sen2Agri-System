5f41f12c1ec51081ff71c06e40f58b9c1f8ec843
Florian Popescu <florian.popescu@c-s.ro>
2015-05-15 18:16:35 +0300

    Adding DB objects to SC

---
+++ database/schema/08.sp_get_categories.sql
@@ -0,0 +1,8 @@
+﻿CREATE OR REPLACE FUNCTION sp_get_categories() RETURNS 
+TABLE (id INT, name CHARACTER VARYING) AS $$
+BEGIN
+
+RETURN QUERY SELECT config_category.id, config_category.name FROM config_category ORDER BY config_category.display_order;
+
+END;
+$$ LANGUAGE plpgsql;
