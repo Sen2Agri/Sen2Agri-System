76b41d05c298a321d44bfc1ad7a6ce9cb11df143
Laurentiu Nicola <grayshade@gmail.com>
2015-06-11 16:15:33 +0300

    Add some error checks

--- sen2agri-persistence/persistencemanager.cpp
+++ sen2agri-persistence/persistencemanager.cpp
@@ -1,63 +1,98 @@
 #include <functional>
 
+#include <sys/types.h>
+#include <grp.h>
+#include <pwd.h>
+#include <limits>
+#include <unistd.h>
+
 #include <QtSql>
 #include <QDebug>
 #include <QDBusConnectionInterface>
 #include <QDBusMessage>
 #include <QThreadPool>
 
-#include <sys/types.h>
-#include <grp.h>
-#include <pwd.h>
-#include <unistd.h>
+#include <optional.hpp>
 
 #include "persistencemanager.hpp"
 #include "make_unique.hpp"
 
-// TODO error logging
-static gid_t getGroupId(const char *name)
+static QString getSystemErrorMessage(int err)
 {
-    group grp;
-    group *result;
-
-    auto buflen = sysconf(_SC_GETGR_R_SIZE_MAX);
-    if (buflen == -1) {
-        buflen = 16384;
-    }
-
-    auto buf(std::make_unique<char[]>(buflen));
-
-    if (!getgrnam_r(name, &grp, buf.get(), buflen, &result)) {
-        return grp.gr_gid;
-    }
-
-    return 0;
+    char buf[1024];
+    return strerror_r(err, buf, sizeof(buf));
 }
 
-// TODO error logging
-static bool isUserAdmin(uid_t uid)
+static long getSysConf(int name, long fallback)
 {
-    passwd pwd;
-    passwd *result;
+    auto value = sysconf(name);
 
-    auto buflen = sysconf(_SC_GETPW_R_SIZE_MAX);
-    if (buflen == -1) {
-        buflen = 16384;
+    if (value <= 0) {
+        value = fallback;
     }
 
+    return value;
+}
+
+static bool getPasswordEntry(uid_t uid, passwd &pwd)
+{
+    passwd *result;
+
+    auto buflen = getSysConf(_SC_GETPW_R_SIZE_MAX, 16384);
     auto buf(std::make_unique<char[]>(buflen));
 
-    if (!getpwuid_r(uid, &pwd, buf.get(), buflen, &result)) {
-        int ngroups = sysconf(_SC_NGROUPS_MAX);
-        auto groups(std::make_unique<gid_t>(buflen));
+    if (auto r = getpwuid_r(uid, &pwd, buf.get(), buflen, &result)) {
+        throw std::runtime_error(QStringLiteral("Unable to get user information: %1")
+                                     .arg(getSystemErrorMessage(r))
+                                     .toStdString());
+    } else {
+        return result != nullptr;
+    }
+}
 
-        auto adminGroupId = getGroupId("sen2agri-admin");
+static bool getGroupEntry(const char *name, group grp)
+{
+    group *result;
 
-        if (getgrouplist(pwd.pw_name, pwd.pw_gid, groups.get(), &ngroups) > 0) {
-            for (int i = 0; i < ngroups; i++) {
-                if (groups.get()[i] == adminGroupId) {
-                    return true;
-                }
+    auto buflen = getSysConf(_SC_GETGR_R_SIZE_MAX, 16384);
+    auto buf(std::make_unique<char[]>(buflen));
+
+    if (auto r = getgrnam_r(name, &grp, buf.get(), buflen, &result)) {
+        throw std::runtime_error(QStringLiteral("Unable to get group information: %1")
+                                     .arg(getSystemErrorMessage(r))
+                                     .toStdString());
+    } else {
+        return result != nullptr;
+    }
+}
+
+static bool isUserAdmin(uid_t uid)
+{
+    // TODO what to do with this?
+    static const char adminGroupName[] = "sen2agri-admin";
+
+    // don't check the group membership if the caller is root
+    if (!uid) {
+        return true;
+    }
+
+    passwd pwd;
+    if (!getPasswordEntry(uid, pwd)) {
+        return false;
+    }
+
+    group grp;
+    if (!getGroupEntry(adminGroupName, grp)) {
+        return false;
+    }
+
+    int ngroups = getSysConf(_SC_NGROUPS_MAX, NGROUPS_MAX);
+    auto groups(std::make_unique<gid_t[]>(ngroups));
+
+    if (getgrouplist(pwd.pw_name, pwd.pw_gid, groups.get(), &ngroups) > 0) {
+        for (int i = 0; i < ngroups; i++) {
+            if (groups[i] == grp.gr_gid) {
+                return true;
             }
         }
     }
@@ -74,12 +109,18 @@ bool PersistenceManager::IsCallerAdmin()
 {
     const auto &reply = connection().interface()->serviceUid(message().service());
     if (!reply.isValid()) {
-        Logger::error(reply.error().message());
+        Logger::error(QStringLiteral("Unable to determine caller credentials: %1")
+                          .arg(reply.error().message()));
 
         return false;
     }
 
-    return isUserAdmin(reply.value());
+    try {
+        return isUserAdmin(reply.value());
+    } catch (const std::runtime_error &e) {
+        Logger::error(e.what());
+        return false;
+    }
 }
 
 ConfigurationSet PersistenceManager::GetConfigurationSet()
