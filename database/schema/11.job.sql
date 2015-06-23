4bede46982952906859feba2575f8818c9e950d4
Florian Popescu <florian.popescu@c-s.ro>
2015-06-23 11:58:33 +0300

    Adding DB objects to git

--- database/schema/11.job.sql
+++ database/schema/11.job.sql
@@ -4,7 +4,7 @@
   processor_id smallint NOT NULL,
   site_id smallint NOT NULL,
   start_type_id smallint NOT NULL,
-  parameters varchar,
+  parameters json,
   submit_timestamp timestamp with time zone NOT NULL DEFAULT now(),
   start_timestamp timestamp with time zone,
   end_timestamp timestamp with time zone,
--- database/schema/13.step.sql
+++ database/schema/13.step.sql
@@ -2,7 +2,7 @@
 (
   name character varying NOT NULL,
   task_id int NOT NULL,
-  parameters character varying,
+  parameters json,
   submit_timestamp timestamp with time zone NOT NULL DEFAULT now(),
   start_timestamp timestamp with time zone,
   end_timestamp timestamp with time zone,
