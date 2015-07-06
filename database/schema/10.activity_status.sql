b047248b77293baf832c4cc376967336ede778af
Florian Popescu <florian.popescu@c-s.ro>
2015-07-06 16:08:53 +0300

    Adding DB objects to git

--- database/schema/10.activity_status.sql
+++ database/schema/10.activity_status.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE activity_status
 (
-  id smallserial NOT NULL,
+  id smallint NOT NULL,
   name character varying NOT NULL DEFAULT ''::character varying,
   description character varying,
   CONSTRAINT activity_status_pkey PRIMARY KEY (id)
