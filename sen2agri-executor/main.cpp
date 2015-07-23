ef3784d30a6e95d17dec06c0e3f574f65ed75cc0
Laurentiu Nicola <lnicola@dend.ro>
2015-07-23 16:43:02 +0300

    Handle exceptions

--- sen2agri-executor/main.cpp
+++ sen2agri-executor/main.cpp
@@ -2,6 +2,7 @@
 #include <QCoreApplication>
 #include <QDBusError>
 
+#include "logger.hpp"
 #include "orchestratorrequestshandler.h"
 #include "ressourcemanageritf.h"
 #include "configurationmgr.h"
@@ -13,57 +14,64 @@
 
 int main(int argc, char *argv[])
 {
-    QString str;
-    QCoreApplication a(argc, argv);
+    try {
+        QString str;
+        QCoreApplication a(argc, argv);
 
-    qDebug() << "Current execution path: " << QDir::currentPath();
+        qDebug() << "Current execution path: " << QDir::currentPath();
 
-    registerMetaTypes();
+        registerMetaTypes();
 
-    // Create the QEventLoop
-    QEventLoop pause;
-    // connect the QHttp.requestFinished() Signal to the QEventLoop.quit() Slot
-    QObject::connect(PersistenceItfModule::GetInstance(), SIGNAL(OnConfigurationReceived()), &pause, SLOT(quit()));
-    // The code that will run during the QEventLoop
-    // get the configuration from the persistence manager
-    PersistenceItfModule::GetInstance()->RequestConfiguration();
-    // Execute the QEventLoop - it will quit when the above finished due to the connect()
-    pause.exec();
+        // Create the QEventLoop
+        QEventLoop pause;
+        // connect the QHttp.requestFinished() Signal to the QEventLoop.quit() Slot
+        QObject::connect(PersistenceItfModule::GetInstance(), SIGNAL(OnConfigurationReceived()),
+                         &pause, SLOT(quit()));
+        // The code that will run during the QEventLoop
+        // get the configuration from the persistence manager
+        PersistenceItfModule::GetInstance()->RequestConfiguration();
+        // Execute the QEventLoop - it will quit when the above finished due to the connect()
+        pause.exec();
 
-    // Initialize the server for receiving the messages from the executing Processor Wrappers
-    QString strIpVal;
-    QString strPortVal;
-    AbstractExecInfosProtSrv *pExecInfosSrv =
-            ExecInfosProtSrvFactory::GetInstance()->CreateExecInfosProtSrv(ExecInfosProtSrvFactory::SIMPLE_UDP);
-    str = QString("SRV_IP_ADDR");
-    ConfigurationMgr::GetInstance()->GetValue(str, strIpVal);
-    str = QString("SRV_PORT_NO");
-    ConfigurationMgr::GetInstance()->GetValue(str, strPortVal);
-    pExecInfosSrv->SetProcMsgListener(RessourceManagerItf::GetInstance());
-    pExecInfosSrv->StartCommunication(strIpVal, strPortVal.toInt());
+        // Initialize the server for receiving the messages from the executing Processor Wrappers
+        QString strIpVal;
+        QString strPortVal;
+        AbstractExecInfosProtSrv *pExecInfosSrv =
+            ExecInfosProtSrvFactory::GetInstance()->CreateExecInfosProtSrv(
+                ExecInfosProtSrvFactory::SIMPLE_UDP);
+        str = QString("SRV_IP_ADDR");
+        ConfigurationMgr::GetInstance()->GetValue(str, strIpVal);
+        str = QString("SRV_PORT_NO");
+        ConfigurationMgr::GetInstance()->GetValue(str, strPortVal);
+        pExecInfosSrv->SetProcMsgListener(RessourceManagerItf::GetInstance());
+        pExecInfosSrv->StartCommunication(strIpVal, strPortVal.toInt());
 
-    // start the ressource manager
-    RessourceManagerItf::GetInstance()->Start();
+        // start the ressource manager
+        RessourceManagerItf::GetInstance()->Start();
 
-    // Initialize the DBus for Orchestrator requests handling
-    OrchestratorRequestsHandler orchestratorReqHandler;
-    new ProcessorsExecutorAdaptor(&orchestratorReqHandler);
+        // Initialize the DBus for Orchestrator requests handling
+        OrchestratorRequestsHandler orchestratorReqHandler;
+        new ProcessorsExecutorAdaptor(&orchestratorReqHandler);
 
-    QDBusConnection connection = QDBusConnection::systemBus();
+        QDBusConnection connection = QDBusConnection::systemBus();
 
-    if (!connection.registerObject("/org/esa/sen2agri/processorsExecutor", &orchestratorReqHandler)) {
-        QString str = QString("Error registering the object with D-Bus: %1, exiting.")
-                .arg(connection.lastError().message());
+        if (!connection.registerObject("/org/esa/sen2agri/processorsExecutor",
+                                       &orchestratorReqHandler)) {
+            QString str = QString("Error registering the object with D-Bus: %1, exiting.")
+                              .arg(connection.lastError().message());
 
-        throw std::runtime_error(str.toStdString());
+            throw std::runtime_error(str.toStdString());
+        }
+
+        if (!connection.registerService(SERVICE_NAME)) {
+            QString str = QString("Error registering the object with D-Bus: %1, exiting.")
+                              .arg(connection.lastError().message());
+
+            throw std::runtime_error(str.toStdString());
+        }
+
+        return a.exec();
+    } catch (const std::exception &e) {
+        Logger::fatal(e.what());
     }
-
-    if (!connection.registerService(SERVICE_NAME)) {
-        QString str = QString("Error registering the object with D-Bus: %1, exiting.")
-                .arg(connection.lastError().message());
-
-        throw std::runtime_error(str.toStdString());
-    }
-
-    return a.exec();
 }
