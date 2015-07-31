4893acc9e4d8df200cc3fefe6930e652526d6112
Cosmin Udroiu <cosmin@c-s.ro>
2015-07-31 21:50:59 +0300

    Update

--- sen2agri-processors/sen2agri-composite/UpdateSynthesis/UpdateSynthesis/updatesynthesisfunctor.h
+++ sen2agri-processors/sen2agri-composite/UpdateSynthesis/UpdateSynthesis/updatesynthesisfunctor.h
@@ -74,166 +74,35 @@ typedef enum {LAND=1, WATER,SNOW,CLOUD,CLOUD_SHADOW} FlagType;
 template< class TInput, class TOutput>
 class UpdateSynthesisFunctor
 {
-public:
-    UpdateSynthesisFunctor()
-    {
-        m_fQuantificationValue = -1;
-        m_bPrevL3ABandsAvailable = false;
-        m_nL2ABandStartIndex = 0;
-        m_nCloudMaskBandIndex = -1;
-        m_nSnowMaskBandIndex = -1;
-        m_nWaterMaskBandIndex = -1;
-        m_nCurrentL2AWeightBandIndex = -1;
-        m_nPrevL3AWeightBandStartIndex = -1;
-        m_nPrevL3AWeightedAvDateBandIndex = -1;
-        m_nPrevL3AReflectanceBandStartIndex = -1;
-        m_nPrevL3APixelFlagBandIndex = -1;
-        m_nRedBandIndex = -1;
-        m_nNbOfL3AReflectanceBands = 0;
-    }
-
-    bool operator!=( const UpdateSynthesisFunctor & ) const
-    {
-        return false;
-    }
-    bool operator==( const UpdateSynthesisFunctor & other ) const
-    {
-        return !(*this != other);
-    }
-
-    TOutput operator()( const TInput & A ) const
-    {
-        ResetCurrentPixelValues();
-        if(IsLandPixel(A))
-        {
-            HandleLandPixel(A);
-        } else {
-            if(IsSnowPixel(A) || IsWaterPixel(A))
-            {
-                // if pixel is snow or water *replace the reflectance value
-                HandleSnowOrWaterPixel(A);
-            } else {
-                // if pixel is cloud or shadow *pixel never observed cloud snow or water free
-                HandleCloudOrShadowPixel(A);
-            }
-        }
-
-        int nTotalOutBandsNo = 2*m_nNbOfL3AReflectanceBands + 2;
-        TOutput var(nTotalOutBandsNo);
-        var.SetSize(nTotalOutBandsNo);
-
-        int i;
-        int cnt = 0;
-
-        // Weighted Average Reflectances
-        for(i = 0; i < m_nNbOfL3AReflectanceBands; i++)
-        {
-            var[cnt++] = m_CurrentPixelWeights[i];
-        }
-        // Weighted Average Date L3A
-        var[cnt++] = m_fCurrentPixelWeightedDate;
-        // Weight for B2 for L3A
-        for(i = 0; i < m_nNbOfL3AReflectanceBands; i++)
-        {
-            var[cnt++] = m_CurrentWeightedReflectances[i];
-        }
-        // Pixel status
-        var[cnt++] = m_fCurrentPixelFlag;
-
-        return var;
-    }
-
-    void Initialize(SensorType sensorType, ResolutionType resolution, bool bPrevL3ABandsAvailable)
-    {
-        m_sensorType = sensorType;
-        m_resolution = resolution;
-        m_bPrevL3ABandsAvailable = bPrevL3ABandsAvailable;
-        if(resolution == RES_10M) {
-            m_nNbOfL3AReflectanceBands = WEIGHTED_REFLECTANCE_10M_BANDS_NO;
-            if(sensorType == SENSOR_S2)
-            {
-                m_nNbL2ABands = S2_L2A_10M_BANDS_NO;
-                m_nL2ABandStartIndex = S2_L2A_10M_BANDS_START_IDX;
-                m_nCloudMaskBandIndex = S2_L2A_10M_CLD_MASK_IDX;
-                m_nSnowMaskBandIndex = S2_L2A_10M_SNOW_MASK_IDX;
-                m_nWaterMaskBandIndex = S2_L2A_10M_WATER_MASK_IDX;
-                m_nCurrentL2AWeightBandIndex = S2_L2A_10M_TOTAL_WEIGHT_IDX;
-                m_nPrevL3AWeightBandStartIndex = S2_L2A_10M_L3A_WEIGHT_START_IDX;
-                m_nPrevL3AWeightedAvDateBandIndex = S2_L2A_10M_L3A_W_AV_DATE_IDX;
-                m_nPrevL3AReflectanceBandStartIndex = S2_L2A_10M_L3A_REFL_START_IDX;
-                m_nPrevL3APixelFlagBandIndex = S2_L2A_10M_L3A_PIXEL_STATUS_IDX;
-                m_nRedBandIndex = S2_L2A_10M_RED_BAND_IDX;
-                m_nBlueBandIndex = S2_L2A_10M_BLUE_BAND_IDX;
-
-            } else if (sensorType == SENSOR_L8) {
-
-                m_nNbL2ABands = L8_L2A_10M_BANDS_NO;
-                m_nL2ABandStartIndex = L8_L2A_10M_BANDS_START_IDX;
-                m_nCloudMaskBandIndex = L8_L2A_10M_CLD_MASK_IDX;
-                m_nSnowMaskBandIndex = L8_L2A_10M_SNOW_MASK_IDX;
-                m_nWaterMaskBandIndex = L8_L2A_10M_WATER_MASK_IDX;
-                m_nCurrentL2AWeightBandIndex = L8_L2A_10M_TOTAL_WEIGHT_IDX;
-                m_nPrevL3AWeightBandStartIndex = L8_L2A_10M_L3A_WEIGHT_START_IDX;
-                m_nPrevL3AWeightedAvDateBandIndex = L8_L2A_10M_L3A_W_AV_DATE_IDX;
-                m_nPrevL3AReflectanceBandStartIndex = L8_L2A_10M_L3A_REFL_START_IDX;
-                m_nPrevL3APixelFlagBandIndex = L8_L2A_10M_L3A_PIXEL_STATUS_IDX;
-                m_nRedBandIndex = L8_L2A_10M_RED_BAND_IDX;
-                m_nBlueBandIndex = L8_L2A_10M_BLUE_BAND_IDX;
-
-            } else {
-                // TODO: Throw an error
-            }
-        } else if(resolution == RES_20M) {
-            m_nNbOfL3AReflectanceBands = WEIGHTED_REFLECTANCE_20M_BANDS_NO;
-            if(sensorType == SENSOR_S2)
-            {
-                m_nNbL2ABands = S2_L2A_20M_BANDS_NO;
-                m_nL2ABandStartIndex = S2_L2A_20M_BANDS_START_IDX;
-                m_nCloudMaskBandIndex = S2_L2A_20M_CLD_MASK_IDX;
-                m_nSnowMaskBandIndex = S2_L2A_20M_SNOW_MASK_IDX;
-                m_nWaterMaskBandIndex = S2_L2A_20M_WATER_MASK_IDX;
-                m_nCurrentL2AWeightBandIndex = S2_L2A_20M_TOTAL_WEIGHT_IDX;
-                m_nPrevL3AWeightBandStartIndex = S2_L2A_20M_L3A_WEIGHT_START_IDX;
-                m_nPrevL3AWeightedAvDateBandIndex = S2_L2A_20M_L3A_W_AV_DATE_IDX;
-                m_nPrevL3AReflectanceBandStartIndex = S2_L2A_20M_L3A_REFL_START_IDX;
-                m_nPrevL3APixelFlagBandIndex = S2_L2A_20M_L3A_PIXEL_STATUS_IDX;
-                m_nRedBandIndex = S2_L2A_20M_RED_BAND_IDX;
-                m_nBlueBandIndex = S2_L2A_20M_BLUE_BAND_IDX;
-
-            } else if (sensorType == SENSOR_L8) {
-
-                m_nNbL2ABands = L8_L2A_20M_BANDS_NO;
-                m_nL2ABandStartIndex = L8_L2A_20M_BANDS_START_IDX;
-                m_nCloudMaskBandIndex = L8_L2A_20M_CLD_MASK_IDX;
-                m_nSnowMaskBandIndex = L8_L2A_20M_SNOW_MASK_IDX;
-                m_nWaterMaskBandIndex = L8_L2A_20M_WATER_MASK_IDX;
-                m_nCurrentL2AWeightBandIndex = L8_L2A_20M_TOTAL_WEIGHT_IDX;
-                m_nPrevL3AWeightBandStartIndex = L8_L2A_20M_L3A_WEIGHT_START_IDX;
-                m_nPrevL3AWeightedAvDateBandIndex = L8_L2A_20M_L3A_W_AV_DATE_IDX;
-                m_nPrevL3AReflectanceBandStartIndex = L8_L2A_20M_L3A_REFL_START_IDX;
-                m_nPrevL3APixelFlagBandIndex = L8_L2A_20M_L3A_PIXEL_STATUS_IDX;
-                m_nRedBandIndex = L8_L2A_20M_RED_BAND_IDX;
-                m_nBlueBandIndex = L8_L2A_20M_BLUE_BAND_IDX;
-
-            } else {
-                // TODO: Throw an error
-            }
-        } else {
-            // TODO: Throw an error
-        }
-    }
-
-    void SetReflectanceQuantificationValue(float fQuantifVal)
-    {
-        m_fQuantificationValue = fQuantifVal;
-    }
-
-    void SetCurrentDate(int nDate)
-    {
-        m_nCurrentDate = nDate;
-    }
-
 private:
+    SensorType m_sensorType;
+    ResolutionType m_resolution;
+
+    float m_fQuantificationValue;
+    int m_nCurrentDate;
+
+    bool m_bPrevL3ABandsAvailable;
+    int m_nNbOfL3AReflectanceBands;
+    int m_nNbL2ABands;
+
+    int m_nL2ABandStartIndex;
+    int m_nCloudMaskBandIndex;
+    int m_nSnowMaskBandIndex;
+    int m_nWaterMaskBandIndex;
+    int m_nCurrentL2AWeightBandIndex;
+    int m_nPrevL3AWeightBandStartIndex;
+    int m_nPrevL3AWeightedAvDateBandIndex;
+    int m_nPrevL3AReflectanceBandStartIndex;
+    int m_nPrevL3APixelFlagBandIndex;
+    int m_nRedBandIndex;
+    int m_nBlueBandIndex;
+
+    // output pixel values
+    float m_CurrentWeightedReflectances[L3A_WEIGHTED_REFLECTANCES_MAX_NO];
+    float m_CurrentPixelWeights[L3A_WEIGHTED_REFLECTANCES_MAX_NO];
+    float m_fCurrentPixelFlag;
+    float m_fCurrentPixelWeightedDate;
+
     void ResetCurrentPixelValues()
     {
         for(int i = 0; i<m_nNbOfL3AReflectanceBands; i++)
@@ -274,7 +143,7 @@ private:
                 float fCurReflectance = GetL2AReflectanceForPixelVal(A[nCurrentBandIndex]);
                 float fPrevReflect = GetPrevL3AReflectanceValue(A, i);
                 float fPrevWeight = GetPrevL3AWeightValue(A, i);
-                float fCurrentWeight = GetCurrentWeight(A);
+                float fCurrentWeight = GetCurrentL2AWeightValue(A);
                 float fPrevWeightedDate = GetPrevL3AWeightedAvDateValue(A);
                 m_CurrentWeightedReflectances[i] = (fPrevWeight * fPrevReflect + fCurrentWeight * fCurReflectance) /
                         (fPrevWeight + fCurrentWeight);
@@ -406,11 +275,6 @@ private:
         }
     }
 
-    bool IsLandPixel(const TInput & A)
-    {
-        return (!IsSnowPixel(A) && !IsWaterPixel(A) && !IsCloudPixel(A) && !IsCloudShadowPixel(A));
-    }
-
     bool IsSnowPixel(const TInput & A)
     {
         if(m_nSnowMaskBandIndex == -1)
@@ -438,6 +302,11 @@ private:
         return (val != 0);
     }
 
+    bool IsLandPixel(const TInput & A)
+    {
+        return (!IsSnowPixel(A) && !IsWaterPixel(A) && !IsCloudPixel(A));
+    }
+
     bool IsRedBand(int index)
     {
         if((m_nRedBandIndex != -1) && (index == m_nRedBandIndex)) {
@@ -458,7 +327,7 @@ private:
 
     float GetPrevL3AWeightValue(const TInput & A, int offset)
     {
-        if(m_nPrevL3AWeightBandStartIndex == -1)
+        if(!m_bPrevL3ABandsAvailable || m_nPrevL3AWeightBandStartIndex == -1)
             return WEIGHT_NO_DATA;
 
         int val = (int)static_cast<float>(A[m_nPrevL3AWeightBandStartIndex+offset]);
@@ -467,7 +336,7 @@ private:
 
     float GetPrevL3AWeightedAvDateValue(const TInput & A)
     {
-        if(m_nPrevL3AWeightedAvDateBandIndex == -1)
+        if(!m_bPrevL3ABandsAvailable || m_nPrevL3AWeightedAvDateBandIndex == -1)
             return DATE_NO_DATA;
 
         int val = (int)static_cast<float>(A[m_nPrevL3AWeightedAvDateBandIndex]);
@@ -476,7 +345,7 @@ private:
 
     float GetPrevL3AReflectanceValue(const TInput & A, int offset)
     {
-        if(m_nPrevL3AReflectanceBandStartIndex == -1)
+        if(!m_bPrevL3ABandsAvailable || m_nPrevL3AReflectanceBandStartIndex == -1)
             return REFLECTANCE_NO_DATA;
 
         int val = (int)static_cast<float>(A[m_nPrevL3AReflectanceBandStartIndex + offset]);
@@ -485,7 +354,7 @@ private:
 
     float GetPrevL3APixelFlagValue(const TInput & A)
     {
-        if(m_nPrevL3APixelFlagBandIndex == -1)
+        if(!m_bPrevL3ABandsAvailable || m_nPrevL3APixelFlagBandIndex == -1)
             return FLAG_NO_DATA;
 
         int val = (int)static_cast<float>(A[m_nPrevL3APixelFlagBandIndex]);
@@ -497,34 +366,164 @@ private:
         return m_nBlueBandIndex;
     }
 
-private:
-    SensorType m_sensorType;
-    ResolutionType m_resolution;
+public:
+    UpdateSynthesisFunctor()
+    {
+        m_fQuantificationValue = -1;
+        m_bPrevL3ABandsAvailable = false;
+        m_nL2ABandStartIndex = 0;
+        m_nCloudMaskBandIndex = -1;
+        m_nSnowMaskBandIndex = -1;
+        m_nWaterMaskBandIndex = -1;
+        m_nCurrentL2AWeightBandIndex = -1;
+        m_nPrevL3AWeightBandStartIndex = -1;
+        m_nPrevL3AWeightedAvDateBandIndex = -1;
+        m_nPrevL3AReflectanceBandStartIndex = -1;
+        m_nPrevL3APixelFlagBandIndex = -1;
+        m_nRedBandIndex = -1;
+        m_nNbOfL3AReflectanceBands = 0;
+    }
 
-    float m_fQuantificationValue;
-    int m_nCurrentDate;
+    bool operator!=( const UpdateSynthesisFunctor & ) const
+    {
+        return false;
+    }
+    bool operator==( const UpdateSynthesisFunctor & other ) const
+    {
+        return !(*this != other);
+    }
 
-    bool m_bPrevL3ABandsAvailable;
-    int m_nNbOfL3AReflectanceBands;
-    int m_nNbL2ABands;
+    TOutput operator()( const TInput & A )
+    {
+        ResetCurrentPixelValues();
+        if(IsLandPixel(A))
+        {
+            HandleLandPixel(A);
+        } else {
+            if(IsSnowPixel(A) || IsWaterPixel(A))
+            {
+                // if pixel is snow or water *replace the reflectance value
+                HandleSnowOrWaterPixel(A);
+            } else {
+                // if pixel is cloud or shadow *pixel never observed cloud snow or water free
+                HandleCloudOrShadowPixel(A);
+            }
+        }
 
-    int m_nL2ABandStartIndex;
-    int m_nCloudMaskBandIndex;
-    int m_nSnowMaskBandIndex;
-    int m_nWaterMaskBandIndex;
-    int m_nCurrentL2AWeightBandIndex;
-    int m_nPrevL3AWeightBandStartIndex;
-    int m_nPrevL3AWeightedAvDateBandIndex;
-    int m_nPrevL3AReflectanceBandStartIndex;
-    int m_nPrevL3APixelFlagBandIndex;
-    int m_nRedBandIndex;
-    int m_nBlueBandIndex;
+        int nTotalOutBandsNo = 2*m_nNbOfL3AReflectanceBands + 2;
+        TOutput var(nTotalOutBandsNo);
+        var.SetSize(nTotalOutBandsNo);
 
-    // output pixel values
-    float m_CurrentWeightedReflectances[L3A_WEIGHTED_REFLECTANCES_MAX_NO];
-    float m_CurrentPixelWeights[L3A_WEIGHTED_REFLECTANCES_MAX_NO];
-    float m_fCurrentPixelFlag;
-    float m_fCurrentPixelWeightedDate;
+        int i;
+        int cnt = 0;
+
+        // Weighted Average Reflectances
+        for(i = 0; i < m_nNbOfL3AReflectanceBands; i++)
+        {
+            var[cnt++] = m_CurrentPixelWeights[i];
+        }
+        // Weighted Average Date L3A
+        var[cnt++] = m_fCurrentPixelWeightedDate;
+        // Weight for B2 for L3A
+        for(i = 0; i < m_nNbOfL3AReflectanceBands; i++)
+        {
+            var[cnt++] = m_CurrentWeightedReflectances[i];
+        }
+        // Pixel status
+        var[cnt++] = m_fCurrentPixelFlag;
+
+        return var;
+    }
+
+    void Initialize(SensorType sensorType, ResolutionType resolution, bool bPrevL3ABandsAvailable)
+    {
+        m_sensorType = sensorType;
+        m_resolution = resolution;
+        m_bPrevL3ABandsAvailable = bPrevL3ABandsAvailable;
+        if(resolution == RES_10M) {
+            m_nNbOfL3AReflectanceBands = WEIGHTED_REFLECTANCE_10M_BANDS_NO;
+            if(sensorType == SENSOR_S2)
+            {
+                m_nNbL2ABands = S2_L2A_10M_BANDS_NO;
+                m_nL2ABandStartIndex = S2_L2A_10M_BANDS_START_IDX;
+                m_nCloudMaskBandIndex = S2_L2A_10M_CLD_MASK_IDX;
+                m_nSnowMaskBandIndex = S2_L2A_10M_SNOW_MASK_IDX;
+                m_nWaterMaskBandIndex = S2_L2A_10M_WATER_MASK_IDX;
+                m_nCurrentL2AWeightBandIndex = S2_L2A_10M_TOTAL_WEIGHT_IDX;
+                m_nPrevL3AWeightBandStartIndex = S2_L2A_10M_L3A_WEIGHT_START_IDX;
+                m_nPrevL3AWeightedAvDateBandIndex = S2_L2A_10M_L3A_W_AV_DATE_IDX;
+                m_nPrevL3AReflectanceBandStartIndex = S2_L2A_10M_L3A_REFL_START_IDX;
+                m_nPrevL3APixelFlagBandIndex = S2_L2A_10M_L3A_PIXEL_STATUS_IDX;
+                m_nRedBandIndex = S2_L2A_10M_RED_BAND_IDX;
+                m_nBlueBandIndex = S2_L2A_10M_BLUE_BAND_IDX;
+
+            } else if (sensorType == SENSOR_L8) {
+
+                m_nNbL2ABands = L8_L2A_10M_BANDS_NO;
+                m_nL2ABandStartIndex = L8_L2A_10M_BANDS_START_IDX;
+                m_nCloudMaskBandIndex = L8_L2A_10M_CLD_MASK_IDX;
+                m_nSnowMaskBandIndex = L8_L2A_10M_SNOW_MASK_IDX;
+                m_nWaterMaskBandIndex = L8_L2A_10M_WATER_MASK_IDX;
+                m_nCurrentL2AWeightBandIndex = L8_L2A_10M_TOTAL_WEIGHT_IDX;
+                m_nPrevL3AWeightBandStartIndex = L8_L2A_10M_L3A_WEIGHT_START_IDX;
+                m_nPrevL3AWeightedAvDateBandIndex = L8_L2A_10M_L3A_W_AV_DATE_IDX;
+                m_nPrevL3AReflectanceBandStartIndex = L8_L2A_10M_L3A_REFL_START_IDX;
+                m_nPrevL3APixelFlagBandIndex = L8_L2A_10M_L3A_PIXEL_STATUS_IDX;
+                m_nRedBandIndex = L8_L2A_10M_RED_BAND_IDX;
+                m_nBlueBandIndex = L8_L2A_10M_BLUE_BAND_IDX;
+
+            } else {
+                // TODO: Throw an error
+            }
+        } else if(resolution == RES_20M) {
+            m_nNbOfL3AReflectanceBands = WEIGHTED_REFLECTANCE_20M_BANDS_NO;
+            if(sensorType == SENSOR_S2)
+            {
+                m_nNbL2ABands = S2_L2A_20M_BANDS_NO;
+                m_nL2ABandStartIndex = S2_L2A_20M_BANDS_START_IDX;
+                m_nCloudMaskBandIndex = S2_L2A_20M_CLD_MASK_IDX;
+                m_nSnowMaskBandIndex = S2_L2A_20M_SNOW_MASK_IDX;
+                m_nWaterMaskBandIndex = S2_L2A_20M_WATER_MASK_IDX;
+                m_nCurrentL2AWeightBandIndex = S2_L2A_20M_TOTAL_WEIGHT_IDX;
+                m_nPrevL3AWeightBandStartIndex = S2_L2A_20M_L3A_WEIGHT_START_IDX;
+                m_nPrevL3AWeightedAvDateBandIndex = S2_L2A_20M_L3A_W_AV_DATE_IDX;
+                m_nPrevL3AReflectanceBandStartIndex = S2_L2A_20M_L3A_REFL_START_IDX;
+                m_nPrevL3APixelFlagBandIndex = S2_L2A_20M_L3A_PIXEL_STATUS_IDX;
+                m_nRedBandIndex = S2_L2A_20M_RED_BAND_IDX;
+                m_nBlueBandIndex = S2_L2A_20M_BLUE_BAND_IDX;
+
+            } else if (sensorType == SENSOR_L8) {
+
+                m_nNbL2ABands = L8_L2A_20M_BANDS_NO;
+                m_nL2ABandStartIndex = L8_L2A_20M_BANDS_START_IDX;
+                m_nCloudMaskBandIndex = L8_L2A_20M_CLD_MASK_IDX;
+                m_nSnowMaskBandIndex = L8_L2A_20M_SNOW_MASK_IDX;
+                m_nWaterMaskBandIndex = L8_L2A_20M_WATER_MASK_IDX;
+                m_nCurrentL2AWeightBandIndex = L8_L2A_20M_TOTAL_WEIGHT_IDX;
+                m_nPrevL3AWeightBandStartIndex = L8_L2A_20M_L3A_WEIGHT_START_IDX;
+                m_nPrevL3AWeightedAvDateBandIndex = L8_L2A_20M_L3A_W_AV_DATE_IDX;
+                m_nPrevL3AReflectanceBandStartIndex = L8_L2A_20M_L3A_REFL_START_IDX;
+                m_nPrevL3APixelFlagBandIndex = L8_L2A_20M_L3A_PIXEL_STATUS_IDX;
+                m_nRedBandIndex = L8_L2A_20M_RED_BAND_IDX;
+                m_nBlueBandIndex = L8_L2A_20M_BLUE_BAND_IDX;
+
+            } else {
+                // TODO: Throw an error
+            }
+        } else {
+            // TODO: Throw an error
+        }
+    }
+
+    void SetReflectanceQuantificationValue(float fQuantifVal)
+    {
+        m_fQuantificationValue = fQuantifVal;
+    }
+
+    void SetCurrentDate(int nDate)
+    {
+        m_nCurrentDate = nDate;
+    }
 };
 
 #endif // UPDATESYNTHESIS_H
