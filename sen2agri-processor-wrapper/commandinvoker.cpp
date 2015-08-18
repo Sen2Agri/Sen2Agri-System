d2834f2bf293e0d539615045462c84c6b3492708
Laurentiu Nicola <lnicola@dend.ro>
2015-08-18 15:46:30 +0300

    Wait for the process to finish

--- sen2agri-processor-wrapper/commandinvoker.cpp
+++ sen2agri-processor-wrapper/commandinvoker.cpp
@@ -2,20 +2,11 @@
 
 #include "commandinvoker.h"
 
-CommandInvoker::CommandInvoker()
-{
-    m_pListener = NULL;
-}
+CommandInvoker::CommandInvoker() { m_pListener = NULL; }
 
-CommandInvoker::~CommandInvoker()
-{
+CommandInvoker::~CommandInvoker() {}
 
-}
-
-void CommandInvoker::SetListener(ICommandInvokerListener *pListener)
-{
-    m_pListener = pListener;
-}
+void CommandInvoker::SetListener(ICommandInvokerListener *pListener) { m_pListener = pListener; }
 
 bool CommandInvoker::InvokeCommand(QString &strCmd, QStringList &listParams, bool bIsAsync)
 {
@@ -23,18 +14,23 @@ bool CommandInvoker::InvokeCommand(QString &strCmd, QStringList &listParams, boo
     QString outputStr;
 
     m_logStr.clear();
-    if(listParams.isEmpty()) {
+    if (listParams.isEmpty()) {
         m_process.start(strCmd);
     } else {
         m_process.start(strCmd, listParams);
     }
     if (m_process.waitForStarted(-1)) {
-        if(!bIsAsync) {
-            while(m_process.waitForReadyRead(-1)) {
+        if (!bIsAsync) {
+            if (!m_process.waitForFinished(-1)) {
+                Logger::error(QStringLiteral("Unable to wait for process to finish: %1")
+                                  .arg(m_process.errorString()));
+            }
+
+            while (m_process.waitForReadyRead(-1)) {
                 QByteArray ba = m_process.readAllStandardOutput();
                 outputStr = QString(ba);
                 m_logStr += outputStr;
-                if(m_pListener)
+                if (m_pListener)
                     m_pListener->OnNewMessage(outputStr);
             }
         }
@@ -49,12 +45,9 @@ bool CommandInvoker::InvokeCommand(QString &strCmd, QStringList &listParams, boo
 void CommandInvoker::StopCurCmdExec()
 {
     QProcess::ProcessState procState = m_process.state();
-    if(procState != QProcess::NotRunning) {
+    if (procState != QProcess::NotRunning) {
         m_process.kill();
     }
 }
 
-QString& CommandInvoker::GetExecutionLog()
-{
-    return m_logStr;
-}
+QString &CommandInvoker::GetExecutionLog() { return m_logStr; }
