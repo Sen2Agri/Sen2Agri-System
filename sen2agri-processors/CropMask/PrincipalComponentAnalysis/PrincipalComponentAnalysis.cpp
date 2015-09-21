20b0f36eef75ac8e1e0c7a12803405f94fdb67cb
Lucian Barbulescu <lucian.barbulescu@c-s.ro>
2015-09-21 17:59:15 +0300

    Implemented post processing modules

---
+++ sen2agri-processors/CropMask/PrincipalComponentAnalysis/PrincipalComponentAnalysis.cpp
@@ -0,0 +1,224 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+//  Software Guide : BeginCommandLineArgs
+//    INPUTS: {reference polygons}, {sample ratio}
+//    OUTPUTS: {training polygons}, {validation_polygons}
+//  Software Guide : EndCommandLineArgs
+
+
+//  Software Guide : BeginLatex
+// The sample selection consists in splitting the reference data into 2 disjoint sets, the training set and the validation set.
+// These sets are composed of polygons, not individual pixels.
+//
+//  Software Guide : EndLatex
+
+//  Software Guide : BeginCodeSnippet
+#include "otbWrapperApplication.h"
+#include "otbWrapperApplicationFactory.h"
+
+#include "otbPCAImageFilter.h"
+#include "otbVectorImage.h"
+
+typedef short                                PixelValueType;
+typedef otb::VectorImage<PixelValueType, 2>  ImageType;
+
+typedef otb::ImageFileReader<ImageType>                                     ReaderType;
+typedef otb::PCAImageFilter< ImageType, ImageType, otb::Transform::FORWARD> PCAFilterType;
+
+//  Software Guide : EndCodeSnippet
+
+namespace otb
+{
+
+//  Software Guide : BeginLatex
+//  Application class is defined in Wrapper namespace.
+//
+//  Software Guide : EndLatex
+
+//  Software Guide : BeginCodeSnippet
+namespace Wrapper
+{
+//  Software Guide : EndCodeSnippet
+
+
+//  Software Guide : BeginLatex
+//
+//  SampleSelection class is derived from Application class.
+//
+//  Software Guide : EndLatex
+
+//  Software Guide : BeginCodeSnippet
+class PrincipalComponentAnalysis : public Application
+//  Software Guide : EndCodeSnippet
+{
+public:
+  //  Software Guide : BeginLatex
+  // The \code{ITK} public types for the class, the superclass and smart pointers.
+  // Software Guide : EndLatex
+
+  //  Software Guide : BeginCodeSnippet
+  typedef PrincipalComponentAnalysis Self;
+  typedef Application Superclass;
+  typedef itk::SmartPointer<Self> Pointer;
+  typedef itk::SmartPointer<const Self> ConstPointer;
+  // Software Guide : EndCodeSnippet
+
+  //  Software Guide : BeginLatex
+  //  Invoke the macros necessary to respect ITK object factory mechanisms.
+  //  Software Guide : EndLatex
+
+  //  Software Guide : BeginCodeSnippet
+  itkNewMacro(Self)
+;
+
+  itkTypeMacro(PrincipalComponentAnalysis, otb::Application)
+;
+  //  Software Guide : EndCodeSnippet
+
+
+private:
+
+  //  Software Guide : BeginLatex
+  //  \code{DoInit()} method contains class information and description, parameter set up, and example values.
+  //  Software Guide : EndLatex
+
+  void DoInit()
+  {
+
+    // Software Guide : BeginLatex
+    // Application name and description are set using following methods :
+    // \begin{description}
+    // \item[\code{SetName()}] Name of the application.
+    // \item[\code{SetDescription()}] Set the short description of the class.
+    // \item[\code{SetDocName()}] Set long name of the application (that can be displayed \dots).
+    // \item[\code{SetDocLongDescription()}] This methods is used to describe the class.
+    // \item[\code{SetDocLimitations()}] Set known limitations (threading, invalid pixel type \dots) or bugs.
+    // \item[\code{SetDocAuthors()}] Set the application Authors. Author List. Format : "John Doe, Winnie the Pooh" \dots
+    // \item[\code{SetDocSeeAlso()}] If the application is related to one another, it can be mentioned.
+    // \end{description}
+    // Software Guide : EndLatex
+
+    //  Software Guide : BeginCodeSnippet
+      SetName("PrincipalComponentAnalysis");
+      SetDescription("The feature extraction step produces the relevant features for the classication.");
+
+      SetDocName("PrincipalComponentAnalysis");
+      SetDocLongDescription("The feature extraction step produces the relevant features for the classication. The features are computed"
+                            "for each date of the resampled and gaplled time series and concatenated together into a single multi-channel"
+                            "image file. The selected features are the surface reflectances, the NDVI, the NDWI and the brightness.");
+      SetDocLimitations("None");
+      SetDocAuthors("LBU");
+      SetDocSeeAlso(" ");
+    //  Software Guide : EndCodeSnippet
+
+
+    // Software Guide : BeginLatex
+    // \code{AddDocTag()} method categorize the application using relevant tags.
+    // \code{Code/ApplicationEngine/otbWrapperTags.h} contains some predefined tags defined in \code{Tags} namespace.
+    // Software Guide : EndLatex
+
+    //  Software Guide : BeginCodeSnippet
+    AddDocTag(Tags::Vector);
+    //  Software Guide : EndCodeSnippet
+
+    // Software Guide : BeginLatex
+    // The input parameters:
+    // - ref: Vector file containing reference data
+    // - ratio: Ratio between the number of training and validation polygons per class (dafault: 0.75)
+    // The output parameters:
+    // - tp: Vector file containing reference data for training
+    // - vp: Vector file containing reference data for validation
+    // Software Guide : EndLatex
+
+    //  Software Guide : BeginCodeSnippet
+    AddParameter(ParameterType_InputImage, "ndvi", "The NDVI time series");
+    AddParameter(ParameterType_Int, "nc", "The number of required components.");
+
+    AddParameter(ParameterType_OutputImage, "out", "Vector image containing the principal component images");
+
+    SetDefaultParameterInt("nc", 6);
+    SetMinimumParameterIntValue("nc",1);
+    SetMinimumParameterIntValue("nc",6);
+
+     //  Software Guide : EndCodeSnippet
+
+    // Software Guide : BeginLatex
+    // An example commandline is automatically generated. Method \code{SetDocExampleParameterValue()} is
+    // used to set parameters. Dataset should be located in  \code{OTB-Data/Examples} directory.
+    // Software Guide : EndLatex
+
+    //  Software Guide : BeginCodeSnippet
+    SetDocExampleParameterValue("ndvi", "ndvi.tif");
+    SetDocExampleParameterValue("nc", "6");
+    SetDocExampleParameterValue("out", "pca.tif");
+    //  Software Guide : EndCodeSnippet
+  }
+
+  // Software Guide : BeginLatex
+  // \code{DoUpdateParameters()} is called as soon as a parameter value change. Section \ref{sec:appDoUpdateParameters}
+  // gives a complete description of this method.
+  // Software Guide : EndLatex
+  //  Software Guide :BeginCodeSnippet
+  void DoUpdateParameters()
+  {
+
+      m_ndviReader = ReaderType::New();
+      m_pcaFilter = PCAFilterType::New();
+  }
+  //  Software Guide : EndCodeSnippet
+
+  // Software Guide : BeginLatex
+  // The algorithm consists in a random sampling without replacement of the polygons of each class with
+  // probability p = sample_ratio value for the training set and
+  // 1 - p for the validation set.
+  // Software Guide : EndLatex
+  //  Software Guide :BeginCodeSnippet
+  void DoExecute()
+  {
+      // Get the number of components
+      int nc = GetParameterInt("nc");
+
+      //Read the input file
+      m_ndviReader->SetFileName(GetParameterString("ndvi"));
+      m_ndviReader->UpdateOutputInformation();
+
+      m_pcaFilter->SetNumberOfPrincipalComponentsRequired(nc);
+      m_pcaFilter->SetInput(m_ndviReader->GetOutput());
+
+      SetParameterOutputImage("out", m_pcaFilter->GetOutput());
+
+  }
+  //  Software Guide :EndCodeSnippet
+
+  ReaderType::Pointer                     m_ndviReader;
+  PCAFilterType::Pointer                  m_pcaFilter;
+};
+}
+}
+
+// Software Guide : BeginLatex
+// Finally \code{OTB\_APPLICATION\_EXPORT} is called.
+// Software Guide : EndLatex
+//  Software Guide :BeginCodeSnippet
+OTB_APPLICATION_EXPORT(otb::Wrapper::PrincipalComponentAnalysis)
+//  Software Guide :EndCodeSnippet
+
+
+
