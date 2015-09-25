18298441ddc0477ae21dd0773572225ce44321b2
Cosmin Udroiu <cosmin@c-s.ro>
2015-09-25 17:35:07 +0300

    Updates.

--- sen2agri-processors/VegetationStatus/TestScripts/generate_lai_model.sh
+++ sen2agri-processors/VegetationStatus/TestScripts/generate_lai_model.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-if [ $# -lt 1 ]
+if [ $# -lt 2 ]
 then
-  echo "Usage: $0 <out folder name>"
+  echo "Usage: $0 <include file> <out folder name>"
   echo "The output directory should be given" 1>&2  
   exit
 fi
@@ -20,32 +20,95 @@ function try {
     fi
 }
 
-OUT_FOLDER=$1
+function getReducedAngle() {
+    DECIMAL=$1
+
+    INTEGER=${DECIMAL%.*}
+    FRACT=${DECIMAL#*.}
+
+    echo "$INTEGER.${FRACT:0:1}"
+}
+
+OUT_FOLDER=$2
 
 VEG_STATUS_OTB_LIBS_ROOT="~/sen2agri-processors-build/VegetationStatus"
 IMG_INV_OTB_LIBS_ROOT="$VEG_STATUS_OTB_LIBS_ROOT/otb-bv/src/applications"
 OUT_GENERATED_SAMPLE_FILE="$OUT_FOLDER/out_bv_dist_samples.txt"
 
+# THESE ARE DEFAULT PARAMETERS - THEY WILL BE OVERWRITTEN BY THE PARAMETERS IN
+# THE INCLUDE FILE
+
+#parameters for BVInputVariableGeneration
+GENERATED_SAMPLES_NO=100
+
+#parameters for ProSailSimulator
+SATELLITE_NAME="SPOT4"
+SOLAR_ZENITH_ANGLE=25
+SENSOR_ZENITH_ANGLE=25
+RELATIVE_AZIMUTH_ANGLE=10
+
+#parameters for TrainingDataGenerator
+BV_IDX=0
+ADD_REFLS=0
+RED_INDEX=0
+NIR_INDEX=2
+
+#parameters for generating model
+REGRESSION_TYPE="nn"
+BEST_OF=1
+
+#USER modif
+#add directories where SPOT products are to be found
+source $1
+#end of USER modif
+
+RSR_FILE=""
+if [ "$SATELLITE_NAME" == "SPOT4" ]; then
+    RSR_FILE="../otb-bv/data/spot4hrvir1.rsr"
+else 
+    if [ "$SATELLITE_NAME" == "SPOT5" ]; then
+        RSR_FILE="../otb-bv/data/spot5hrg1.rsr"
+    else
+        if [ "$SATELLITE_NAME" == "S2" ]; then
+            RSR_FILE="../otb-bv/data/Sentinel2.rsr"
+        else
+            if [ "$SATELLITE_NAME" == "L8" ]; then
+                RSR_FILE="../otb-bv/data/landsat8.rsr"
+            else
+                echo "Unknown satellite name: $SATELLITE_NAME" 
+            fi
+        fi
+    fi
+fi
+
 # Variables for Prosail Simulator
-RSR_FILE="../otb-bv/data/spot5hrg1.rsr"
+
 OUT_SIMU_REFLS_FILE="$OUT_FOLDER/out_simu_refls.txt"
 OUT_TRAINING_FILE="$OUT_FOLDER/out_training.txt"
 
+SOLAR_ZENITH_REDUCED=$( getReducedAngle $SOLAR_ZENITH_ANGLE)
+SENSOR_ZENITH_REDUCED=$( getReducedAngle $SENSOR_ZENITH_ANGLE)
+REL_AZIMUTH_REDUCED=$( getReducedAngle $RELATIVE_AZIMUTH_ANGLE)
+
+echo "SOLAR ANGLE reduced from $SOLAR_ZENITH_ANGLE to $SOLAR_ZENITH_REDUCED"
+echo "SENSOR ANGLE reduced from $SENSOR_ZENITH_ANGLE to $SENSOR_ZENITH_REDUCED"
+echo "AZIMUTH ANGLE reduced from $RELATIVE_AZIMUTH_ANGLE to $REL_AZIMUTH_REDUCED"
+
 # Variables for InverseModelLearning
-OUT_MODEL_FILE="$OUT_FOLDER/out_model.txt"
-OUT_ERR_EST_FILE="$OUT_FOLDER/out_model_errest.txt"
+OUT_MODEL_FILE="$OUT_FOLDER/Model_THETA_S_"$SOLAR_ZENITH_REDUCED"_THETA_V_"$SENSOR_ZENITH_REDUCED"_REL_PHI_"$REL_AZIMUTH_REDUCED".txt"
+OUT_ERR_EST_FILE="$OUT_FOLDER/Err_Est_Model_THETA_S_"$SOLAR_ZENITH_REDUCED"_THETA_V_"$SENSOR_ZENITH_REDUCED"_REL_PHI_"$REL_AZIMUTH_REDUCED".txt"
 
 #generating Input BV Distribution file
 echo "Generating Input BV Distribution file ..."
-try otbcli BVInputVariableGeneration $IMG_INV_OTB_LIBS_ROOT -samples 100 -out $OUT_GENERATED_SAMPLE_FILE
+try otbcli BVInputVariableGeneration $IMG_INV_OTB_LIBS_ROOT -samples $GENERATED_SAMPLES_NO -out $OUT_GENERATED_SAMPLE_FILE
 
 # Generating simulation reflectances
 echo "Generating simulation reflectances ..."
-try otbcli ProSailSimulator $IMG_INV_OTB_LIBS_ROOT -bvfile $OUT_GENERATED_SAMPLE_FILE -rsrfile $RSR_FILE -out $OUT_SIMU_REFLS_FILE -solarzenith 25 -sensorzenith 25 -azimuth 10
+try otbcli ProSailSimulator $IMG_INV_OTB_LIBS_ROOT -bvfile $OUT_GENERATED_SAMPLE_FILE -rsrfile $RSR_FILE -out $OUT_SIMU_REFLS_FILE -solarzenith $SOLAR_ZENITH_ANGLE -sensorzenith $SENSOR_ZENITH_ANGLE -azimuth $RELATIVE_AZIMUTH_ANGLE
 
 # Generating training file
 echo "Generating training file ..."
-try otbcli TrainingDataGenerator $IMG_INV_OTB_LIBS_ROOT -biovarsfile $OUT_GENERATED_SAMPLE_FILE -simureflsfile $OUT_SIMU_REFLS_FILE -outtrainfile $OUT_TRAINING_FILE -bvidx 0 -addrefls 0 -redidx 0 -niridx 2
+try otbcli TrainingDataGenerator $IMG_INV_OTB_LIBS_ROOT -biovarsfile $OUT_GENERATED_SAMPLE_FILE -simureflsfile $OUT_SIMU_REFLS_FILE -outtrainfile $OUT_TRAINING_FILE -bvidx $BV_IDX -addrefls $ADD_REFLS -redidx $RED_INDEX -niridx $NIR_INDEX
 
 # Generating model
 echo "Generating model ..."
