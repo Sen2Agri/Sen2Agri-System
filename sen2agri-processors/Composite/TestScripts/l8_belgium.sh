6206792bee11311c02978116632c11fe7da998df
Cosmin Udroiu <cosmin@c-s.ro>
2015-10-18 22:40:46 +0300

    New scripts

---
+++ sen2agri-processors/Composite/TestScripts/l8_belgium.sh
@@ -0,0 +1,11 @@
+#USER modif
+#add directories where SPOT products are to be found
+inputXML[0]='/mnt/Sen2Agri_DataSets/L2A/Landsat8/Belgium/BelgiumS2A_20130728_L8_198_025/BelgiumS2A_20130728_L8_198_025.HDR'
+inputXML[1]='/mnt/Sen2Agri_DataSets/L2A/Landsat8/Belgium/BelgiumS2A_20130930_L8_198_025/BelgiumS2A_20130930_L8_198_025.HDR'
+inputXML[2]='/mnt/Sen2Agri_DataSets/L2A/Landsat8/Belgium/BelgiumS2A_20131203_L8_198_025/BelgiumS2A_20131203_L8_198_025.HDR'
+
+L3A_DATE="20131203"
+HALF_SYNTHESIS="75"
+BANDS_MAPPING="bands_mapping_L8.txt"
+#end of USER modif
+
---
+++ sen2agri-processors/Composite/TestScripts/l8_south_africa.sh
@@ -0,0 +1,21 @@
+#USER modif
+#add directories where L8 products are to be found
+inputXML[0]='/mnt/Sen2Agri_DataSets/L2A/Landsat8/South Africa/MACCS_Manual_Format/SouthAfricaS2A_20130426_L8_171_079/SouthAfricaS2A_20130426_L8_171_079.HDR'
+inputXML[1]='/mnt/Sen2Agri_DataSets/L2A/Landsat8/South Africa/MACCS_Manual_Format/SouthAfricaS2A_20130512_L8_171_079/SouthAfricaS2A_20130512_L8_171_079.HDR'
+inputXML[2]='/mnt/Sen2Agri_DataSets/L2A/Landsat8/South Africa/MACCS_Manual_Format/SouthAfricaS2A_20130528_L8_171_079/SouthAfricaS2A_20130528_L8_171_079.HDR'
+inputXML[3]='/mnt/Sen2Agri_DataSets/L2A/Landsat8/South Africa/MACCS_Manual_Format/SouthAfricaS2A_20130613_L8_171_079/SouthAfricaS2A_20130613_L8_171_079.HDR'
+inputXML[4]='/mnt/Sen2Agri_DataSets/L2A/Landsat8/South Africa/MACCS_Manual_Format/SouthAfricaS2A_20130629_L8_171_079/SouthAfricaS2A_20130629_L8_171_079.HDR'
+inputXML[5]='/mnt/Sen2Agri_DataSets/L2A/Landsat8/South Africa/MACCS_Manual_Format/SouthAfricaS2A_20130715_L8_171_079/SouthAfricaS2A_20130715_L8_171_079.HDR'
+inputXML[6]='/mnt/Sen2Agri_DataSets/L2A/Landsat8/South Africa/MACCS_Manual_Format/SouthAfricaS2A_20130731_L8_171_079/SouthAfricaS2A_20130731_L8_171_079.HDR'
+inputXML[7]='/mnt/Sen2Agri_DataSets/L2A/Landsat8/South Africa/MACCS_Manual_Format/SouthAfricaS2A_20130816_L8_171_079/SouthAfricaS2A_20130816_L8_171_079.HDR'
+inputXML[8]='/mnt/Sen2Agri_DataSets/L2A/Landsat8/South Africa/MACCS_Manual_Format/SouthAfricaS2A_20130901_L8_171_079/SouthAfricaS2A_20130901_L8_171_079.HDR'
+inputXML[9]='/mnt/Sen2Agri_DataSets/L2A/Landsat8/South Africa/MACCS_Manual_Format/SouthAfricaS2A_20130917_L8_171_079/SouthAfricaS2A_20130917_L8_171_079.HDR'
+inputXML[10]='/mnt/Sen2Agri_DataSets/L2A/Landsat8/South Africa/MACCS_Manual_Format/SouthAfricaS2A_20131019_L8_171_079/SouthAfricaS2A_20131019_L8_171_079.HDR'
+inputXML[10]='/mnt/Sen2Agri_DataSets/L2A/Landsat8/South Africa/MACCS_Manual_Format/SouthAfricaS2A_20131104_L8_171_079/SouthAfricaS2A_20131104_L8_171_079.HDR'
+inputXML[12]='/mnt/Sen2Agri_DataSets/L2A/Landsat8/South Africa/MACCS_Manual_Format/SouthAfricaS2A_20131120_L8_171_079/SouthAfricaS2A_20131120_L8_171_079.HDR'
+inputXML[13]='/mnt/Sen2Agri_DataSets/L2A/Landsat8/South Africa/MACCS_Manual_Format/SouthAfricaS2A_20131222_L8_171_079/SouthAfricaS2A_20131222_L8_171_079.HDR'
+
+L3A_DATE="20131222"
+HALF_SYNTHESIS="120"
+BANDS_MAPPING="bands_mapping_L8.txt"
+#end of USER modif
