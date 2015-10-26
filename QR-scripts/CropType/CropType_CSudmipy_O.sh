37390f2e2665b289c6d17ebd0cba4533bbe77d52
Ramona Popescu <ramona.popescu@c-s.ro>
2015-10-26 15:34:05 +0200

    crop type scripts for QR

--- QR-scripts/CropType/CropType_CSudmipy_O.sh
+++ QR-scripts/CropType/CropType_CSudmipy_O.sh
@@ -2,7 +2,7 @@
 
 OUT_FOLDER=$1
 
-./CropMask.py -refp /mnt/Imagery_S2A/In-Situ_TDS/France/LC/SudmipyS2A_LandCoverDecoupe_dissolvedGeometry.shp -ratio 0.75 -input \
+./CropType.py -ref /mnt/Imagery_S2A/In-Situ_TDS/France/LC/SudmipyS2A_LandCoverDecoupe_dissolvedGeometry.shp -ratio 0.75 -input \
 /mnt/Satellite_Imagery/S2-QR/CSudmipy-O_LEVEL2A/SPOT4_HRVIR_XS_20130216_N2A_CSudmipy-OD0000B0000/SPOT4_HRVIR_XS_20130216_N2A_CSudmipy-OD0000B0000.xml \
 /mnt/Satellite_Imagery/S2-QR/CSudmipy-O_LEVEL2A/SPOT4_HRVIR_XS_20130221_N2A_CSudmipy-OD0000B0000/SPOT4_HRVIR_XS_20130221_N2A_CSudmipy-OD0000B0000.xml \
 /mnt/Satellite_Imagery/S2-QR/CSudmipy-O_LEVEL2A/SPOT4_HRVIR_XS_20130303_N2A_CSudmipy-OD0000B0000/SPOT4_HRVIR_XS_20130303_N2A_CSudmipy-OD0000B0000.xml \
@@ -19,5 +19,5 @@ OUT_FOLDER=$1
 /mnt/Satellite_Imagery/S2-QR/CSudmipy-O_LEVEL2A/SPOT4_HRVIR_XS_20130606_N2A_CSudmipy-OD0000B0000/SPOT4_HRVIR_XS_20130606_N2A_CSudmipy-OD0000B0000.xml \
 /mnt/Satellite_Imagery/S2-QR/CSudmipy-O_LEVEL2A/SPOT4_HRVIR_XS_20130611_N2A_CSudmipy-OD0000B0000/SPOT4_HRVIR_XS_20130611_N2A_CSudmipy-OD0000B0000.xml \
 /mnt/Satellite_Imagery/S2-QR/CSudmipy-O_LEVEL2A/SPOT4_HRVIR_XS_20130616_N2A_CSudmipy-OD0000B0000/SPOT4_HRVIR_XS_20130616_N2A_CSudmipy-OD0000B0000.xml \
--t0 20130131 -tend 20130615 -rate 5 -radius 100 -nbtrsample 4000 -rseed 0 -lmbd 2 -weight 1 -nbcomp 6 -spatialr 10 -ranger 0.65 -minsize 10 -rfnbtrees 100 -rfmax 25 -rfmin 5 -pixsize 20 \
+-t0 20130216 -tend 20130616 -rate 5 -radius 100 -classifier rf -rseed 0 -mask /mnt/data/QR_scripts/CropMask_CSudmipy_O/crop_mask.tif -pixsize 20 \
 -outdir $OUT_FOLDER
