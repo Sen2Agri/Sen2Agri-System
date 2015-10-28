40845ab101a422c0940412a0d91c96588cbd0d8d
Alex Grosu <agrosu@c-s.ro>
2015-10-28 15:25:49 +0200

    corrections for qr scripts

--- QR-scripts/PhenoNDVI/IT/PhenoNDVI_CSudmipy_O.sh
+++ QR-scripts/PhenoNDVI/IT/PhenoNDVI_CSudmipy_O.sh
@@ -13,18 +13,20 @@ function try {
     fi
 }
 
-
-OTB_APP="/home/agrosu/sen2agri-processors-build/VegetationStatus/"
+CROPTYPE_APP="/home/agrosu/sen2agri-processors-build/CropType"
+OTB_APP="/home/agrosu/sen2agri-processors-build/VegetationStatus/phenotb/src/Applications/"
 SCRIPT_PATH="/home/agrosu/sen2agri/sen2agri-processors/VegetationStatus/TestScripts"
-OUTPUT_PATH="/mnt/data/output_temp/out_qr_lai_south_africa_tests"
+OUTPUT_PATH="/mnt/data/output_temp/out_qr_pheno_ndvi_midpir_W"
 
 echo "The OTB appliction will be launched from:"
 echo "$OTB_APP"
-echo "The used RSR file:"
-echo "$SCRIPT_PATH/../otb-bv/data/spot4hrvir1.rsr"
 echo "The output path"
 echo "$OUTPUT_PATH"
+echo "The script path:"
+echo "$SCRIPT_PATH"
 mkdir "$OUTPUT_PATH"
 try cd "$SCRIPT_PATH"
 
-"./lai_retr_qr_southafrica.sh" "$OTB_APP" 0 "../otb-bv/data/spot4hrvir1.rsr" "$OUTPUT_PATH" "tc2-3"
+"./qr_pheno_ndvi_midpir_W.sh" "$CROPTYPE_APP" "$OTB_APP" "$OUTPUT_PATH"
+
+
