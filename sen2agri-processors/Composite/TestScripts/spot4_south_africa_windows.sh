0ebc1219e1b679179dcbce4d011d1c77155e529f
Alex Grosu <agrosu@c-s.ro>
2015-11-03 19:17:36 +0200

    test scripts

--- sen2agri-processors/Composite/TestScripts/spot4_south_africa_windows.sh
+++ sen2agri-processors/Composite/TestScripts/spot4_south_africa_windows.sh
@@ -1,6 +1,50 @@
 #!/bin/bash
+#USER modif
+#add directories where SPOT products are to be found
+inputXML="L3A_DATE=20130228|HALF=15 "
+#inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130131_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130131_N2A_ESouthAfricaD0000B0000.xml "
+#inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130205_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130205_N2A_ESouthAfricaD0000B0000.xml "
+inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130220_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130220_N2A_ESouthAfricaD0000B0000.xml "
+inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130225_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130225_N2A_ESouthAfricaD0000B0000.xml "
+inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130302_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130302_N2A_ESouthAfricaD0000B0000.xml "
+inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130312_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130312_N2A_ESouthAfricaD0000B0000.xml "
+inputXML+="L3A_DATE=20130328|HALF=15 "
+inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130317_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130317_N2A_ESouthAfricaD0000B0000.xml "
+inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130322_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130322_N2A_ESouthAfricaD0000B0000.xml "
+inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130401_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130401_N2A_ESouthAfricaD0000B0000.xml "
+inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130406_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130406_N2A_ESouthAfricaD0000B0000.xml "
+inputXML+="L3A_DATE=20130425|HALF=15 "
+inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130416_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130416_N2A_ESouthAfricaD0000B0000.xml "
+inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130421_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130421_N2A_ESouthAfricaD0000B0000.xml "
+inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130426_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130426_N2A_ESouthAfricaD0000B0000.xml "
+inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130501_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130501_N2A_ESouthAfricaD0000B0000.xml "
+inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130506_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130506_N2A_ESouthAfricaD0000B0000.xml "
+inputXML+="L3A_DATE=20130523|HALF=15 "
+inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130511_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130511_N2A_ESouthAfricaD0000B0000.xml "
+inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130516_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130516_N2A_ESouthAfricaD0000B0000.xml "
+inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130521_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130521_N2A_ESouthAfricaD0000B0000.xml "
+inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130526_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130526_N2A_ESouthAfricaD0000B0000.xml "
+inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130531_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130531_N2A_ESouthAfricaD0000B0000.xml "
+inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130605_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130605_N2A_ESouthAfricaD0000B0000.xml "
+#inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130610_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130610_N2A_ESouthAfricaD0000B0000.xml "
+#inputXML+="/mnt/Imagery_S2A/L2A/Spot4-T5/South\ Africa/SPOT4_HRVIR1_XS_20130615_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130615_N2A_ESouthAfricaD0000B0000.xml "
 
-./spot4_south_africa_20130228.sh /home/agrosu/sen2agri-processors-build/Composite 20 /mnt/scratch/composite_southafrica bands_mapping_spot.txt
-./spot4_south_africa_20130328.sh /home/agrosu/sen2agri-processors-build/Composite 20 /mnt/scratch/composite_southafrica bands_mapping_spot.txt
-./spot4_south_africa_20130425.sh /home/agrosu/sen2agri-processors-build/Composite 20 /mnt/scratch/composite_southafrica bands_mapping_spot.txt
-./spot4_south_africa_20130523.sh /home/agrosu/sen2agri-processors-build/Composite 20 /mnt/scratch/composite_southafrica bands_mapping_spot.txt
+
+L3A_DATE="20130615"
+HALF_SYNTHESIS="70"
+#end of USER modif
+
+if [ $# -lt 4 ]
+then
+  echo "Usage: $0 <otb directory application> <resolution> <out folder name> <bands mapping file> [scattering coefficient file - S2 case only]"
+  echo "The file with input xmls should be given. The resolution for which the computations will be performed should be given. The output directory should be given" 1>&2  
+  exit
+fi
+
+SCAT_COEF=""
+if [ $# == 5 ] ; then    
+    ./run_composite_windows.sh "$1" "$inputXML " "$2" "$3" "$L3A_DATE" "$HALF_SYNTHESIS" "$4" "$5"
+else
+    ./run_composite_windows.sh "$1" "$inputXML " "$2" "$3" "$L3A_DATE" "$HALF_SYNTHESIS" "$4"
+fi
+
