b709fc459e9eddcef99d87f1495eb6fa0d364632
Laurentiu Nicola <lnicola@dend.ro>
2015-11-11 15:54:48 +0200

    Add packaging scripts

---
+++ packaging/gdal.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+rm -rf gdal-2.0.1
+rm -rf ~/gdal-install
+
+tar zxf gdal-2.0.1.tar.gz
+cd gdal-2.0.1
+./configure
+make -j4
+mkdir ~/gdal-install
+make install DESTDIR=~/gdal-install
+cd ..
+mkdir tmp
+fpm -s dir -t rpm -n gdal-local -v 2.0.1 -C ~/gdal-install -p gdal-local-VERSION.fc22.ARCH.rpm --workdir ~/tmp usr
