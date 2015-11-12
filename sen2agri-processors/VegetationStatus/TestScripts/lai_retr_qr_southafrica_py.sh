aa2b709ab36b05b6cfe0df660f18d02035b0dbe1
Alex Grosu <agrosu@c-s.ro>
2015-11-12 16:56:30 +0200

    example for running lai_retrieve_processing.py script

---
+++ sen2agri-processors/VegetationStatus/TestScripts/lai_retr_qr_southafrica_py.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+
+#UNCOMMENT THE DEFINITIONS BELOW IF YO NEED OTHER VALUES
+
+#parameters for BVInputVariableGeneration
+#GENERATED_SAMPLES_NO=100
+
+#parameters for ProSailSimulator
+SOLAR_ZENITH_ANGLE=67.372200673
+SENSOR_ZENITH_ANGLE=0.23422731138
+# Computed as PHI_V - 180 - PHI_S
+# if (rel_az < -180.0) then rel_az = rel_az + 360.0
+# if (rel_az > 180.0) then rel_az = rel_az - 360.0
+# PHI_V>-73.809703566</PHI_V>
+#<PHI_S>145.43902353</PHI_S>
+RELATIVE_AZIMUTH_ANGLE=133.750784413
+
+#parameters for TrainingDataGenerator
+#BV_IDX=0
+#ADD_REFLS=0
+#RED_INDEX=0
+#NIR_INDEX=2
+
+#parameters for generating model
+#REGRESSION_TYPE="nn"
+#BEST_OF=1
+
+#end of USER modif
+
+./lai_retrieve_processing.py --applocation /home/agrosu/sen2agri-processors-build --input \
+"/mnt/Satellite_Imagery/S2-QR/SouthAfrica/SPOT4_HRVIR1_XS_20130131_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130131_N2A_ESouthAfricaD0000B0000.xml" \
+"/mnt/Satellite_Imagery/S2-QR/SouthAfrica/SPOT4_HRVIR1_XS_20130205_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130205_N2A_ESouthAfricaD0000B0000.xml" \
+"/mnt/Satellite_Imagery/S2-QR/SouthAfrica/SPOT4_HRVIR1_XS_20130220_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130220_N2A_ESouthAfricaD0000B0000.xml" \
+"/mnt/Satellite_Imagery/S2-QR/SouthAfrica/SPOT4_HRVIR1_XS_20130225_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130225_N2A_ESouthAfricaD0000B0000.xml" \
+--res 0 --t0 20130131 --tend 20130225 --outdir /mnt/scratch/lai_retr_python/python --rsrfile /home/agrosu/sen2agri/sen2agri-processors/VegetationStatus/otb-bv/data/spot4hrvir1.rsr --solarzenith $SOLAR_ZENITH_ANGLE --sensorzenith $SENSOR_ZENITH_ANGLE --relativeazimuth $RELATIVE_AZIMUTH_ANGLE
