bbacbebfab825af3a0c8bfb0d76115d1e4b32a75
Laurentiu Nicola <lnicola@dend.ro>
2016-01-18 16:29:10 +0200

    Add run_qr script

---
+++ scripts/run_qr.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+## SPOT4
+#./CropMask-CSudmipy-E.sh
+#./CropMask-CSudmipy-O.sh
+#./CropMask-SouthAfrica.sh
+#./CropMask-Ukraine.sh
+#./CropMask-Belgium.sh
+
+#./CropType-CSudmipy-E.sh
+#./CropType-CSudmipy-O.sh
+#./CropType-SouthAfrica.sh
+#./CropType-Ukraine.sh
+#./CropType-Belgium.sh
+
+## SPOT5
+#./CropMask-Belgium-S5.sh
+#./CropMask-Burkina-S5.sh
+#./CropMask-Ukraine-S5.sh
+
+#./CropType-Belgium-S5.sh
+#./CropType-Burkina-S5.sh
+#./CropType-Ukraine-S5.sh
+
+## SPOT4 no in-situ data
+# ./CropMask-Belgium-noinsitu.sh
+# ./CropMask-CSudmipy-E-noinsitu.sh
+# ./CropMask-CSudmipy-O-noinsitu.sh
+# ./CropMask-SouthAfrica-noinsitu.sh
+# ./CropMask-Ukraine-noinsitu.sh
+
+## SPOT5 no in-situ data
+#./CropMask-Belgium-S5-noinsitu.sh
+#./CropMask-Ukraine-S5-noinsitu.sh
