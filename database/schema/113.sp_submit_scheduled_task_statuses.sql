a15d0ff0cba86f69e8ae2a236d7a52256959caa6
Cosmin Udroiu <cosmin@c-s.ro>
2016-02-19 13:56:46 +0200

    Updates for the sql queries.

--- database/schema/113.sp_submit_scheduled_task_statuses.sql
+++ database/schema/113.sp_submit_scheduled_task_statuses.sql
@@ -2,8 +2,6 @@
 IN _statuses json
 ) RETURNS void AS $$
 BEGIN
-
-
         UPDATE scheduled_task_status SET
 		next_schedule = params.next_schedule,
 		last_scheduled_run = params.last_scheduled_run,
@@ -11,9 +9,9 @@ BEGIN
 		last_retry_timestamp = params.last_retry_timestamp,
 		estimated_next_run_time = params.estimated_next_run_time
 		
-	FROM  json_populate_recordset(null::params, _statuses) AS params
+	FROM  json_populate_recordset(null::scheduled_task_status, _statuses) AS params
 	
-        WHERE id = params.id;
+        WHERE scheduled_task_status.id = params.id;
 
 END;
 $$ LANGUAGE plpgsql;
