b352a0cbcb83a8bf6f2a4418722e8973e7d87602
Mickael Savinaud <mickael.savinaud@c-s.fr>
2016-02-25 00:38:03 +0100

    ENH: support multidate

--- scripts_CS/2013/LAIRetrieval/LAIRetrieval_Belgium_multidate_All.sh
+++ scripts_CS/2013/LAIRetrieval/LAIRetrieval_Belgium_multidate_All.sh
@@ -2,12 +2,9 @@
 
 python /home/msavinaud/dev/s2agri/src/sen2agri-processors/VegetationStatus/TestScripts/lai_retrieve_processing_mul.py \
 --applocation /home/msavinaud/dev/s2agri/build \
---input \
-"/data/s2agri/input/EOData/2013/Belgium/Belgium/SPOT4_HRVIR1_XS_20130318_N2A_EBelgiumD0000B0000/SPOT4_HRVIR1_XS_20130318_N2A_EBelgiumD0000B0000.xml" \
-"/data/s2agri/input/EOData/2013/Belgium/Belgium/SPOT4_HRVIR1_XS_20130402_N2A_EBelgiumD0000B0000/SPOT4_HRVIR1_XS_20130402_N2A_EBelgiumD0000B0000.xml" \
-"/data/s2agri/input/EOData/2013/Belgium/Belgium/SPOT4_HRVIR1_XS_20130407_N2A_EBelgiumD0000B0000/SPOT4_HRVIR1_XS_20130407_N2A_EBelgiumD0000B0000.xml" \
---t0 20130407 \
---tend 20130407 \
+--inputdir /data/s2agri/input/EOData/2013/Belgium/Belgium/ \
 --outdir /data/s2agri/output/2013/Belgium/LAI_Belgium_monodate \
 --backwardwindow 2 \
---forwardwindow 0
+--forwardwindow 0 \
+--sat SPOT4 \
+--inst HRVIR1
