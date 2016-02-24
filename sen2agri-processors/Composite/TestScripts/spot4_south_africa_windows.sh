945e3d92a45245c234425b22f6e0a3007b020ec0
Cosmin Udroiu <cosmin.udroiu@c-s.ro>
2016-02-24 17:14:05 +0200

    Scripts updates

--- sen2agri-processors/Composite/TestScripts/spot4_south_africa_windows.sh
+++ sen2agri-processors/Composite/TestScripts/spot4_south_africa_windows.sh
@@ -10,14 +10,14 @@ OUT_FOLDER=$3
 "/mnt/Imagery_S2A/L2A/Spot4-T5/South Africa/SPOT4_HRVIR1_XS_20130225_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130225_N2A_ESouthAfricaD0000B0000.xml" \
 "/mnt/Imagery_S2A/L2A/Spot4-T5/South Africa/SPOT4_HRVIR1_XS_20130302_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130302_N2A_ESouthAfricaD0000B0000.xml" \
 "/mnt/Imagery_S2A/L2A/Spot4-T5/South Africa/SPOT4_HRVIR1_XS_20130312_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130312_N2A_ESouthAfricaD0000B0000.xml" \
---res $RES --outdir $OUT_FOLDER/20130228 --bandsmap bands_mapping_spot.txt
+--res $RES --outdir $OUT_FOLDER/20130228 --bandsmap /usr/share/sen2agri/bands_mapping_spot.txt
 
 ./composite_processing.py --applocation $BUILD_FOLDER --syntdate 20130318 --synthalf 15 --input \
 "/mnt/Imagery_S2A/L2A/Spot4-T5/South Africa/SPOT4_HRVIR1_XS_20130317_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130317_N2A_ESouthAfricaD0000B0000.xml" \
 "/mnt/Imagery_S2A/L2A/Spot4-T5/South Africa/SPOT4_HRVIR1_XS_20130322_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130322_N2A_ESouthAfricaD0000B0000.xml" \
 "/mnt/Imagery_S2A/L2A/Spot4-T5/South Africa/SPOT4_HRVIR1_XS_20130401_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130401_N2A_ESouthAfricaD0000B0000.xml" \
 "/mnt/Imagery_S2A/L2A/Spot4-T5/South Africa/SPOT4_HRVIR1_XS_20130406_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130406_N2A_ESouthAfricaD0000B0000.xml" \
---res $RES --outdir $OUT_FOLDER/20130328 --bandsmap bands_mapping_spot.txt
+--res $RES --outdir $OUT_FOLDER/20130328 --bandsmap /usr/share/sen2agri/bands_mapping_spot.txt
 
 ./composite_processing.py --applocation $BUILD_FOLDER --syntdate 20130425 --synthalf 15 --input \
 "/mnt/Imagery_S2A/L2A/Spot4-T5/South Africa/SPOT4_HRVIR1_XS_20130416_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130416_N2A_ESouthAfricaD0000B0000.xml" \
@@ -25,7 +25,7 @@ OUT_FOLDER=$3
 "/mnt/Imagery_S2A/L2A/Spot4-T5/South Africa/SPOT4_HRVIR1_XS_20130426_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130426_N2A_ESouthAfricaD0000B0000.xml" \
 "/mnt/Imagery_S2A/L2A/Spot4-T5/South Africa/SPOT4_HRVIR1_XS_20130501_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130501_N2A_ESouthAfricaD0000B0000.xml" \
 "/mnt/Imagery_S2A/L2A/Spot4-T5/South Africa/SPOT4_HRVIR1_XS_20130506_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130506_N2A_ESouthAfricaD0000B0000.xml" \
---res $RES --outdir $OUT_FOLDER/20130425 --bandsmap bands_mapping_spot.txt
+--res $RES --outdir $OUT_FOLDER/20130425 --bandsmap /usr/share/sen2agri/bands_mapping_spot.txt
 
 ./composite_processing.py --applocation $BUILD_FOLDER --syntdate 20130523 --synthalf 15 --input \
 "/mnt/Imagery_S2A/L2A/Spot4-T5/South Africa/SPOT4_HRVIR1_XS_20130511_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130511_N2A_ESouthAfricaD0000B0000.xml" \
@@ -34,4 +34,4 @@ OUT_FOLDER=$3
 "/mnt/Imagery_S2A/L2A/Spot4-T5/South Africa/SPOT4_HRVIR1_XS_20130526_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130526_N2A_ESouthAfricaD0000B0000.xml" \
 "/mnt/Imagery_S2A/L2A/Spot4-T5/South Africa/SPOT4_HRVIR1_XS_20130531_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130531_N2A_ESouthAfricaD0000B0000.xml" \
 "/mnt/Imagery_S2A/L2A/Spot4-T5/South Africa/SPOT4_HRVIR1_XS_20130605_N2A_ESouthAfricaD0000B0000/SPOT4_HRVIR1_XS_20130605_N2A_ESouthAfricaD0000B0000.xml" \
---res $RES --outdir $OUT_FOLDER/20130523 --bandsmap bands_mapping_spot.txt
+--res $RES --outdir $OUT_FOLDER/20130523 --bandsmap /usr/share/sen2agri/bands_mapping_spot.txt
--- sen2agri-processors/Composite/TestScripts/spot4_windows.sh
+++ sen2agri-processors/Composite/TestScripts/spot4_windows.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./spot4_south_africa_windows.sh ~/sen2agri-processors-build/ 20 /mnt/scratch/composite_alex_southafrica bands_mapping_spot.txt
-./spot4_ukraine_windows.sh ~/sen2agri-processors-build/ 20 /mnt/scratch/composite_alex_ukraine bands_mapping_spot.txt
-./midp_W_windows.sh ~/sen2agri-processors-build/ 20 /mnt/scratch/composite_alex_midp_W bands_mapping_spot.txt
+./spot4_ukraine_windows.sh ~/sen2agri-processors-build 20 /mnt/data/output_temp/composite_ukraine
+./spot4_south_africa_windows.sh ~/sen2agri-processors-build 20 /mnt/data/output_temp/composite_south_africa
+./midp_W_windows.sh ~/sen2agri-processors-build 20 /mnt/data/output_temp/composite_sudmidp_W
