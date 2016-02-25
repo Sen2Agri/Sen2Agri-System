0aad30d4e0ee9df3229a4346cd314d814ad5583f
Mickael Savinaud <mickael.savinaud@c-s.fr>
2016-02-25 09:22:54 +0100

    ENH: add script used for LAI monodate in 2015

---
+++ scripts_CS/2015/LAIRetrieval/LAIRetrieval_Russia_All.sh
@@ -0,0 +1,21 @@
+#! /bin/bash -l
+
+# Retrieve the list of SPOT4 file
+python generate_file_list.py --inputdir /data/s2agri/input/EOData/2015/Russia/Toula/ \
+--instrument HRG2 --outfile /data/s2agri/output/2015/Russia/LAI_conf_file.txt
+
+# Compute the LAI for each product
+
+while read F  ; do
+echo $F
+python /home/msavinaud/dev/s2agri/src/sen2agri-processors/VegetationStatus/TestScripts/lai_retrieve_processing_CS.py \
+--applocation /data/s2agri/sen2agri-processors-build-thor \
+--input $F \
+--res 0 \
+--t0 20150503 \
+--tend 20150503 \
+--generatemodel YES \
+--outdir /data/s2agri/output/2015/Russia/LAI_Russia_monodate \
+--rsrfile /home/msavinaud/dev/s2agri/src/sen2agri-processors/VegetationStatus/otb-bv/data/spot5hrg1.rsr
+        
+done </data/s2agri/output/2015/Russia/LAI_conf_file.txt
