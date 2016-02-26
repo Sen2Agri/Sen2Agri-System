319874deff44339e5f1f53eb96c13513115c3b5c
Claudia Dabu <claudia.dabu@c-s.ro>
2016-02-26 11:59:20 +0200

    Updated styles and js functions

--- sen2agri-dashboard/scripts/processing.js
+++ sen2agri-dashboard/scripts/processing.js
@@ -2,5 +2,5 @@ $(document).ready(function() {
 	//set_current_job_refresh();
 	//set_server_resource_refresh();
 	set_processor_statistics_refresh();
-	//set_product_availability_data_refresh();
+	set_product_availability_data_refresh();
 })
