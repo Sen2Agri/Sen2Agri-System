659825f0489c11606167f0db33247732ca6f7152
Cosmin Udroiu <cosmin@c-s.ro>
2016-02-28 17:40:20 +0200

    Updated sql function to get product by name.

--- database/05-functions/117.sp_get_product_by_name.sql
+++ database/05-functions/117.sp_get_product_by_name.sql
@@ -1,10 +1,10 @@
 CREATE OR REPLACE FUNCTION sp_get_product_by_name(
     IN _name character varying)
-  RETURNS TABLE(product_type_id smallint, processor_id smallint, site_id smallint, full_path character varying) AS
+  RETURNS TABLE(produc_id smallint, product_type_id smallint, processor_id smallint, site_id smallint, full_path character varying, created_timestamp timestamp with time zone) AS
 $BODY$
 BEGIN
 
-RETURN QUERY SELECT product.product_type_id, product.processor_id, product.site_id, product.full_path
+RETURN QUERY SELECT product.product_type_id AS product_id, product.product_type_id, product.processor_id, product.site_id, product.full_path, product.created_timestamp 
 FROM product
 WHERE name = _name;
 
