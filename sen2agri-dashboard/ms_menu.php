7d3b8bfb4a5c76812c04570c21734becedd0b5d6
Claudia Dabu <claudia.dabu@c-s.ro>
2016-03-01 10:32:05 +0200

    ms_menu modified configuration to custom jobs

--- sen2agri-dashboard/ms_menu.php
+++ sen2agri-dashboard/ms_menu.php
@@ -7,7 +7,7 @@ if (isset($_SESSION['siteId'])) {
                 <li><a href="main.php">Products</a></li>
                 <li><a href="system.php">System Overview</a></li>
                 <li><a href="dashboard.php#tab_l2a">Dashboard</a></li>
-                <li><a href="config.php">Configuration</a></li>
+                <li><a href="config.php">Custom Jobs</a></li>
                 <li style="float: right;"><a href="logout.php">Logout</a></li>
             </ul>
         </div>
@@ -22,7 +22,7 @@ if (isset($_SESSION['siteId'])) {
                 <li>Products</li>
                 <li>System Overview</li>
                 <li>Dashboard</li>
-                <li>Configuration</li>
+                <li>Custom Jobs</li>
                 <li style="float: right;"><a href="login.php">Login</a></li>
             </ul>
         </div>
