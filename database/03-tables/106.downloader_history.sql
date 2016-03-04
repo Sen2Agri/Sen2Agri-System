b2f334b8a5a7b010f526d967eba97de88377721a
Alex Grosu <agrosu@c-s.ro>
2016-03-04 16:09:41 +0200

    added the product_timestamp to the downloader_history table

--- database/03-tables/106.downloader_history.sql
+++ database/03-tables/106.downloader_history.sql
@@ -4,6 +4,7 @@ CREATE TABLE downloader_history
   site_id smallint NOT NULL,
   satellite_id smallint NOT NULL references satellite(id),
   product_name varchar NOT NULL,
+  product_timestamp timestamp with time zone NOT NULL DEFAULT now(),	
   full_path varchar NOT NULL,
   created_timestamp timestamp with time zone NOT NULL DEFAULT now(), 
   
