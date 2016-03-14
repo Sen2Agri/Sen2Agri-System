fd8566919af3f9d474755d921c8359359afb8635
Laurentiu Nicola <lnicola@dend.ro>
2016-03-14 12:48:08 +0200

    Add overlap check

--- sen2agri-dashboard/scripts/check_shp.py
+++ sen2agri-dashboard/scripts/check_shp.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 
 import argparse
+import itertools
 import sys
 from osgeo import ogr
 from osgeo import osr
@@ -31,17 +32,26 @@ def main():
     if featureCount != 1:
         union = ogr.Geometry(ogr.wkbMultiPolygon)
 
+    features = []
     for feature in layer:
         geometry = feature.GetGeometryRef()
         if not geometry.IsValid():
             print("Invalid feature: {}".format(geometry.ExportToWkt()))
             return 2
 
+        features.append(feature)
+
         geometry.Transform(transform)
         if featureCount == 1:
             union = geometry
         else:
             union.AddGeometry(geometry)
+
+    for pair in itertools.combinations(features, 2):
+        g0, g1 = pair[0].GetGeometryRef(), pair[1].GetGeometryRef()
+        if g0.Overlaps(g1):
+            print("Overlapping features: {} and {}".format(g0, g1))
+            return 3
 
     print("Union: {}".format(union.ExportToWkt()))
     return 0
