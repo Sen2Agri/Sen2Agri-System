90dca7c5562f3c593b3ba976b4f24d531a68a8ca
Claudia Dabu <claudia.dabu@c-s.ro>
2016-03-18 17:31:37 +0200

    Functions used in site

---
+++ database/05-functions/sp_get_dashboard_processor_scheduled_task.sql
@@ -0,0 +1,20 @@
+﻿CREATE OR REPLACE FUNCTION sp_get_dashboard_processor_scheduled_task(IN _processor_id smallint)
+  RETURNS TABLE(id smallint, name character varying, site_name character varying, repeat_type smallint, first_run_time character varying, repeat_after_days smallint, repeat_on_month_day smallint) AS
+$BODY$
+BEGIN
+   RETURN QUERY
+	SELECT st.id,
+              st.name,        
+              site.name,
+              st.repeat_type,
+              st.first_run_time,
+              st.repeat_after_days,
+              st.repeat_on_month_day
+        FROM scheduled_task AS st, site
+        WHERE st.processor_id = _processor_id AND st.site_id=site.id
+        ORDER BY st.id;
+    
+        
+END
+$BODY$
+  LANGUAGE plpgsql
