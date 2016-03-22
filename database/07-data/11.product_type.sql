fe90c8ce69275db80c47264ee8748a59324f8593
Cosmin Udroiu <cosmin@c-s.ro>
2016-03-22 18:56:41 +0200

    Updates for processors and products SQLs.

---
+++ database/07-data/11.product_type.sql
@@ -0,0 +1,44 @@
+﻿INSERT INTO product_type
+(name, description)
+VALUES 
+('l2a','L2A Atmosferic corrections');
+
+INSERT INTO product_type
+(name, description)
+VALUES 
+('l3a','L3A Composite product');
+
+INSERT INTO product_type
+(name, description)
+VALUES 
+('l3b_lai_monodate','L3B LAI mono-date product');
+
+INSERT INTO product_type
+(name, description)
+VALUES 
+('l3e_pheno','L3E Pheno NDVI product');
+
+INSERT INTO product_type
+(name, description)
+VALUES 
+('l4a','L4A Crop mask product');
+
+INSERT INTO product_type
+(name, description)
+VALUES 
+('l4b','L4B Crop type product');
+
+INSERT INTO product_type
+(name, description)
+VALUES 
+('l1c','L1C product');
+
+INSERT INTO product_type
+(name, description)
+VALUES 
+('l3c_lai_reproc','L3C LAI Reprocessed product');
+
+INSERT INTO product_type
+(name, description)
+VALUES 
+('l3d_lai_fitted','L3D LAI End of Season product');
