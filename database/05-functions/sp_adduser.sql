2fb89f11885aa682f939115ab9447bffa0f8781b
Cosmin Udroiu <cosmin@c-s.ro>
2016-03-23 18:52:00 +0200

    Updated sp_adduser.

--- database/05-functions/sp_adduser.sql
+++ database/05-functions/sp_adduser.sql
@@ -1,9 +1,14 @@
-CREATE OR REPLACE FUNCTION sp_adduser("userName" character varying, email character varying, pwd text, "roleId" smallint DEFAULT 2, "siteId" smallint DEFAULT NULL::smallint)
+CREATE OR REPLACE FUNCTION sp_adduser(
+    "userName" character varying, 
+    email character varying, 
+    pwd text, 
+    "roleId" smallint DEFAULT 2, 
+    "siteId" smallint DEFAULT NULL::smallint)
   RETURNS smallint AS
 $BODY$INSERT INTO "user"(
             id, login, email, role_id, site_id, password)
     VALUES (
-    (SELECT MAX(id)+1 FROM public.user), 
+    COALESCE((SELECT MAX(id) FROM public.user) :: integer, 0) + 1, 
     $1, $2, $4, $5,
     crypt($3, gen_salt('md5')));
 SELECT id FROM public.user WHERE login = $1;$BODY$
