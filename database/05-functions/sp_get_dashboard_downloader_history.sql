4e565d9e2cee144ae17bca59a98c87e4b5d792d4
Claudia Dabu <claudia.dabu@c-s.ro>
2016-03-24 09:56:43 +0200

    function corrected

--- database/05-functions/sp_get_dashboard_downloader_history.sql
+++ database/05-functions/sp_get_dashboard_downloader_history.sql
@@ -6,15 +6,15 @@ BEGIN
 
 	SELECT  1, count(status_id)
 		FROM downloader_history
-		WHERE status_id  = 1 AND ( site_id = 1)
+		WHERE status_id  = 1 AND ( $1 IS NULL OR site_id = _siteid)
    UNION
 	SELECT  2, count(status_id)
 		FROM downloader_history
-		WHERE status_id IN (2, 5) AND ( site_id = 1)
+		WHERE status_id IN (2, 5) AND ( $1 IS NULL OR site_id = _siteid)
    UNION
 	SELECT  3, count(status_id)
 		FROM downloader_history
-		WHERE status_id IN (3, 4) AND ( site_id = 1)
+		WHERE status_id IN (3, 4) AND ( $1 IS NULL OR site_id = _siteid)
    ORDER BY 1;
 
 END
