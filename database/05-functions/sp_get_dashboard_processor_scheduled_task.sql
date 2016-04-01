e5af1b01d2fd155cd965afa6a807af3e8b61509b
Claudia Dabu <claudia.dabu@c-s.ro>
2016-04-01 16:37:50 +0300

    functions modified for proessor_params LAI for dashboard

--- database/05-functions/sp_get_dashboard_processor_scheduled_task.sql
+++ database/05-functions/sp_get_dashboard_processor_scheduled_task.sql
@@ -1,5 +1,6 @@
-﻿CREATE OR REPLACE FUNCTION sp_get_dashboard_processor_scheduled_task(IN _processor_id smallint)
-  RETURNS TABLE(id smallint, name character varying, site_name character varying, repeat_type smallint, first_run_time character varying, repeat_after_days smallint, repeat_on_month_day smallint) AS
+﻿
+CREATE OR REPLACE FUNCTION sp_get_dashboard_processor_scheduled_task(IN _processor_id smallint)
+  RETURNS TABLE(id smallint, name character varying, site_name character varying, repeat_type smallint, first_run_time character varying, repeat_after_days smallint, repeat_on_month_day smallint, processor_params character varying) AS
 $BODY$
 BEGIN
    RETURN QUERY
@@ -9,7 +10,8 @@ BEGIN
               st.repeat_type,
               st.first_run_time,
               st.repeat_after_days,
-              st.repeat_on_month_day
+              st.repeat_on_month_day,
+              st.processor_params
         FROM scheduled_task AS st, site
         WHERE st.processor_id = _processor_id AND st.site_id=site.id
         ORDER BY st.id;
