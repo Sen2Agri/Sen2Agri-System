fdad8e21e62af126fcef20fae4e8ae3f27578b74
Alex Grosu <agrosu@c-s.ro>
2016-04-12 18:00:55 +0300

    copyright text added on cpp/h and py files

--- sen2agri-processors/Composite/MaskHandler/MaskHandlerFunctor.h
+++ sen2agri-processors/Composite/MaskHandler/MaskHandlerFunctor.h
@@ -1,3 +1,18 @@
+/*=========================================================================
+  *
+  * Program:      Sen2agri-Processors
+  * Language:     C++
+  * Copyright:    2015-2016, CS Romania, office@c-s.ro
+  * See COPYRIGHT file for details.
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+
+ =========================================================================*/
+ 
 #ifndef MASKHANDLER_H
 #define MASKHANDLER_H
 
--- sen2agri-processors/validity_checker/validity_checker.py
+++ sen2agri-processors/validity_checker/validity_checker.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+_____________________________________________________________________________
+
+   Program:      Sen2Agri-Processors
+   Language:     Python
+   Copyright:    2015-2016, CS Romania, office@c-s.ro
+   See COPYRIGHT file for details.
+   
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+_____________________________________________________________________________
+
+"""
 from __future__ import print_function
 import argparse
 import re
