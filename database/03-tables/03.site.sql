210ac921b66f6622f043df06c05859a17642d78c
Laurentiu Nicola <lnicola@dend.ro>
2016-04-19 17:28:31 +0300

    Change the site extent to multipolygon

--- database/03-tables/03.site.sql
+++ database/03-tables/03.site.sql
@@ -3,6 +3,6 @@
   id smallserial,
   name character varying NOT NULL,
   short_name character varying,
-  geog geography(polygon) NOT NULL,
+  geog geography(multipolygon) NOT NULL,
   CONSTRAINT site_pkey PRIMARY KEY (id)
 );
