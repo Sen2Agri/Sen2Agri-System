42f4ee63f61f6fb485938b429c72e36fe73f004a
Laurentiu Nicola <lnicola@dend.ro>
2016-04-29 18:04:06 +0300

    Add missing categories to the config_category table script

--- database/07-data/01.config_category.sql
+++ database/07-data/01.config_category.sql
@@ -1,34 +1,16 @@
-﻿INSERT INTO config_category
-(name, display_order)
-VALUES 
-('General', 0);
+﻿INSERT INTO config_category VALUES (8, 'Executor', 7, false);
+INSERT INTO config_category VALUES (12, 'Dashboard', 8, false);
+INSERT INTO config_category VALUES (1, 'General', 0, true);
+INSERT INTO config_category VALUES (2, 'L2A Atmospheric Corrections', 1, true);
+INSERT INTO config_category VALUES (3, 'L3A Composite', 2, true);
+INSERT INTO config_category VALUES (4, 'L3B Vegetation Status', 3, true);
+INSERT INTO config_category VALUES (5, 'L4A Crop Mask', 4, true);
+INSERT INTO config_category VALUES (6, 'L4B Crop Type', 5, true);
+INSERT INTO config_category VALUES (7, 'Archiver', 6, true);
+INSERT INTO config_category VALUES (13, 'Monitoring Agent', 9, false);
+INSERT INTO config_category VALUES (14, 'Resources', 10, false);
+INSERT INTO config_category VALUES (16, 'Demmaccs', 12, false);
+INSERT INTO config_category VALUES (15, 'Downloader', 11, true);
+INSERT INTO config_category VALUES (17, 'Site', 13, false);
 
-INSERT INTO config_category
-(name, display_order)
-VALUES 
-('L2A Atmospheric Corrections', 1);
-
-INSERT INTO config_category
-(name, display_order)
-VALUES 
-('L3A Composite', 2);
-
-INSERT INTO config_category
-(name, display_order)
-VALUES 
-('L3B Vegetation Status', 3);
-
-INSERT INTO config_category
-(name, display_order)
-VALUES 
-('L4A Crop Mask', 4);
-
-INSERT INTO config_category
-(name, display_order)
-VALUES 
-('L4B Crop Type', 5);
-
-INSERT INTO config_category
-(name, display_order)
-VALUES 
-('Archiver', 6);
+SELECT pg_catalog.setval('config_category_id_seq', 17, true);
