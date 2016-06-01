905310bd8f21314e1e65479f5e3de4547592d7e0
Alex Grosu <agrosu@c-s.ro>
2016-06-01 15:11:03 +0300

    alignment

--- python-libs/sen2agri_common_db.py
+++ python-libs/sen2agri_common_db.py
@@ -70,18 +70,17 @@ g_exit_flag = False
 
 
 def log(location, info, log_filename = ""):
-    if DEBUG:        
-        print("{}:{}".format(str(datetime.datetime.now()), str(info)))
-        sys.stdout.flush()
-    if len(location) > 0 and len(log_filename) > 0:
-        try:
+    try:
+        if DEBUG:        
+            print("{}:{}:{}".format(str(datetime.datetime.now()), os.getpid(), str(info)))
+            sys.stdout.flush()
+        if len(location) > 0 and len(log_filename) > 0:
             logfile = os.path.join(location, log_filename)
             log = open(logfile, 'a')
-            log.write("{}:{}\n".format(str(datetime.datetime.now()),str(info)))
+            log.write("{}:{}:{}\n".format(str(datetime.datetime.now()), os.getpid(), str(info)))
             log.close()
-        except:
-            print("Could NOT write inside the log file {}".format(logfile))
-            sys.stdout.flush()
+    except:
+        print("Could NOT write inside the log file {}".format(logfile))     
 
 
 def run_command(cmd_array, log_path = "", log_filename = ""):
@@ -257,19 +256,24 @@ def landsat_crop_to_cutline(landsat_product_path, working_dir):
     if(len(landsat_files) < FILES_IN_LANDSAT_L1_PRODUCT):
         return "", "Found {} files in LANDSAT product {}. Should have been {}".format(len(landsat_files), landsat_product_path, FILES_IN_LANDSAT_L1_PRODUCT)
 
-    first_tile_file_path = ""
+    first_tile_file_path = None
     tmp_shape_file = ""
     for landsat_file in landsat_files:
         landsat_file_basename = os.path.basename(landsat_file[:len(landsat_file) - 1]) if landsat_file.endswith("/") else os.path.basename(landsat_file)
         band = re.match("LC8\w{13}LGN\w{2}_B1.TIF", landsat_file_basename)
         if band is not None:
-            first_tile_file_path = landsat_file
+            # the filename for the first output tile is created only (not accessed)
+            first_tile_file_path = "{}/{}".format(aligned_landsat_directory_path, landsat_file_basename)
             out = check_output(["gdalsrsinfo", "-o", "wkt", landsat_file])
             print("out = {}".format(out))
             tmp_shape_file = "{}/tmp.shp".format(alignment_directory)
             print("tmp = {}".format(tmp_shape_file))
             #TODO: handle errors !!!!
             run_command(["ogr2ogr", "-t_srs", out, "-where", "PR={}".format(tile_id), "-overwrite", tmp_shape_file, "/usr/share/sen2agri/wrs2_descending/wrs2_descending.shp"])    
+            break
+    if not first_tile_file_path :
+	return "", "Unable to find B1 in output product"
+
     processed_files_counter = 0
     for landsat_file in landsat_files:
         landsat_file_basename = os.path.basename(landsat_file[:len(landsat_file) - 1]) if landsat_file.endswith("/") else os.path.basename(landsat_file)
@@ -279,60 +283,65 @@ def landsat_crop_to_cutline(landsat_product_path, working_dir):
             output_file = "{}/{}".format(aligned_landsat_directory_path, landsat_file_basename)
             run_command(["gdalwarp", "-overwrite", "-crop_to_cutline", "-cutline", tmp_shape_file, landsat_file, output_file])
             processed_files_counter += 1
-        else:
-            metadata = re.match("LC8\w{13}LGN\w{2}_MTL.txt", landsat_file_basename)
-            if metadata is not None:
-                output_metadata_file = "{}/{}".format(aligned_landsat_directory_path, landsat_file_basename)
-                print(output_metadata_file)
-                shape_env_points_wgs84, shape_env_points = get_footprint(first_tile_file_path)
-                try:
-                    with open(landsat_file, 'r') as genuine_metadata, open(output_metadata_file, 'w') as modified_metadata:
-                        for line in genuine_metadata:
-                            # UTM coordinates
-                            if "CORNER_UL_PROJECTION_X_PRODUCT" in line:
-                                line = "    CORNER_UL_PROJECTION_X_PRODUCT = " + str(shape_env_points[0][0]) + "\n"
-                            if "CORNER_UL_PROJECTION_Y_PRODUCT" in line:
-                                line = "    CORNER_UL_PROJECTION_Y_PRODUCT = " + str(shape_env_points[0][1]) + "\n"
-                            if "CORNER_UR_PROJECTION_X_PRODUCT" in line:
-                                line = "    CORNER_UR_PROJECTION_X_PRODUCT = " + str(shape_env_points[3][0]) + "\n"
-                            if "CORNER_UR_PROJECTION_Y_PRODUCT" in line:
-                                line = "    CORNER_UR_PROJECTION_Y_PRODUCT = " + str(shape_env_points[3][1]) + "\n"
-                            if "CORNER_LL_PROJECTION_X_PRODUCT" in line:
-                                line = "    CORNER_LL_PROJECTION_X_PRODUCT = " + str(shape_env_points[1][0]) + "\n"
-                            if "CORNER_LL_PROJECTION_Y_PRODUCT" in line:
-                                line = "    CORNER_LL_PROJECTION_Y_PRODUCT = " + str(shape_env_points[1][1]) + "\n"
-                            if "CORNER_LR_PROJECTION_X_PRODUCT" in line:
-                                line = "    CORNER_LR_PROJECTION_X_PRODUCT = " + str(shape_env_points[2][0]) + "\n"
-                            if "CORNER_LR_PROJECTION_Y_PRODUCT" in line:
-                                line = "    CORNER_LR_PROJECTION_Y_PRODUCT = " + str(shape_env_points[2][1]) + "\n"
-                            # latlong coordinates
-                            
-                            if "CORNER_UL_LAT_PRODUCT" in line:
-                                line = "    CORNER_UL_LAT_PRODUCT = " + str(shape_env_points_wgs84[0][0]) + "\n"
-                            if "CORNER_UL_LON_PRODUCT" in line:
-                                line = "    CORNER_UL_LON_PRODUCT = " + str(shape_env_points_wgs84[0][1]) + "\n"
-                            if "CORNER_UR_LAT_PRODUCT" in line:
-                                line = "    CORNER_UR_LAT_PRODUCT = " + str(shape_env_points_wgs84[3][0]) + "\n"
-                            if "CORNER_UR_LON_PRODUCT" in line:
-                                line = "    CORNER_UR_LON_PRODUCT = " + str(shape_env_points_wgs84[3][1]) + "\n"
-                            if "CORNER_LL_LAT_PRODUCT" in line:
-                                line = "    CORNER_LL_LAT_PRODUCT = " + str(shape_env_points_wgs84[1][0]) + "\n"
-                            if "CORNER_LL_LON_PRODUCT" in line:
-                                line = "    CORNER_LL_LON_PRODUCT = " + str(shape_env_points_wgs84[1][1]) + "\n"
-                            if "CORNER_LR_LAT_PRODUCT" in line:
-                                line = "    CORNER_LR_LAT_PRODUCT = " + str(shape_env_points_wgs84[2][0]) + "\n"
-                            if "CORNER_LR_LON_PRODUCT" in line:
-                                line = "    CORNER_LR_LON_PRODUCT = " + str(shape_env_points_wgs84[2][1]) + "\n"
-                            
-                            modified_metadata.write(line)
-                except EnvironmentError:
-                    return "", "Could not open the landsat metadata file for alignment or could not create the output file: Input = {} | Output = {}".format(landsat_file, output_metadata_file)
-                processed_files_counter += 1
-    print(processed_files_counter)
+
+    print("Using footprint reference: {}".format(first_tile_file_path))
+
+    for landsat_file in landsat_files:
+        landsat_file_basename = os.path.basename(landsat_file[:len(landsat_file) - 1]) if landsat_file.endswith("/") else os.path.basename(landsat_file)
+	metadata = re.match("LC8\w{13}LGN\w{2}_MTL.txt", landsat_file_basename)
+	if metadata is not None:
+	    output_metadata_file = "{}/{}".format(aligned_landsat_directory_path, landsat_file_basename)
+	    print(output_metadata_file)
+	    shape_env_points_wgs84, shape_env_points = get_footprint(first_tile_file_path)
+	    try:
+	        with open(landsat_file, 'r') as genuine_metadata, open(output_metadata_file, 'w') as modified_metadata:
+		    for line in genuine_metadata:
+		        # UTM coordinates
+		        if "CORNER_UL_PROJECTION_X_PRODUCT" in line:
+		    	    line = "    CORNER_UL_PROJECTION_X_PRODUCT = " + str(shape_env_points[0][0]) + "\n"
+		        if "CORNER_UL_PROJECTION_Y_PRODUCT" in line:
+		    	    line = "    CORNER_UL_PROJECTION_Y_PRODUCT = " + str(shape_env_points[0][1]) + "\n"
+		        if "CORNER_UR_PROJECTION_X_PRODUCT" in line:
+		    	    line = "    CORNER_UR_PROJECTION_X_PRODUCT = " + str(shape_env_points[3][0]) + "\n"
+		        if "CORNER_UR_PROJECTION_Y_PRODUCT" in line:
+		    	    line = "    CORNER_UR_PROJECTION_Y_PRODUCT = " + str(shape_env_points[3][1]) + "\n"
+		        if "CORNER_LL_PROJECTION_X_PRODUCT" in line:
+		    	    line = "    CORNER_LL_PROJECTION_X_PRODUCT = " + str(shape_env_points[1][0]) + "\n"
+		        if "CORNER_LL_PROJECTION_Y_PRODUCT" in line:
+		    	    line = "    CORNER_LL_PROJECTION_Y_PRODUCT = " + str(shape_env_points[1][1]) + "\n"
+		        if "CORNER_LR_PROJECTION_X_PRODUCT" in line:
+		    	    line = "    CORNER_LR_PROJECTION_X_PRODUCT = " + str(shape_env_points[2][0]) + "\n"
+		        if "CORNER_LR_PROJECTION_Y_PRODUCT" in line:
+		    	    line = "    CORNER_LR_PROJECTION_Y_PRODUCT = " + str(shape_env_points[2][1]) + "\n"
+		        # latlong coordinates
+		        
+		        if "CORNER_UL_LAT_PRODUCT" in line:
+		    	    line = "    CORNER_UL_LAT_PRODUCT = " + str(shape_env_points_wgs84[0][0]) + "\n"
+		        if "CORNER_UL_LON_PRODUCT" in line:
+		    	    line = "    CORNER_UL_LON_PRODUCT = " + str(shape_env_points_wgs84[0][1]) + "\n"
+		        if "CORNER_UR_LAT_PRODUCT" in line:
+		    	    line = "    CORNER_UR_LAT_PRODUCT = " + str(shape_env_points_wgs84[3][0]) + "\n"
+		        if "CORNER_UR_LON_PRODUCT" in line:
+		    	    line = "    CORNER_UR_LON_PRODUCT = " + str(shape_env_points_wgs84[3][1]) + "\n"
+		        if "CORNER_LL_LAT_PRODUCT" in line:
+		    	    line = "    CORNER_LL_LAT_PRODUCT = " + str(shape_env_points_wgs84[1][0]) + "\n"
+		        if "CORNER_LL_LON_PRODUCT" in line:
+		    	    line = "    CORNER_LL_LON_PRODUCT = " + str(shape_env_points_wgs84[1][1]) + "\n"
+		        if "CORNER_LR_LAT_PRODUCT" in line:
+		    	    line = "    CORNER_LR_LAT_PRODUCT = " + str(shape_env_points_wgs84[2][0]) + "\n"
+		        if "CORNER_LR_LON_PRODUCT" in line:
+		    	    line = "    CORNER_LR_LON_PRODUCT = " + str(shape_env_points_wgs84[2][1]) + "\n"
+		        
+		        modified_metadata.write(line)
+	    except EnvironmentError:
+	        return "", "Could not open the landsat metadata file for alignment or could not create the output file: Input = {} | Output = {}".format(landsat_file, output_metadata_file)
+	    processed_files_counter += 1
     if(processed_files_counter != FILES_IN_LANDSAT_L1_PRODUCT):
         return "", "The number of processed files in LANDSAT alignment is {} which is different than how many the should have been: {}".format(processed_files_counter, FILES_IN_LANDSAT_L1_PRODUCT)
     #all went ok, return the path for the aligned product
     return aligned_landsat_directory_path, ""
+
+
 ###########################################################################
 class OptionParser (optparse.OptionParser):
 
