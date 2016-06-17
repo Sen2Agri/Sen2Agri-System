c055fc2756f98c4bee6f91fbcd9149e14752f70c
Laurentiu Nicola <lnicola@dend.ro>
2016-06-17 15:15:23 +0300

    Add -ram 4096 to PCA step

--- scripts/CropMask.py
+++ scripts/CropMask.py
@@ -276,7 +276,7 @@ try:
 #Dimension reduction (Step 26)
     executeStep("PrincipalComponentAnalysis", "otbcli",
             "PrincipalComponentAnalysis",buildFolder
-            ,"-ndvi", rndvi, "-nc", nbcomp, "-out", pca, skip=fromstep>26, rmfiles=[] if keepfiles else [ndvi])
+            ,"-ndvi", rndvi, "-nc", nbcomp, "-out", pca, "-ram", "4096", skip=fromstep>26, rmfiles=[] if keepfiles else [ndvi])
 
 #Mean-Shift segmentation (Step 27, 28 and 29)
     executeStep("MeanShiftSmoothing", "otbcli_MeanShiftSmoothing", "-in", pca,"-modesearch","0", "-spatialr", spatialr, "-ranger", ranger, "-maxiter", "20", "-foutpos", mean_shift_smoothing_spatial, "-fout", mean_shift_smoothing, skip=fromstep>27, rmfiles=[] if keepfiles else [pca])
