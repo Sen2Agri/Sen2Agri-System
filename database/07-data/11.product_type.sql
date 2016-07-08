c8f6b9d6ac91cf8c0cc2bedba0303f27d823787c
Laurentiu Nicola <lnicola@dend.ro>
2016-07-08 13:39:23 +0300

    Revert to the old numbering of product types

--- database/07-data/11.product_type.sql
+++ database/07-data/11.product_type.sql
@@ -1,45 +1,45 @@
 ﻿INSERT INTO product_type
 (id, name, description)
 VALUES
-(1, 'l1c','L1C product');
+(7, 'l1c','L1C product');
 
 INSERT INTO product_type
 (id, name, description)
 VALUES
-(2, 'l2a','L2A Atmosferic corrections');
+(1, 'l2a','L2A Atmosferic corrections');
 
 INSERT INTO product_type
 (id, name, description)
 VALUES
-(3, 'l3a','L3A Composite product');
+(2, 'l3a','L3A Composite product');
 
 INSERT INTO product_type
 (id, name, description)
 VALUES
-(4, 'l3b_lai_monodate','L3B LAI mono-date product');
+(3, 'l3b_lai_monodate','L3B LAI mono-date product');
 
 INSERT INTO product_type
 (id, name, description)
 VALUES
-(5, 'l3c_lai_reproc','L3C LAI Reprocessed product');
+(8, 'l3c_lai_reproc','L3C LAI Reprocessed product');
 
 INSERT INTO product_type
 (id, name, description)
 VALUES
-(6, 'l3d_lai_fitted','L3D LAI End of Season product');
+(9, 'l3d_lai_fitted','L3D LAI End of Season product');
 
 INSERT INTO product_type
 (id, name, description)
 VALUES
-(7, 'l3e_pheno','L3E Pheno NDVI product');
+(4, 'l3e_pheno','L3E Pheno NDVI product');
 
 INSERT INTO product_type
 (id, name, description)
 VALUES
-(8, 'l4a','L4A Crop mask product');
+(5, 'l4a','L4A Crop mask product');
 
 INSERT INTO product_type
 (id, name, description)
 VALUES
-(9, 'l4b','L4B Crop type product');
+(6, 'l4b','L4B Crop type product');
 
