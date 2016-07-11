50a7689e9d84b162129854ea78c338ed9ef12f4d
Nicu Stancioi <nicu@c-s.ro>
2016-07-11 15:59:01 +0300

    Added Jobs history in "Monitoring" panel

--- sen2agri-dashboard/monitoring.php
+++ sen2agri-dashboard/monitoring.php
@@ -1,12 +1,10 @@
 <?php include 'master.php'; ?>
-<?php include 'getHistoryDownloads.php'; ?>
-<?php include 'getHistoryDownloadsCurrent.php'; ?>
 <?php
 if (! (empty ( $_SESSION ['siteId'] ))) { // not admin
 		header("Location: monitoring_user.php");
 		exit();
 	}
-	
+
 function select_option() {
 	if (empty ( $_SESSION ['siteId'] )) { 
 		 // if admin
@@ -31,31 +29,48 @@ function statistics_admin(){
 	$table="";
 	while ( $row = pg_fetch_row ( $result_downloads ) ) {
 
-		$td = "<td colspan=\"2\">" . $row [1] . "</td>";
-		$table= $table.$td;
+		$td = "<td>" . $row [1] . "</td>";
+		$table = $table . $td;
 	}
 	echo $table;
 }
 
 function current_downloads_admin(){
-		$db = pg_connect ( ConfigParams::$CONN_STRING ) or die ( "Could not connect" );
-		$result="";
-		$result = pg_query_params ( $db, "SELECT * FROM sp_get_dashboard_current_downloads(null)", array () ) or die ( "Could not execute." );
-		
-		$tr_current ="";
-		while ( $row = pg_fetch_row ( $result ) ) {
-			$tr = "<tr>".
-					"<td colspan=\"2\">". $row[0]."</td>".
-					"<td colspan=\"2\">". $row[1]."</td>".
-					"<td colspan=\"2\">". $row[2]."</td>".
-					"<td colspan=\"2\">". $row[3]."</td>".
-					"</tr>";
-			$tr_current = $tr_current . $tr;
-		}
-		echo $tr_current;
+	$db = pg_connect ( ConfigParams::$CONN_STRING ) or die ( "Could not connect" );
+	$result="";
+	$result = pg_query_params ( $db, "SELECT * FROM sp_get_dashboard_current_downloads(null)", array () ) or die ( "Could not execute." );
+	
+	$tr_current ="";
+	while ( $row = pg_fetch_row ( $result ) ) {
+		$tr = "<tr>".
+				"<td>". $row[0]."</td>".
+				"<td>". $row[1]."</td>".
+				"<td>". $row[2]."</td>".
+				"<td>". $row[3]."</td>".
+			"</tr>";
+		$tr_current = $tr_current . $tr;
 	}
-
-
+	echo $tr_current;
+}
+function get_history_jobs_admin(){
+	$db = pg_connect ( ConfigParams::$CONN_STRING ) or die ( "Could not connect" );
+	$result="";
+	$result = pg_query_params ( $db, "SELECT * FROM sp_get_job_history(null, 1)", array () ) or die ( "Could not execute." );
+	
+	$tr_current ="";
+	while ( $row = pg_fetch_row ( $result ) ) {
+		$tr = "<tr>".
+				"<td>". $row[0]."</td>".
+				"<td>". $row[1]."</td>".
+				"<td>". $row[2]."</td>".
+				"<td>". $row[3]."</td>".
+				"<td>". $row[4]."</td>".
+				"<td>". $row[5]."</td>".
+			"</tr>";
+		$tr_current = $tr_current . $tr;
+	}
+	echo $tr_current;
+}
 ?>
 <div id="main">
 	<div id="main2">
@@ -64,75 +79,94 @@ function current_downloads_admin(){
 			<div class="panel panel-default">
 				<div class="panel-heading">Monitoring</div>
 				<div class="panel-body">
-
+					
 					<div class="row">
 						<div class="col-md-2">
 							<!-- select site -->
 							<div class="form-group form-group-sm">
 								<select class="form-control" name="site_select"
-								onchange="siteInfo(this.value);siteInfoCurrent(this.value);">
-									<!-- get sites -->
-									<?php   select_option();?>	
-							</select>
+										onchange="siteInfo(this.value); siteInfoCurrent(this.value); siteJobs(this.value, 1);">
+									<?php select_option();?>
+								</select>
 							</div>
 							<!-- end select site -->
 						</div>
 					</div>
-
+					
 					<!-------------- Download statistics ---------------->
 						<div class="panel panel-default">
 							<div class="panel-heading">Download statistics</div>
-							<table class="table full_width"">
+							<table class="table full_width">
 								<thead>
 									<tr>
-										<th colspan="2">Downloads in progress</th>
-										<th colspan="2">Successful downloads</th>
-										<th colspan="2">Failed downloads</th>
+										<th>Downloads in progress</th>
+										<th>Successful downloads</th>
+										<th>Failed downloads</th>
 									</tr>
 								</thead>
-							<!--  <tbody >-->
-					
 								<tbody >
-								<tr id="refresh_statistics">
-								<?php statistics_admin();?>
-									<!-- code from getHistoryDownloads.php comes here --></tr>
-							
-									<tr>
-									<td colspan="2"></td>
-									<td colspan="2"></td>
-									<td colspan="2"></td></tr>
-										</tbody>								
+								
+									<tr id="refresh_statistics">
+										<?php statistics_admin();?>
+									</tr>
+									
+								</tbody>								
 							</table>
 						</div>
 						<!-------------- End Download statistics ---------------->
-
+						
 						<!-------------- Current processing details ---------------->
 						<div class="panel panel-default">
 							<div class="panel-heading">Current downloads</div>
-								<div class="panel-body">
+							<div class="panel-body">
 								<table class="table full_width" style="text-align: left">
 									<thead>
 										<tr>
-											<th colspan="2">Site</th>
-											<th colspan="2">Product</th>
-											<th colspan="2">Product Type</th>
-											<th colspan="2">Status modified</th>
+											<th>Site</th>
+											<th>Product</th>
+											<th>Product Type</th>
+											<th>Status modified</th>
 										</tr>
 									</thead>
-									 <tbody  id="refresh_downloading">
-									 <?php current_downloads_admin();?>
-									 <!-- code from getHistoryDownloadsCurrent.php  -->
-									 </tbody>
-
+									<tbody id="refresh_downloading">
+										<?php current_downloads_admin();?>
+									</tbody>
 								</table>
 							</div>
 						</div>
 						<!-------------- Current processing details ---------------->
-
-
+						
+						<!-------------- Job history ---------------->
+						<div class="panel panel-default">
+							<div class="panel-heading">Current downloads</div>
+							<div style="float: right; padding: 10px;">
+								<button id="page_move_first" type="button" class="btn btn-default" onclick="move_to_first_jobs_page();">&lt;&lt;</button>
+								<button id="page_move_prev"  type="button" class="btn btn-default" onclick="move_to_previous_jobs_page();">Previous Page</button>
+								<button id="page_current"    type="button" class="btn btn-default" style="font-weight: 700;">1</button>
+								<button id="page_move_next"  type="button" class="btn btn-default" onclick="move_to_next_jobs_page()">Next Page</button>
+								<button id="page_move_last"  type="button" class="btn btn-default disabled" onclick="move_to_last_jobs_page();">&gt;&gt;</button>
+							</div>
+							<div class="panel-body">
+								<table class="table full_width" style="text-align: left" id="history_jobs">
+									<thead>
+										<tr>
+											<th>Job ID</th>
+											<th>End timestamp</th>
+											<th>Processor</th>
+											<th>Site</th>
+											<th>Status</th>
+											<th>Start type</th>
+										</tr>
+									</thead>
+									<tbody id="refresh_jobs">
+										<?php get_history_jobs_admin();?>
+									</tbody>
+								</table>
+							</div>
+						</div>
+						<!-------------- Job history ---------------->
 				</div>
 			</div>
-
 			<!-- main -->
 		</div>
 	</div>
@@ -141,6 +175,7 @@ function current_downloads_admin(){
 <script type="text/javascript">
 var timer1;
 var timer2;
+var timer3;
 
 function siteInfo(id) {
 	$.ajax({
@@ -168,6 +203,67 @@ function siteInfoCurrent(id) {
         }
     });
 }
+
+function siteJobs(id, page_no) {
+	jsonJobsPage = page_no;
+	$.ajax({
+        type: "post",
+        url: "getHistoryJobs.php",
+		data:  { 'siteID_selected': id, 'page': page_no }, 
+        success: function(result) {
+        	$("#refresh_jobs").html(result);
+        	
+			// display page number
+			$("#page_current").html(page_no);
+			// toggle move_next button availability
+			if ($("#history_jobs").children('tbody').children('tr').length < 20) {
+				$("#page_move_next").addClass("disabled");
+			} else {
+				$("#page_move_next").removeClass("disabled");
+			}
+			
+			// Schedule the next request
+     		clearTimeout(timer3);
+        	timer3 =  setTimeout(siteJobs, 5000, id, page_no);
+        }
+    });
+}
+
+function move_to_first_jobs_page()
+{
+	id = $('select[name=site_select] option:selected').val();
+	jsonJobsPage = 1;
+	siteJobs(id, jsonJobsPage);
+}
+
+function move_to_previous_jobs_page()
+{
+	if (jsonJobsPage > 1) {
+		id = $('select[name=site_select] option:selected').val();
+		jsonJobsPage --;
+		jsonJobsPage = jsonJobsPage < 1 ? 1 : jsonJobsPage;
+		siteJobs(id, jsonJobsPage);
+	} else {
+		jsonJobsPage = 1;
+	}
+}
+
+function move_to_next_jobs_page()
+{
+	if (!$("#page_move_next").hasClass("disabled")) {
+		id = $('select[name=site_select] option:selected').val();
+		jsonJobsPage ++;
+		siteJobs(id, jsonJobsPage);
+	}
+}
+$( document ).ready(function() {
+	// toggle move_next button availability
+	if ($("#history_jobs").children('tbody').children('tr').length < 20) {
+		$("#page_move_next").addClass("disabled");
+	} else {
+		$("#page_move_next").removeClass("disabled");
+	}
+});
 </script>
 
 <?php include 'ms_foot.php'; ?>
