afdb80b362f2ed99b077309f2c34dead3f580ca0
Laurentiu Nicola <lnicola@dend.ro>
2016-07-12 17:29:48 +0300

    Fix smallserial usage in scheduled_task and scheduled_task_status and add not null constraints

--- database/03-tables/111.scheduled_tasks.sql
+++ database/03-tables/111.scheduled_tasks.sql
@@ -1,28 +1,28 @@
 ﻿CREATE TABLE scheduled_task
 (
-  id smallserial,
+  id smallserial NOT NULL,
   name character varying NOT NULL,
-  processor_id smallserial,
-  site_id smallserial,
+  processor_id smallint NOT NULL,
+  site_id smallint NOT NULL,
   processor_params character varying,
 
   repeat_type smallint,
   repeat_after_days smallint,
   repeat_on_month_day smallint,
-  
+
   retry_seconds integer,
 
   priority smallint,
-  
+
   first_run_time character varying,
-    
+
   CONSTRAINT scheduled_task_pkey PRIMARY KEY (id)
 );
 
 CREATE TABLE scheduled_task_status
 (
-  id smallserial,
-  task_id smallserial,
+  id smallserial NOT NULL,
+  task_id smallint NOT NULL,
 
   next_schedule character varying,
 
@@ -34,4 +34,3 @@ CREATE TABLE scheduled_task_status
 
   CONSTRAINT scheduled_task_status_pkey PRIMARY KEY (id)
 );
-
