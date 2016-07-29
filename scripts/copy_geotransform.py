6f830331f9ccc28bc450ac67f03e8e9658dea7e6
Laurentiu Nicola <lnicola@dend.ro>
2016-07-29 17:26:33 +0300

    Add a script to copy the GeoTransform from one file to another

---
+++ scripts/copy_geotransform.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import argparse
+import gdal
+
+parser = argparse.ArgumentParser(description="Copies the geotransform from one file to another")
+parser.add_argument('source', help="Source dataset")
+parser.add_argument('target', help="Target dataset")
+
+args = parser.parse_args()
+
+source_ds = gdal.Open(args.source, gdal.gdalconst.GA_ReadOnly)
+target_ds = gdal.Open(args.target, gdal.gdalconst.GA_Update)
+
+if not source_ds:
+    raise Exception("Could not open source dataset", args.source)
+if not target_ds:
+    raise Exception("Could not open target dataset", args.target)
+
+target_ds.SetGeoTransform(source_ds.GetGeoTransform())
+target_ds.Update()
