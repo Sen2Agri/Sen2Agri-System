4cbb7a08dedc207bd4ce41f0b23f91b3d8d032b5
Laurentiu Nicola <lnicola@dend.ro>
2016-10-05 15:56:10 +0300

    Stop the daemons before upgrading

--- install_platform/update.sh
+++ install_platform/update.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+systemctl stop sen2agri-orchestrator sen2agri-http-listener sen2agri-sentinel-downloader sen2agri-landsat-downloader sen2agri-monitor-agent
+
 rpm -Uvh --force ../rpm_binaries/*.rpm
 
 for file in migrations/*.sql
 do
     cat "$file" | sudo -u postgres psql sen2agri
 done
+
+systemctl start sen2agri-orchestrator sen2agri-http-listener sen2agri-sentinel-downloader sen2agri-landsat-downloader sen2agri-monitor-agent
