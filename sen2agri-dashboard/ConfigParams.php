acf28df2f5c81e9f90a7dc673a7908ea6b7ee4a1
Laurentiu Nicola <lnicola@dend.ro>
2016-10-06 10:45:56 +0300

    Fix whitespace

--- sen2agri-dashboard/ConfigParams.php
+++ sen2agri-dashboard/ConfigParams.php
@@ -1,10 +1,10 @@
 <?php
 class ConfigParams {
     static $CONN_STRING  = 'host=localhost port=5432 dbname=sen2agri user=admin password=sen2agri';
-	static $SERVICES_URL = 'http://localhost:8080/dashboard';
+    static $SERVICES_URL = 'http://localhost:8080/dashboard';
     static $SITE_ID;
     static $USER_NAME;
-	
+
     static function init() {
         // set login information
         if (isset($_SESSION['siteId']) && isset($_SESSION['userId']) && isset($_SESSION['userName'])) {
