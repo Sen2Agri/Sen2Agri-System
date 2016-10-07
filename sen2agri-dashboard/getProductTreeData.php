117b8c5faa662295261f83d45d64f139647d40d0
Laurentiu Nicola <lnicola@dend.ro>
2016-10-07 17:18:40 +0300

    Optimize product list creation

--- sen2agri-dashboard/getProductTreeData.php
+++ sen2agri-dashboard/getProductTreeData.php
@@ -7,7 +7,7 @@ function findSite($siteName, $siteObjArr) {
 		if ($siteObj->text == $siteName)
 			return $siteObj;
 	}
-	
+
 	return null;
 }
 
@@ -16,7 +16,7 @@ function findProcessor($processorName, $processorObjArr) {
 		if ($processorObj->text == $processorName)
 			return $processorObj;
 	}
-	
+
 	return null;
 }
 
@@ -25,7 +25,7 @@ function findProdType($prodTypeName, $prodTypeObjArr) {
 		if ($prodTypeObj->text == $prodTypeName)
 			return $prodTypeObj;
 	}
-	
+
 	return null;
 }
 
@@ -35,15 +35,6 @@ function normalizePath($path) {
     return $path;
 }
 
-function getProductImageSize($file) {
-	if (file_exists($file)) {
-		$image_info = getImageSize($file);
-		return array('width'=>$image_info[0], 'height'=>$image_info[1]);
-	} else {
-		return array('width'=>null, 'height'=>null);
-	}
-}
-
 $products = array();
 
 try {
@@ -51,7 +42,7 @@ try {
 	$rows         = pg_query($dbconn, "select * from sp_get_dashboard_products(".ConfigParams::$SITE_ID.")") or die(pg_last_error());
 	$responseJson = pg_numrows($rows) > 0 ? pg_fetch_array($rows, 0)[0] : "";
 	$productRows  = json_decode($responseJson);
-	
+
     foreach($productRows as $productRow) {
         $siteObj = findSite($productRow->site, $products);
 		if ($siteObj == null) {
@@ -59,9 +50,9 @@ try {
 			$siteObj->text = $productRow->site;
 			$siteObj->selectable = false;
 			$siteObj->nodes = array();
-			$products[] = $siteObj;			
+			$products[] = $siteObj;
 		}
-		
+
 		$prodTypeObj = findProdType($productRow->product_type_description, $siteObj->nodes);
 		if ($prodTypeObj == null) {
 			$prodTypeObj = new stdClass();
@@ -69,8 +60,8 @@ try {
 			$prodTypeObj->selectable = false;
 			$prodTypeObj->nodes = array();
 			$siteObj->nodes[] = $prodTypeObj;
-		}		
-		
+		}
+
 		if ($productRow->product != null) {
 			$productObj = new stdClass();
 			$productObj->productId = $productRow->product;
@@ -78,17 +69,14 @@ try {
 			$productObj->href = "downloadProduct.php?id=".$productRow->id;
 			$productObj->productCoord = array_map('floatval', explode(",", str_replace(array("(", ")"), "", $productRow->footprint)));
 			$productObj->siteCoord = $productRow->site_coord;
-			
+
 			$productObj->productImageUrl = "getProductImage.php?id=".$productRow->id;
-			$imageSize = getProductImageSize(rtrim(normalizePath($productRow->full_path), '/').'/'.$productRow->quicklook_image);
-			$productObj->productImageWidth = $imageSize['width'];
-			$productObj->productImageHeight = $imageSize['height'];
-			
+
 			$prodTypeObj->nodes[] = $productObj;
 		}
-		
+
     }
-	
+
 } catch (Exception $e) {
     $products = null;
 }
