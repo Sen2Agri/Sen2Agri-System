54998131e3753877770069c3dbb977d6bef35ff8
Laurentiu Nicola <lnicola@dend.ro>
2016-10-07 17:16:11 +0300

    Update migration script

--- install_platform/migrations/migration-1.3-1.3.1.sql
+++ install_platform/migrations/migration-1.3-1.3.1.sql
@@ -137,6 +137,159 @@ begin
         raise notice 'update config set value=''resample'' where key in (''processor.l4a.temporal_resampling_mode'', ''processor.l4b.temporal_resampling_mode'');';
         update config set value='resample' where key in ('processor.l4a.temporal_resampling_mode', 'processor.l4b.temporal_resampling_mode');
 
+        -- bc8471f8275052acf1a0812861b967a4c0a60156
+        raise notice 'applying bc8471f8275052acf1a0812861b967a4c0a60156';
+        raise notice 'create or replace function sp_get_products( [...] );';
+        CREATE OR REPLACE FUNCTION sp_get_products(
+            IN site_id smallint DEFAULT NULL::smallint,
+            IN product_type_id smallint DEFAULT NULL::smallint,
+            IN start_time timestamp with time zone DEFAULT NULL::timestamp with time zone,
+            IN end_time timestamp with time zone DEFAULT NULL::timestamp with time zone)
+        RETURNS TABLE("ProductId" integer, "Product" character varying, "ProductType" character varying, "ProductTypeId" smallint, "Processor" character varying,
+                        "ProcessorId" smallint, "Site" character varying, "SiteId" smallint, full_path character varying,
+                        quicklook_image character varying, footprint polygon, created_timestamp timestamp with time zone) AS
+        $BODY$
+        DECLARE q text;
+        BEGIN
+            q := $sql$
+            WITH site_names(id, name, row) AS (
+                    select id, name, row_number() over (order by name)
+                    from site
+                ),
+                product_type_names(id, name, row) AS (
+                    select id, name, row_number() over (order by name)
+                    from product_type
+                )
+                SELECT P.id AS ProductId,
+                    P.name AS Product,
+                    PT.name AS ProductType,
+                    P.product_type_id AS ProductTypeId,
+                    PR.name AS Processor,
+                    P.processor_id AS ProcessorId,
+                    S.name AS Site,
+                    P.site_id AS SiteId,
+                    P.full_path,
+                    P.quicklook_image,
+                    P.footprint,
+                    P.created_timestamp
+                FROM product P
+                    JOIN product_type_names PT ON P.product_type_id = PT.id
+                    JOIN processor PR ON P.processor_id = PR.id
+                    JOIN site_names S ON P.site_id = S.id
+                WHERE TRUE$sql$;
+
+            IF NULLIF($1, -1) IS NOT NULL THEN
+                q := q || $sql$
+                    AND P.site_id = $1$sql$;
+            END IF;
+            IF NULLIF($2, -1) IS NOT NULL THEN
+                q := q || $sql$
+                    AND P.product_type_id = $2$sql$;
+            END IF;
+            IF $3 IS NOT NULL THEN
+                q := q || $sql$
+                    AND P.created_timestamp >= $3$sql$;
+            END IF;
+            IF $4 IS NOT NULL THEN
+                q := q || $sql$
+                    AND P.created_timestamp <= $4$sql$;
+            END IF;
+            q := q || $SQL$
+                ORDER BY S.row, PT.row, P.name;$SQL$;
+
+            -- raise notice '%', q;
+
+            RETURN QUERY
+                EXECUTE q
+                USING $1, $2, $3, $4;
+        END
+        $BODY$
+        LANGUAGE plpgsql STABLE;
+
+        raise notice 'DROP FUNCTION sp_get_dashboard_products(smallint, smallint);';
+        DROP FUNCTION sp_get_dashboard_products(smallint, smallint);
+
+        raise notice 'create or replace function sp_get_dashboard_products( [...] );';
+        -- Function: sp_get_dashboard_products(smallint, smallint)
+
+        -- DROP FUNCTION sp_get_dashboard_products(smallint, smallint);
+
+        CREATE OR REPLACE FUNCTION sp_get_dashboard_products(
+            site_id smallint DEFAULT NULL::smallint,
+            processor_id smallint DEFAULT NULL::smallint)
+        RETURNS TABLE (
+        --     id product.id%type,
+        --     satellite_id product.satellite_id%type,
+        --     product product.name%type,
+        --     product_type product_type.name%type,
+        --     product_type_description product_type.description%type,
+        --     processor processor.name%type,
+        --     site site.name%type,
+        --     full_path product.full_path%type,
+        --     quicklook_image product.quicklook_image%type,
+        --     footprint product.footprint%type,
+        --     created_timestamp product.created_timestamp%type,
+        --     site_coord text
+            json json
+        ) AS
+        $BODY$
+        DECLARE q text;
+        BEGIN
+            q := $sql$
+                WITH site_names(id, name, geog, row) AS (
+                        select id, name, st_astext(geog), row_number() over (order by name)
+                        from site
+                    ),
+                    product_type_names(id, name, description, row) AS (
+                        select id, name, description, row_number() over (order by description)
+                        from product_type
+                    ),
+                    data(id, satellite_id, product,product_type,product_type_description,processor,site,full_path,quicklook_image,footprint,created_timestamp, site_coord) AS (
+                    SELECT
+                        P.id,
+                        P.satellite_id,
+                        P.name,
+                        PT.name,
+                        PT.description,
+                        PR.name,
+                        S.name,
+                        P.full_path,
+                        P.quicklook_image,
+                        P.footprint,
+                        P.created_timestamp,
+                        S.geog
+                    FROM product P
+                        JOIN product_type_names PT ON P.product_type_id = PT.id
+                        JOIN processor PR ON P.processor_id = PR.id
+                        JOIN site_names S ON P.site_id = S.id
+                    WHERE TRUE -- COALESCE(P.is_archived, FALSE) = FALSE
+            $sql$;
+            IF $1 IS NOT NULL THEN
+                q := q || 'AND P.site_id = $1';
+            END IF;
+            IF $2 IS NOT NULL THEN
+                q := q || 'AND P.product_type_id = $2';
+            END IF;
+
+            q := q || $sql$
+                    ORDER BY S.row, PT.row, P.name
+                )
+        --         select * from data;
+                SELECT array_to_json(array_agg(row_to_json(data)), true) FROM data;
+            $sql$;
+
+        --     raise notice '%', q;
+
+            RETURN QUERY
+            EXECUTE q
+            USING $1, $2;
+        END
+        $BODY$
+        LANGUAGE plpgsql STABLE
+        COST 100;
+        ALTER FUNCTION sp_get_dashboard_products(smallint, smallint)
+        OWNER TO admin;
+
         raise notice 'insert into meta(version) values(''1.3.1'');';
         insert into meta(version) values('1.3.1');
 
