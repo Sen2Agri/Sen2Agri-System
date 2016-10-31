ee71f39941b67ec502b666466b2f3f61a315082b
Laurentiu Nicola <lnicola@dend.ro>
2016-10-31 17:57:12 +0200

    Fix migration for processor.l4a.lut_path

--- install_platform/migrations/migration-1.3.1-1.4.sql
+++ install_platform/migrations/migration-1.3.1-1.4.sql
@@ -9,8 +9,8 @@ begin
             raise notice 'upgrading from 1.3.1 to 1.4';
 
             raise notice 'applying d60950703ca3989963cae1bc2bbeea10a569225a';
-            raise notice 'update config set value = ''/usr/share/sen2agri/crop-mask.map'' where key = ''processor.l4a.lut_path'';';
-            update config set value = '/usr/share/sen2agri/crop-mask.map' where key = 'processor.l4a.lut_path';
+            raise notice 'update config set value = ''/usr/share/sen2agri/crop-mask.lut'' where key = ''processor.l4a.lut_path'';';
+            update config set value = '/usr/share/sen2agri/crop-mask.lut' where key = 'processor.l4a.lut_path';
 
             raise notice 'applying f44ceba163217ae7612d6167ef549cc79fcd7ad8';
             raise notice 'update config set value = ''1'' where key = ''processor.l3a.generate_20m_s2_resolution'';';
