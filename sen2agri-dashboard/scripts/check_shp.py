af5828d3743d96f83187756fb27b65fc055c65fb
Laurentiu Nicola <lnicola@dend.ro>
2016-12-09 14:37:17 +0200

    Implement fixing of bad polygons

--- sen2agri-dashboard/scripts/check_shp.py
+++ sen2agri-dashboard/scripts/check_shp.py
@@ -2,7 +2,6 @@
 from __future__ import print_function
 
 import argparse
-import itertools
 import sys
 from osgeo import ogr
 from osgeo import osr
@@ -11,12 +10,15 @@ from osgeo import osr
 def main():
     parser = argparse.ArgumentParser(
         description="Checks the validity of vector data.")
-    parser.add_argument('-b', '--bounds', help="display input bounds", action='store_true')
+    parser.add_argument(
+        '-b', '--bounds', help="display input bounds", action='store_true')
+    parser.add_argument(
+        '-f', '--fix', help="try to fix dataset", action='store_true')
     parser.add_argument('input', help="the input data source")
 
     args = parser.parse_args()
 
-    dataSource = ogr.Open(args.input, 0)
+    dataSource = ogr.Open(args.input, 1 if args.fix else 0)
 
     if dataSource is None:
         print("Could not open {}".format(args.input))
@@ -41,17 +43,33 @@ def main():
     if args.bounds and featureCount != 1:
         union = ogr.Geometry(ogr.wkbMultiPolygon)
 
+    status = 0
     features = []
     for feature in layer:
         geometry = feature.GetGeometryRef()
         if not geometry.IsValid():
             if id_field_idx is not None:
                 feature_id = feature.GetField(id_field_idx)
-                print("Invalid feature (ID={}): {}".format(feature_id, geometry.ExportToWkt()))
+                print("Invalid feature: ID={}".format(feature_id))
             else:
                 print("Invalid feature: {}".format(geometry.ExportToWkt()))
 
-            return 2
+            bad = True
+            if args.fix:
+                geometry = geometry.Buffer(0)
+                if geometry.IsValid():
+                    feature.SetGeometryDirectly(geometry)
+                    layer.SetFeature(feature)
+
+                    if id_field_idx is not None:
+                        print("Fixed feature: ID={}".format(feature_id))
+                    else:
+                        print("Fixed feature")
+
+                    bad = False
+
+            if bad:
+                status = 2
 
         features.append(feature)
 
@@ -70,9 +88,10 @@ def main():
             layer.SetSpatialFilter(g0)
             for f1 in layer:
                 g1 = f1.GetGeometryRef()
-                if g0.Overlaps(g1):
+                if g0.Overlaps(g1) and g0.Intersection(g1).Area() > 0:
                     if code_field_idx is not None:
-                        c0, c1 = f0.GetField(code_field_idx), f1.GetField(code_field_idx)
+                        c0, c1 = f0.GetField(
+                            code_field_idx), f1.GetField(code_field_idx)
                     else:
                         c0, c1 = None, None
 
@@ -80,14 +99,37 @@ def main():
                         if id_field_idx is not None:
                             i0 = f0.GetField(id_field_idx)
                             i1 = f1.GetField(id_field_idx)
-                            print("Overlapping features (ID={} and ID={}): {} and {}".format(i0, i1, g0, g1))
+                            if i1 > i0:
+                                continue
+
+                            print(
+                                "Overlapping features: ID={} and ID={}".format(i0, i1))
                         else:
                             print("Overlapping features: {} and {}".format(g0, g1))
-                        return 3
+
+                        bad = True
+                        if args.fix:
+                            g0, g1 = g0.Difference(g1), g1.Difference(g0)
+                            f0.SetGeometryDirectly(g0)
+                            f1.SetGeometryDirectly(g1)
+
+                            layer.SetFeature(f0)
+                            layer.SetFeature(f1)
+
+                            print("Fixed features")
+                            bad = False
+
+                        if bad and status == 0:
+                            status = 3
 
     if args.bounds:
         print("Union: {}".format(union.ExportToWkt()))
-    return 0
+
+    if args.fix:
+        layer.SyncToDisk()
+
+    return status
+
 
 if __name__ == '__main__':
     sys.exit(main())
