7d79b89f94d871d97d3b9057f5c6cf55fb88eafc
Cosmin Udroiu <cosmin.udroiu@c-s.ro>
2016-12-22 12:27:19 +0200

    Added truncation of scheduled tasks during stopping all jobs.

--- scripts/stop_all_jobs/stop_all_jobs.sh
+++ scripts/stop_all_jobs/stop_all_jobs.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo "Truncating tables: step task event ..."
+echo "Truncating scheduler tables: scheduled_task and scheduled_task_status ..."
+echo "TRUNCATE scheduled_task" | sudo su - postgres -c 'psql -d sen2agri'
+echo "TRUNCATE scheduled_task_status" | sudo su - postgres -c 'psql -d sen2agri'
+
+echo "Truncating job tables: step task event ..."
 echo "TRUNCATE step" | sudo su - postgres -c 'psql -d sen2agri'
 echo "TRUNCATE task" | sudo su - postgres -c 'psql -d sen2agri'
 echo "TRUNCATE event" | sudo su - postgres -c 'psql -d sen2agri'
