b58325ed375f5dd8fed2070ad08a3945a30eae72
Laurentiu Nicola <lnicola@dend.ro>
2017-01-26 10:30:53 +0200

    Stop and restart sen2agri-scheduler on update

--- install_platform/update.sh
+++ install_platform/update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-systemctl stop sen2agri-executor sen2agri-orchestrator sen2agri-http-listener sen2agri-sentinel-downloader sen2agri-landsat-downloader sen2agri-demmaccs sen2agri-sentinel-downloader.timer sen2agri-landsat-downloader.timer sen2agri-demmaccs.timer sen2agri-monitor-agent
+systemctl stop sen2agri-scheduler sen2agri-executor sen2agri-orchestrator sen2agri-http-listener sen2agri-sentinel-downloader sen2agri-landsat-downloader sen2agri-demmaccs sen2agri-sentinel-downloader.timer sen2agri-landsat-downloader.timer sen2agri-demmaccs.timer sen2agri-monitor-agent
 
 yum -y install ../rpm_binaries/*.rpm
 
@@ -10,4 +10,4 @@ cat migrations/migration-1.4-1.5.sql | su -l postgres -c "psql sen2agri"
 
 systemctl daemon-reload
 
-systemctl start sen2agri-executor sen2agri-orchestrator sen2agri-http-listener sen2agri-sentinel-downloader sen2agri-landsat-downloader sen2agri-demmaccs sen2agri-sentinel-downloader.timer sen2agri-landsat-downloader.timer sen2agri-demmaccs.timer sen2agri-monitor-agent
+systemctl start sen2agri-executor sen2agri-orchestrator sen2agri-http-listener sen2agri-sentinel-downloader sen2agri-landsat-downloader sen2agri-demmaccs sen2agri-sentinel-downloader.timer sen2agri-landsat-downloader.timer sen2agri-demmaccs.timer sen2agri-monitor-agent sen2agri-scheduler
