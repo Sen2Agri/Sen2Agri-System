6240e0a8af24d43ee02e86d8d2a71216e43973be
Laurentiu Nicola <lnicola@dend.ro>
2017-04-10 11:03:36 +0300

    More season changes

--- database/05-functions/sp_dashboard_update_site.sql
+++ database/05-functions/sp_dashboard_update_site.sql
@@ -2,14 +2,9 @@ CREATE OR REPLACE FUNCTION sp_dashboard_update_site(
     _id smallint,
     _short_name character varying,
     _geog character varying,
-    _winter_season_start character varying,
-    _winter_season_end character varying,
-    _summer_season_start character varying,
-    _summer_season_end character varying,
     _enabled boolean)
   RETURNS void AS
 $BODY$
-DECLARE _parameters json[] := '{}';
 BEGIN
 
 IF NULLIF(_short_name, '') IS NOT NULL THEN
@@ -28,33 +23,6 @@ IF NULLIF(_geog, '') IS NOT NULL THEN
     UPDATE site
     SET geog = ST_Multi(ST_Force2D(ST_GeometryFromText(_geog)))
     WHERE id = _id;
-END IF;
-
-IF _winter_season_start IS NOT NULL THEN
-    _parameters := _parameters || json_build_object('key', 'downloader.winter-season.start',
-                                                    'site_id', _id,
-                                                    'value', NULLIF(to_char(nullif(_winter_season_start, '') :: date, 'MMddYYYY'), ''));
-END IF;
-IF _winter_season_end IS NOT NULL THEN
-    _parameters := _parameters || json_build_object('key', 'downloader.winter-season.end',
-                                                    'site_id', _id,
-                                                    'value', NULLIF(to_char(nullif(_winter_season_end, '') :: date, 'MMddYYYY'), ''));
-END IF;
-IF _summer_season_start IS NOT NULL THEN
-    _parameters := _parameters || json_build_object('key', 'downloader.summer-season.start',
-                                                    'site_id', _id,
-                                                    'value', NULLIF(to_char(nullif(_summer_season_start, '') :: date, 'MMddYYYY'), ''));
-END IF;
-IF _summer_season_end IS NOT NULL THEN
-    _parameters := _parameters || json_build_object('key', 'downloader.summer-season.end',
-                                                    'site_id', _id,
-                                                    'value', NULLIF(to_char(nullif(_summer_season_end, '') :: date, 'MMddYYYY'), ''));
-END IF;
-
-if array_length(_parameters, 1) > 0 THEN
-    PERFORM sp_upsert_parameters(
-                array_to_json(_parameters),
-                false);
 END IF;
 
 END;
--- database/05-functions/sp_get_sites.sql
+++ database/05-functions/sp_get_sites.sql
@@ -1,17 +1,15 @@
-﻿CREATE OR REPLACE FUNCTION sp_get_sites(IN _siteid smallint DEFAULT NULL::smallint)
-  RETURNS TABLE(id smallint, name character varying, short_name character varying) AS
+﻿CREATE OR REPLACE FUNCTION sp_get_sites(IN _site_id smallint DEFAULT NULL::smallint)
+  RETURNS TABLE(id smallint, name character varying, short_name character varying, enabled boolean) AS
 $BODY$
 BEGIN
    RETURN QUERY
-   
         SELECT site.id,
                site.name,
-               site.short_name
+               site.short_name,
+               site.enabled
         FROM site
-        WHERE($1 IS NULL OR site.id = $1)
+        WHERE _site_id IS NULL OR site.id = _site_id
         ORDER BY site.name;
-
- 
 END
 $BODY$
   LANGUAGE plpgsql;
