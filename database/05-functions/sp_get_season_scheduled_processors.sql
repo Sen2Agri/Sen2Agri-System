879e803d7364e89959bd9cccfb58922e61a20b73
Laurentiu Nicola <lnicola@dend.ro>
2017-04-11 16:30:11 +0300

    Return processor details from sp_get_season_scheduled_processors

--- database/05-functions/sp_get_season_scheduled_processors.sql
+++ database/05-functions/sp_get_season_scheduled_processors.sql
@@ -2,15 +2,23 @@ create or replace function sp_get_season_scheduled_processors(
     _season_id season.id%type
 )
 returns table (
-    processor_id processor.id%type
+    processor_id processor.id%type,
+    processor_name processor.name%type,
+    processor_short_name processor.short_name%type
 ) as
 $$
 begin
     return query
-        select distinct
-            scheduled_task.processor_id
-        from scheduled_task
-        where scheduled_task.season_id = _season_id;
+        select
+            processor.id,
+            processor.name,
+            processor.short_name
+        from processor
+        where exists(select *
+                     from scheduled_task
+                     where scheduled_task.season_id = _season_id
+                       and scheduled_task.processor_id = processor.id)
+        order by processor.short_name;
 end;
 $$
     language plpgsql stable;
