37c466011a2b761ffd6d8dc029ae3d1d93cd6bcd
Laurentiu Nicola <lnicola@dend.ro>
2017-05-24 18:20:48 +0300

    Add migration for 8dd70afcd17541ebe858845c7b2ee9356931ebd7

--- install_platform/migrations/migration-1.5-1.6.sql
+++ install_platform/migrations/migration-1.5-1.6.sql
@@ -1211,6 +1211,36 @@ begin
             raise notice '%', _statement;
             execute _statement;
 
+            raise notice 'applying 8dd70afcd17541ebe858845c7b2ee9356931ebd7';
+            _statement := $str$
+                CREATE OR REPLACE FUNCTION sp_get_job_history_custom_page(
+                    IN _siteid smallint DEFAULT NULL::smallint,
+                    IN _page integer DEFAULT 1,
+                    IN _rows_per_page integer DEFAULT 20)
+                RETURNS TABLE(id integer, end_timestamp timestamp with time zone, processor character varying, site character varying, status character varying, start_type character varying) AS
+                $BODY$
+                BEGIN
+                    RETURN QUERY
+                        SELECT J.id, J.end_timestamp, P.name, S.name, AST.name, ST.name
+                        FROM job J
+                            JOIN processor P ON J.processor_id = P.id
+                            JOIN site S ON J.site_id = S.id
+                            JOIN job_start_type ST ON J.start_type_id = ST.id
+                            JOIN activity_status AST ON J.status_id = AST.id
+                        WHERE   $1 IS NULL OR site_id = _siteid
+                    ORDER BY J.submit_timestamp DESC
+                    OFFSET ($2 - 1) * _rows_per_page LIMIT _rows_per_page;
+                END
+                $BODY$
+                LANGUAGE plpgsql STABLE
+                COST 100
+                ROWS 1000;
+                ALTER FUNCTION sp_get_job_history_custom_page(smallint, integer, integer)
+                OWNER TO admin;
+            $str$;
+            raise notice '%', _statement;
+            execute _statement;
+
             _statement := 'update meta set version = ''1.6'';';
             raise notice '%', _statement;
             execute _statement;
