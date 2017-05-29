1db9347de2110525eb1d0356af9e0f32cf823e7d
Laurentiu Nicola <lnicola@dend.ro>
2017-05-29 16:04:33 +0300

    Sort job output by step.submit_timestamp

--- database/05-functions/sp_get_job_output.sql
+++ database/05-functions/sp_get_job_output.sql
@@ -19,7 +19,8 @@ begin
         inner join step on step.task_id = task.id
         left outer join step_resource_log on step_resource_log.step_name = step.name and step_resource_log.task_id = task.id
         left outer join config on config.site_id is null and config.key = 'executor.module.path.' || task.module_short_name
-        where task.job_id = _job_id;
+        where task.job_id = _job_id
+        order by step.submit_timestamp;
 end;
 $$
     language plpgsql stable;
