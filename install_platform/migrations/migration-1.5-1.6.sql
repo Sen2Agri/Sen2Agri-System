66db90d03ca54e93fc176f582e66ef4925434c45
Laurentiu Nicola <lnicola@dend.ro>
2017-05-31 18:42:51 +0300

    Add migration for 4e10d7162e51bc0ebdf7788670ce0d579d076376

--- install_platform/migrations/migration-1.5-1.6.sql
+++ install_platform/migrations/migration-1.5-1.6.sql
@@ -1242,6 +1242,90 @@ begin
             raise notice '%', _statement;
             execute _statement;
 
+            raise notice 'applying 4e10d7162e51bc0ebdf7788670ce0d579d076376';
+            _statement := $str$
+                create or replace function sp_split_site(
+                    _site_id site.id%type
+                )
+                returns void
+                as
+                $$
+                declare _name site.name%type;
+                declare _short_name site.short_name%type;
+                declare _geog site.geog%type;
+                declare _enabled site.enabled%type;
+                declare _tile_id text;
+                declare _satellite_id satellite.id%type;
+                declare _new_site_id site.id%type;
+                begin
+                    select name,
+                        short_name,
+                        geog,
+                        enabled
+                    into _name,
+                        _short_name,
+                        _geog,
+                        _enabled
+                    from site
+                    where site.id = _site_id;
+
+                    if not found then
+                        raise exception 'Could not find site id %', _site_id;
+                    end if;
+
+                    for _tile_id,
+                        _satellite_id
+                    in
+                        select tile_id, 1
+                        from sp_get_site_tiles(_site_id, 1)
+                        union all
+                        select tile_id, 2
+                        from sp_get_site_tiles(_site_id, 2)
+                    loop
+                        insert into site(name,
+                                        short_name,
+                                        geog,
+                                        enabled)
+                        values (
+                            _name || '_' || _tile_id,
+                            _short_name || '_' || lower(_tile_id),
+                            _geog,
+                            _enabled
+                        )
+                        returning id
+                        into _new_site_id;
+
+                        insert into site_tiles(site_id,
+                                            satellite_id,
+                                            tiles)
+                        values (
+                            _new_site_id,
+                            _satellite_id,
+                            array[_tile_id]
+                        );
+
+                        insert into season(site_id,
+                                        name,
+                                        start_date,
+                                        end_date,
+                                        mid_date,
+                                        enabled)
+                        select _new_site_id,
+                            name,
+                            start_date,
+                            end_date,
+                            mid_date,
+                            enabled
+                        from season
+                        where season.site_id = _site_id;
+                    end loop;
+                end;
+                $$
+                    language plpgsql volatile;
+            $str$;
+            raise notice '%', _statement;
+            execute _statement;
+
             _statement := 'update meta set version = ''1.6'';';
             raise notice '%', _statement;
             execute _statement;
