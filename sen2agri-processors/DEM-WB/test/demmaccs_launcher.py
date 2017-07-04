937f305e9739385efc1a48ecf2be86f1c924b09a
Cosmin Udroiu <cosmin.udroiu@c-s.ro>
2017-07-04 09:56:54 +0300

    Update for the L2A substring in the new L8 format.

--- sen2agri-processors/DEM-WB/test/demmaccs_launcher.py
+++ sen2agri-processors/DEM-WB/test/demmaccs_launcher.py
@@ -119,7 +119,15 @@ def launch_demmaccs(l1c_context):
         elif l2a_basename.startswith("LC8"):
             l2a_basename += "_L2A"
         elif l2a_basename.startswith("LC08"):
-            l2a_basename = l2a_basename.replace("L1TP", "L2A")
+            if l2a_basename.find("_L1TP_") :
+                l2a_basename = l2a_basename.replace("_L1TP_", "_L2A_")
+            elif l2a_basename.find("_L1GS_") :
+                l2a_basename = l2a_basename.replace("_L1GS_", "_L2A_")
+            elif l2a_basename.find("_L1GT_") :
+                l2a_basename = l2a_basename.replace("_L1GT_", "_L2A_")
+            else:
+                log(general_log_path, "The L1C product name is bad - L2A cannot be filled: {}".format(l2a_basename), general_log_filename)
+                continue
         else:
             log(general_log_path, "The L1C product name is bad: {}".format(l2a_basename), general_log_filename)
             continue
