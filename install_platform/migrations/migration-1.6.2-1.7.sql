8f6cee0d25045b45817c1901a9fbc71c6796116f
Laurentiu Nicola <lnicola@dend.ro>
2017-10-02 17:28:03 +0300

    Add migration for 06122c0b0a8294b27f6b18fe899609b0118ff2b2

--- install_platform/migrations/migration-1.6.2-1.7.sql
+++ install_platform/migrations/migration-1.6.2-1.7.sql
@@ -9,11 +9,17 @@ begin
         if exists (select * from meta where version = '1.6.2') then
             raise notice 'upgrading from 1.6.2 to 1.7';
 
+            raise notice 'applying 06122c0b0a8294b27f6b18fe899609b0118ff2b2';
+            _statement := 'update config_metadata set type = ''string'' where key = ''processor.l4a.reference_data_dir'';';
+            raise notice '%', _statement;
+            execute _statement;
+
+            raise notice 'applying 00d4d2597c599f0d30cbc7c5b2a7662c45126db1';
             if not exists (select * from downloader_status where status_description = 'processing_failed') then
                 raise notice 'INSERT INTO downloader_status VALUES (6, ''processing_failed'');';
                 INSERT INTO downloader_status VALUES (6, 'processing_failed');
             end if;
-            
+
             _statement := 'update meta set version = ''1.7'';';
             raise notice '%', _statement;
             execute _statement;
