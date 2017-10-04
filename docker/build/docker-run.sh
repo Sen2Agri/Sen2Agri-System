57dcc6577c6781deb4bbf5f0fd15924c2cecb34c
Laurentiu Nicola <lnicola@dend.ro>
2017-10-04 13:02:38 +0300

    Remove build container afterwards

--- docker/build/docker-run.sh
+++ docker/build/docker-run.sh
@@ -7,7 +7,7 @@ docker build \
     --build-arg gid=$(id -g) \
     .
 
-docker run -it \
+docker run -it --rm \
        -v $(realpath ../..):/sen2agri \
        -u $(id -u):$(id -g) \
        sen2agri-build /bin/bash entry.sh
