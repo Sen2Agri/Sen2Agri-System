fca5de096b567d8b6e17afce3748736cca782411
Laurentiu Nicola <lnicola@dend.ro>
2017-10-04 11:38:17 +0300

    Merge Docker build and run scripts

--- docker/build/docker-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-docker build \
-    -t sen2agri-build \
-    --build-arg user=$USER \
-    --build-arg group=$(id -ng) \
-    --build-arg uid=$(id -u) \
-    --build-arg gid=$(id -g) \
-    .
