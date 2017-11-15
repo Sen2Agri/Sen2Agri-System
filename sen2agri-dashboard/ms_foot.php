4dec527d257a341ab045c8ae583bd5963274f199
Laurentiu Nicola <lnicola@dend.ro>
2017-11-15 15:20:00 +0200

    Adjust copyright year

--- sen2agri-dashboard/ms_foot.php
+++ sen2agri-dashboard/ms_foot.php
@@ -14,7 +14,7 @@ if (isset($_SESSION['siteId'])) {
     </div>
     <div id="footer" style="height: 20px;">
 		<div id="footer2" style="height: 20px; text-align: center; vertical-align: middle; padding: 0 0 0 0;">
-			<span>Copyright &copy; 2015 CS ROMANIA SA</span>
+			<span>Copyright &copy; 2015&ndash;2017 CS ROMANIA SA</span>
 			<div class="debug"></div>
 		</div>
     </div>
