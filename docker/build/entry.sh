4f2763ccbef0393ce72d038c0b197ec1f4b07eb2
Laurentiu Nicola <lnicola@dend.ro>
2017-11-22 16:24:13 +0200

    Improve docker build script

--- docker/build/entry.sh
+++ docker/build/entry.sh
@@ -8,20 +8,26 @@ rm -rf Sen2AgriPlatform \
 rm -rf Sen2AgriRPM/sen2agri-app-*.rpm \
        Sen2AgriRPM/sen2agri-downloaders-demmaccs-*.rpm \
        Sen2AgriRPM/sen2agri-processors-*.rpm \
-       Sen2AgriRPM/sen2agri-website-*.rpm \
+       Sen2AgriRPM/sen2agri-website-*.rpm
+
+mkdir -p Sen2AgriRPM
+if ls Sen2AgriPlatform/rpm_binaries/otb-*.rpm 2>&1 >/dev/null
+then
+    cp Sen2AgriPlatform/rpm_binaries/otb-*.rpm Sen2AgriRPM
+fi
 
 if ! ls Sen2AgriRPM/otb-*.rpm 2>&1 >/dev/null
 then
     ./Sen2AgriPlatformBuild.sh
 fi
 
+mv Sen2AgriPlatform/rpm_binaries/*.rpm Sen2AgriRPM
 sudo yum -y install Sen2AgriRPM/otb-*.rpm
 
 ./Sen2AgriProcessorsBuild.sh
 ./Sen2AgriAppBuild.sh
 ./Sen2AgriWebSiteBuild.sh
 
-mkdir -p Sen2AgriRPM
 mv Sen2AgriApp/rpm_binaries/*.rpm \
    Sen2AgriProcessors/rpm_binaries/*.rpm \
    Sen2AgriWebSite/rpm_binaries/*.rpm \
