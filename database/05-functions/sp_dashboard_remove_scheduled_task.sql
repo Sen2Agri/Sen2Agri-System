d54122c1bc9710619df1cb81bf038c89801ab1ac
Claudia Dabu EXT <cdabu_ext@thor-c3.si.c-s.fr>
2018-01-31 15:58:56 +0200

    function that remove a Scheduled job

---
+++ database/05-functions/sp_dashboard_remove_scheduled_task.sql
@@ -0,0 +1,13 @@
+﻿
+CREATE OR REPLACE FUNCTION sp_dashboard_remove_scheduled_task(_schedule_id smallint)
+  RETURNS void AS
+$BODY$
+BEGIN 
+
+DELETE FROM scheduled_task WHERE id = _schedule_id;
+
+DELETE FROM scheduled_task_status WHERE task_id = _schedule_id;
+
+END;
+$BODY$
+  LANGUAGE plpgsql
