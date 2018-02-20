dea15d24ee01ff269f727bd6df02254f3cd5f03f
Laurentiu Nicola <lnicola@dend.ro>
2018-02-20 15:56:50 +0200

    Disable BigTIFF, increase overview block size, enable int predictor, enable multi-threading

--- scripts/optimize_gtiff.py
+++ scripts/optimize_gtiff.py
@@ -8,11 +8,11 @@ import sys
 from osgeo import gdal
 
 
-def run_command(args):
+def run_command(args, env=None):
     args = map(str, args)
     cmd_line = " ".join(map(pipes.quote, args))
     print(cmd_line)
-    subprocess.call(args)
+    subprocess.call(args, env=env)
 
 
 def parse_args():
@@ -42,6 +42,7 @@ def get_image_size(image):
     ds = gdal.Open(image, gdal.gdalconst.GA_ReadOnly)
     return (ds.RasterXSize, ds.RasterYSize)
 
+
 def get_overview_levels(size, min_size):
     levels = []
     f = 1
@@ -51,32 +52,35 @@ def get_overview_levels(size, min_size):
         levels.append(f)
     return levels
 
+
 def main():
     args = parse_args()
     print(args)
 
     run_command(['gdaladdo', '-clean', args.input])
 
-    command = ['gdal_translate', '-co', 'BIGTIFF=YES', '-co', 'INTERLEAVE=' + args.interleave]
+    env = {'GDAL_NUM_THREADS': 'ALL_CPUS'}
+    command = ['gdal_translate', '-co', 'BIGTIFF=NO', '-co', 'INTERLEAVE=' + args.interleave]
     if args.overviews:
         command += ['-co', 'COPY_SRC_OVERVIEWS=YES']
 
         size = get_image_size(args.input)
         levels = get_overview_levels(max(size[0], size[1]), args.min_size)
         if levels:
-            run_command(['gdaladdo', '-r', args.resampler, args.input] + levels)
+            run_command(['gdaladdo', '-r', args.resampler, args.input] + levels, env)
 
     parts = os.path.splitext(args.input)
     temp = "".join([parts[0], ".tmpcog", parts[1]])
 
     if args.compress:
-        command += ['-co', 'COMPRESS=' + args.compress]
+        command += ['-co', 'COMPRESS=' + args.compress, '-co', 'PREDICTOR=2']
 
     if args.tiled:
         command += ['-co', 'TILED=YES', '-co', 'BLOCKXSIZE=' + str(args.block_size), '-co', 'BLOCKYSIZE=' + str(args.block_size)]
+        env['GDAL_TIFF_OVR_BLOCKSIZE'] = str(args.block_size)
 
     command += [args.input, temp]
-    run_command(command)
+    run_command(command, env)
 
     os.rename(temp, args.input)
 
