2982f793a8d380bb717be1577d8ba3f732856fe6
Claudia Dabu <claudia.dabu@c-s.ro>
2018-02-27 17:00:58 +0200

    Add a filter for tile selection -Products tab

---
+++ database/05-functions/sp_get_products_sites.sql
@@ -0,0 +1,21 @@
+﻿
+-- Function: sp_get_products_sites(smallint)
+
+-- DROP FUNCTION sp_get_products_sites(smallint);
+
+CREATE OR REPLACE FUNCTION sp_get_products_sites(IN _site_id smallint DEFAULT NULL::smallint)
+  RETURNS TABLE(id smallint, "name" character varying, short_name character varying, enabled boolean) AS
+$BODY$
+                BEGIN
+                RETURN QUERY
+                        SELECT DISTINCT (S.id),
+                            S.name,
+                            S.short_name,
+                            S.enabled
+                        FROM site S
+                        JOIN product P ON P.site_id = S.id
+                        WHERE _site_id IS NULL OR S.id = _site_id
+                        ORDER BY S.name;
+                END
+                $BODY$
+  LANGUAGE plpgsql VOLATILE
