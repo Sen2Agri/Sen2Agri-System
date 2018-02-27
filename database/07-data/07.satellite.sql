255f676afec2236697144ff5466e3117693b1845
Cosmin Udroiu <cosmin.udroiu@c-s.ro>
2018-02-27 14:01:42 +0200

    Removed (for now) the column short_name from satellite table.

--- database/07-data/07.satellite.sql
+++ database/07-data/07.satellite.sql
@@ -1,26 +1,20 @@
 INSERT INTO satellite(
 id,
-satellite_name, 
-short_name)
+satellite_name)
 VALUES (
 1,
-'sentinel2',
-'S2');
+'sentinel2');
 
 INSERT INTO satellite(
 id,
-satellite_name,
-short_name)
+satellite_name)
 VALUES (
 2,
-'landsat8',
-'L8');
+'landsat8');
 
 INSERT INTO satellite(
 id,
-satellite_name,
-short_name)
+satellite_name)
 VALUES (
 3,
-'sentinel1',
-'S1');
+'sentinel1');
