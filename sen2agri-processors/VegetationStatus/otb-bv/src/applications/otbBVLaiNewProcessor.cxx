6b8b768ba866ef99fd9a01813c522a8bf7ae40e5
Cosmin Udroiu <cosmin.udroiu@c-s.ro>
2018-03-06 13:55:09 +0200

    Added detailed angles for LAI INRA

--- sen2agri-processors/VegetationStatus/otb-bv/src/applications/otbBVLaiNewProcessor.cxx
+++ sen2agri-processors/VegetationStatus/otb-bv/src/applications/otbBVLaiNewProcessor.cxx
@@ -391,7 +391,7 @@ private:
         paramOut->Write();
     }
 
-    AnglesImageType::Pointer createAnglesBands(const std::unique_ptr<MetadataHelper> &pHelper) {
+    AnglesImageType::Pointer createMeanAnglesBands(const std::unique_ptr<MetadataHelper> &pHelper) {
         auto sz = m_defImgReader->GetOutput()->GetLargestPossibleRegion().GetSize();
         auto spacing = m_defImgReader->GetOutput()->GetSpacing();
         double quantifValue = pHelper->GetReflectanceQuantificationValue();
@@ -445,6 +445,67 @@ private:
         return retImg;
     }
 
+    AnglesImageType::Pointer createAnglesBands(const std::unique_ptr<MetadataHelper> &pHelper) {
+        auto sz = m_defImgReader->GetOutput()->GetLargestPossibleRegion().GetSize();
+        auto spacing = m_defImgReader->GetOutput()->GetSpacing();
+        //double quantifValue = pHelper->GetReflectanceQuantificationValue();
+        int width = sz[0];
+        int height = sz[1];
+
+        m_AnglesRaster = AnglesImageType::New();
+
+        AnglesImageType::IndexType start;
+        start[0] =   0;  // first index on X
+        start[1] =   0;  // first index on Y
+
+        AnglesImageType::SizeType size;
+        size[0]  = ANGLES_GRID_SIZE;  // size along X
+        size[1]  = ANGLES_GRID_SIZE;  // size along Y
+
+        AnglesImageType::RegionType region;
+        region.SetSize(size);
+        region.SetIndex(start);
+
+        m_AnglesRaster->SetRegions(region);
+        m_AnglesRaster->SetNumberOfComponentsPerPixel(3);
+
+        AnglesImageType::SpacingType anglesRasterSpacing;
+        anglesRasterSpacing[0] = (((float)width) * spacing[0]) / ANGLES_GRID_SIZE; // spacing along X
+        anglesRasterSpacing[1] = (((float)height) * spacing[1]) / ANGLES_GRID_SIZE; // spacing along Y
+        m_AnglesRaster->SetSpacing(anglesRasterSpacing);
+
+        m_AnglesRaster->Allocate();
+        const MetadataHelperAngles &solarAngles = pHelper->GetDetailedSolarAngles();
+        const std::vector<MetadataHelperViewingAnglesGrid> &viewingAngles = pHelper->GetAllDetectorsDetailedViewingAngles();
+        const std::vector<std::vector<double>> &sensorZenithMeanMatrix = getMeanViewingAngles(viewingAngles, true);
+        const std::vector<std::vector<double>> &sensorZenithCosMatrix = computeCosValues(sensorZenithMeanMatrix);
+        const std::vector<std::vector<double>> &sensorAzimuthMeanMatrix = getMeanViewingAngles(viewingAngles, false);
+        const std::vector<std::vector<double>> &sunZenithCosMatrix = computeCosValues(solarAngles.Zenith.Values);
+        const std::vector<std::vector<double>> &relAzimuthMatrix = computeRelativeAzimuth(solarAngles.Azimuth.Values, sensorAzimuthMeanMatrix);
+        for(unsigned int i = 0; i < ANGLES_GRID_SIZE; i++) {
+            const std::vector<double> &curSensorZenithLine = sensorZenithCosMatrix[i];
+            const std::vector<double> &curSunZenithCosLine = sunZenithCosMatrix[i];
+            const std::vector<double> &curRelAzimuthLine = relAzimuthMatrix[i];
+            for(unsigned int j = 0; j < ANGLES_GRID_SIZE; j++) {
+                itk::VariableLengthVector<float> vct(3);
+                vct[0] = curSensorZenithLine[j];
+                vct[1] = curSunZenithCosLine[j];
+                vct[2] = curRelAzimuthLine[j];
+
+                AnglesImageType::IndexType idx;
+                idx[0] = j;
+                idx[1] = i;
+                m_AnglesRaster->SetPixel(idx, vct);
+            }
+        }
+        m_AnglesRaster->UpdateOutputInformation();
+        AnglesImageType::Pointer retImg = m_AnglesResampler.getResampler(m_AnglesRaster, width, height)->GetOutput();
+        retImg->UpdateOutputInformation();
+
+        return retImg;
+    }
+
+
     OutImageType::Pointer MaskModelOutput(OutImageType::Pointer img, MaskedOutputFilterType::Pointer maskingFunctor,
                                           const std::unique_ptr<MetadataHelper> &pHelper) {
         MetadataHelper::SingleBandShortImageType::Pointer imgMsk = pHelper->GetMasksImage(ALL, false);
@@ -480,6 +541,146 @@ private:
         std::cout << std::endl;
         std::cout << "=================================" << std::endl;
     }
+
+    double getMeanSolarAngles(const MetadataHelperAngles &solarAngles) {
+        return 0.0;
+    }
+
+    std::vector<std::vector<double>> getMeanViewingAngles(const std::vector<MetadataHelperViewingAnglesGrid> &viewingAngles,
+                                                          bool useZenith) {
+        std::vector<std::vector<double>> sumsMatrix;
+        std::vector<std::vector<int>> cntNotNanMatrix;
+        for (const MetadataHelperViewingAnglesGrid &grid : viewingAngles) {
+            const MetadataHelperAngleList &anglesList = useZenith ? grid.Angles.Zenith : grid.Angles.Azimuth;
+            for (size_t i = 0; i<anglesList.Values.size(); i++) {
+                const std::vector<double> &valuesLine = anglesList.Values[i];
+                if (sumsMatrix.size() == i) {
+                    expandMatrices(valuesLine, sumsMatrix, cntNotNanMatrix);
+                } else {
+                    updateMatrices(valuesLine, i, sumsMatrix, cntNotNanMatrix);
+                }
+            }
+        }
+        return getMatrixMeanValues(sumsMatrix, cntNotNanMatrix);
+    }
+
+    void expandMatrices(const std::vector<double> &valuesLine, std::vector<std::vector<double>> &sumsMatrix,
+                        std::vector<std::vector<int>> &cntNotNanMatrix) {
+        std::vector<double> sumsVect;
+        std::vector<int> cntNotNanVect;
+        for (size_t i = 0; i<valuesLine.size(); i++) {
+            if (std::isnan(valuesLine[i])) {
+                sumsVect.push_back(0.0);
+                cntNotNanVect.push_back(0);
+            } else {
+                sumsVect.push_back(valuesLine[i]);
+                cntNotNanVect.push_back(1);
+            }
+        }
+        sumsMatrix.push_back(sumsVect);
+        cntNotNanMatrix.push_back(cntNotNanVect);
+    }
+
+    void updateMatrices(const std::vector<double> &valuesLine, int lineIdxInMatrix, std::vector<std::vector<double>> &sumsMatrix,
+                        std::vector<std::vector<int>> &cntNotNanMatrix) {
+        std::vector<double> &sumsVect = sumsMatrix[lineIdxInMatrix];
+        std::vector<int> &cntNotNanVect = cntNotNanMatrix[lineIdxInMatrix];
+
+        // Normally, the size of the values line and the sums and notNan vectors are the same
+        // but if they are not, then expand the sums vector and the
+        while (sumsVect.size() < valuesLine.size()) {
+            sumsVect.push_back(0.0);
+            cntNotNanVect.push_back(0);
+        }
+        // now update the lines
+        for (size_t i = 0; i<valuesLine.size(); i++) {
+            if (!std::isnan(valuesLine[i])) {
+                sumsVect[i] += valuesLine[i];
+                cntNotNanVect[i] += 1;
+            }
+        }
+    }
+
+    std::vector<std::vector<double>> getMatrixMeanValues(const std::vector<std::vector<double>> &sumsMatrix,
+                                                         const std::vector<std::vector<int>> &nonNanMatrix) {
+        //printMatrix(sumsMatrix);
+        //printMatrix(nonNanMatrix);
+
+        std::vector<std::vector<double>> retMeanValuesMatrix;
+        for (size_t i = 0; i < sumsMatrix.size(); i++) {
+            const std::vector<double> &sumsVect = sumsMatrix[i];
+            const std::vector<int> &nonNansVect = nonNanMatrix[i];
+            std::vector<double> meanValsVect;
+            for (size_t j = 0; j<sumsVect.size(); j++) {
+                meanValsVect.push_back((nonNansVect[j] != 0) ?
+                                           (sumsVect[j]/nonNansVect[j]) :
+                                           std::numeric_limits<double>::quiet_NaN());
+             }
+            retMeanValuesMatrix.push_back(meanValsVect);
+        }
+
+        //printMatrix(retMeanValuesMatrix);
+        return retMeanValuesMatrix;
+    }
+
+    std::vector<std::vector<double>> computeCosValues(const std::vector<std::vector<double>> &meanValuesMatrix) {
+        std::vector<std::vector<double>> retCosValuesMatrix;
+        for (const std::vector<double> &meanValsVect : meanValuesMatrix) {
+            std::vector<double> cosValsVect;
+            for (double val : meanValsVect) {
+                if (std::isnan(val)) {
+                    cosValsVect.push_back(0);
+                } else {
+                    cosValsVect.push_back(10000 * cos((val * M_PI ) / 180));
+                }
+             }
+            retCosValuesMatrix.push_back(cosValsVect);
+        }
+        //printMatrix(retCosValuesMatrix);
+        return retCosValuesMatrix;
+    }
+
+    std::vector<std::vector<double>> computeRelativeAzimuth(const std::vector<std::vector<double>> &solarAzimuthValuesMatrix,
+                                                            const std::vector<std::vector<double>> &sensorAzimuthValuesMatrix) {
+        std::vector<std::vector<double>> retRelAzimuthValuesMatrix;
+        for (size_t i = 0; i < solarAzimuthValuesMatrix.size(); i++) {
+            const std::vector<double> &solarAzimuthVect = solarAzimuthValuesMatrix[i];
+            const std::vector<double> &sensorAzimuthVect = sensorAzimuthValuesMatrix[i];
+            std::vector<double> relAzimuthValsVect;
+            for (size_t j = 0; j<solarAzimuthVect.size(); j++) {
+                if (std::isnan(solarAzimuthVect[j]) || std::isnan(sensorAzimuthVect[j])) {
+                    relAzimuthValsVect.push_back(0);
+                } else {
+                    double val = solarAzimuthVect[j] - sensorAzimuthVect[j];
+                    relAzimuthValsVect.push_back(10000 * cos((val * M_PI ) / 180));
+                }
+             }
+            retRelAzimuthValuesMatrix.push_back(relAzimuthValsVect);
+        }
+
+        return retRelAzimuthValuesMatrix;
+    }
+
+    template<typename T>
+    void printMatrix(const std::vector<std::vector<T>> &matrix) {
+        std::cout << "[";
+        for (const std::vector<T> &curLine : matrix) {
+            std::cout << "[";
+            int items = 0;
+            for (T val : curLine) {
+                if (items == 5) {
+                    std::cout << std::endl;
+                    items = 0;
+                }
+                std::cout << " " << val << std::setprecision(7) << " ";
+                items++;
+             }
+            std::cout << "]" << std::endl;
+        }
+        std::cout << "]" << std::endl;
+    }
+
+
 
     ImageListToVectorImageFilterType::Pointer m_ftsConcat;
 
