47b8105520c2151a40e2664e622b9f65a1217df4
alex <alex@c-s.ro>
2018-11-27 17:00:56 +0200

    update gipp files for maja

--- install_platform/update_gipp.sh
+++ install_platform/update_gipp.sh
@@ -11,7 +11,30 @@ function install_gipp_files()
         mv "${GIPP_PATH}" "${GIPP_PATH}_OLD_${DATE}"
         mkdir -p "${GIPP_PATH}"
         echo "Copying new GIPP files to ${GIPP_PATH} ..."
-        cp -fR ../gipp/* "${GIPP_PATH}"
+	while [[ $answer != '1' ]] && [[ $answer != '2' ]]
+	do	
+	    read -n1 -p "The new GIPP files should be copied for MACCS or for MAJA? (1 for MACCS / 2 for MAJA): " -r answer
+	    printf "\n"
+	    case $answer in
+		1)
+		    echo "MACCS GIPP files will be copied"
+		    l1c_processor=1
+		    ;;
+		2)
+		    echo "MAJA GIPP files will be copied"
+		    l1c_processor=2
+		    ;;
+		*)
+		    echo "Unknown answer"
+		    ;;
+	    esac
+	done
+	if [ $l1c_processor -eq 1 ]; then	
+            cp -fR ../gipp/* "${GIPP_PATH}"
+	elif [ $l1c_processor -eq 2 ]; then
+	    cp -fR ../gipp_maja/* "${GIPP_PATH}"
+	fi	
+	    
         echo "Done!"
     fi
 }
