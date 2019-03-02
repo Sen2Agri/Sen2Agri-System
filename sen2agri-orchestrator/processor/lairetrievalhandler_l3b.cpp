98e9e4f092736e970bea8cf0d71d4df73bb507b8
Cosmin Udroiu <cosmin.udroiu@c-s.ro>
2019-03-02 13:16:38 +0200

    Removal of unneeded line

--- sen2agri-orchestrator/processor/lairetrievalhandler_l3b.cpp
+++ sen2agri-orchestrator/processor/lairetrievalhandler_l3b.cpp
@@ -576,7 +576,6 @@ QStringList LaiRetrievalHandlerL3B::GetBvErrImageInvArgs(const QString &ftsFile,
 
 QStringList LaiRetrievalHandlerL3B::GetQuantifyImageArgs(const std::map<QString, QString> &,
                                                          const QString &inFileName, const QString &outFileName)  {
-    (void)configParams;
     return { "QuantifyImage",
         "-in", inFileName,
         "-out", outFileName
