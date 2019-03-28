0c82d6f9246961936e425a693435a1b6a45240fd
Laurențiu Nicola <lnicola@dend.ro>
2019-03-28 18:34:11 +0200

    Add strata support to extract-parcels.py

--- scripts/extract-parcels.py
+++ scripts/extract-parcels.py
@@ -4,9 +4,12 @@ from __future__ import print_function
 import argparse
 import csv
 from datetime import date
+import os.path
+from osgeo import gdal, ogr
 import psycopg2
 from psycopg2.sql import SQL, Literal, Identifier
 import psycopg2.extras
+import sys
 try:
     from configparser import ConfigParser
 except ImportError:
@@ -46,6 +49,76 @@ def get_site_name(conn, site_id):
         return rows[0][0]
 
 
+def save_to_csv(cursor, path):
+    with open(path, 'wb') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+
+        writer.writerow(['NewID', 'AREA', 'CTnum', 'LC'])
+        for row in cursor:
+            writer.writerow(row)
+
+
+def extract_parcels(config, args, lpis_table, lut_table, id, geom):
+    with psycopg2.connect(host=config.host, port=config.port, dbname=config.dbname, user=config.user, password=config.password) as conn:
+        q1 = """
+            select lpis."NewID",
+                lpis."Area_meters" as "AREA",
+                lut."ctnuml4a" as "CTnum",
+                lpis."LC"
+            from {} lpis
+            inner join {} lut on lut.ctnum :: int = lpis."CTnum"
+            where lpis."LC" = any({})
+            and lpis."S1Pix" >= {}
+            and lpis."S2Pix" >= {}
+            and "GeomValid"
+            and not "Duplic"
+            and not "Overlap"
+            order by "NewID"
+            """
+        q2 = """
+            with stratum as (
+                select ST_Transform(ST_GeomFromText({}, {}), Find_SRID('public', {}, 'wkb_geometry')) as geom
+            )
+            select lpis."NewID",
+                lpis."Area_meters" as "AREA",
+                lut."ctnuml4a" as "CTnum",
+                lpis."LC"
+            from stratum
+            inner join {} lpis on lpis.wkb_geometry && stratum.geom and ST_Relate(lpis.wkb_geometry, stratum.geom, '2********')
+            inner join {} lut on lut.ctnum :: int = lpis."CTnum"
+            where lpis."LC" = any({})
+            and lpis."S1Pix" >= {}
+            and lpis."S2Pix" >= {}
+            and "GeomValid"
+            and not "Duplic"
+            and not "Overlap"
+            order by "NewID"
+            """
+
+        with conn.cursor() as cursor:
+            if args.strata is None:
+                output = args.output
+                query = SQL(q1).format(Identifier(lpis_table), Identifier(lut_table), Literal(args.lc), Literal(args.min_s1_pix), Literal(args.min_s2_pix))
+                print(query.as_string(conn))
+
+                cursor.execute(query)
+                save_to_csv(cursor, output)
+                conn.commit()
+            else:
+                dirname = os.path.dirname(args.output)
+                basename = os.path.basename(args.output)
+                split = os.path.splitext(basename)
+
+                query = SQL(q2).format(Literal(geom.ExportToWkt()), Literal(args.srid), Literal(lpis_table), Identifier(lpis_table), Identifier(lut_table), Literal(args.lc), Literal(args.min_s1_pix), Literal(args.min_s2_pix))
+
+                print(query.as_string(conn))
+
+                cursor.execute(query)
+                output = os.path.join(dirname, "{}-{}{}".format(split[0], id, split[1]))
+                save_to_csv(cursor, output)
+                conn.commit()
+
+
 def main():
     parser = argparse.ArgumentParser(description="Crops and recompresses S1 L2A products")
     parser.add_argument('-c', '--config-file', default='/etc/sen2agri/sen2agri.conf', help="configuration file location")
@@ -54,11 +127,18 @@ def main():
     parser.add_argument('--lc', help="LC values", nargs='+', type=int, default=[1, 2, 3, 4])
     parser.add_argument('--min-s1-pix', help="minimum number of S1 pixels", type=int, default=1)
     parser.add_argument('--min-s2-pix', help="minimum number of S2 pixels", type=int, default=3)
+    parser.add_argument('--strata', help="strata definition")
+    parser.add_argument('--srid', help="strata SRID")
     parser.add_argument('output', help="output file", default="parcels.csv")
 
     args = parser.parse_args()
 
     config = Config(args)
+
+    if args.strata is not None:
+        if args.srid is None:
+            print("--srid is required with --strata")
+            sys.exit(1)
 
     with psycopg2.connect(host=config.host, port=config.port, dbname=config.dbname, user=config.user, password=config.password) as conn:
         site_name = get_site_name(conn, config.site_id)
@@ -66,35 +146,16 @@ def main():
         lpis_table = "decl_{}_{}".format(site_name, year)
         lut_table = "lut_{}_{}".format(site_name, year)
 
-        with conn.cursor() as cursor:
-            query = SQL(
-                """
-                select lpis."NewID",
-                    lpis."Area_meters" as "AREA",
-                    lut."ctnuml4a" as "CTnum",
-                    lpis."LC"
-                from {} lpis
-                inner join {} lut on lut.ctnum :: int = lpis."CTnum"
-                where lpis."LC" = any({})
-                and lpis."S1Pix" >= {}
-                and lpis."S2Pix" >= {}
-                and "GeomValid"
-                and not "Duplic"
-                and not "Overlap"
-                order by "NewID"
-                """)
-            query = query.format(Identifier(lpis_table), Identifier(lut_table), Literal(args.lc), Literal(args.min_s1_pix), Literal(args.min_s2_pix))
-            print(query.as_string(conn))
-
-            with open(args.output, 'wb') as csvfile:
-                writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
-
-                cursor.execute(query)
-                writer.writerow(['NewID', 'AREA', 'CTnum', 'LC'])
-                for row in cursor:
-                    writer.writerow(row)
-
-            conn.commit()
+    if args.strata is None:
+        extract_parcels(config, args, lpis_table, lut_table, None, None)
+    else:
+        ds = ogr.Open(args.strata, gdal.gdalconst.GA_ReadOnly)
+        layer = ds.GetLayer()
+        for feature in layer:
+            id = feature.GetField("id")
+            geom = feature.GetGeometryRef()
+            if id == 2:
+                extract_parcels(config, args, lpis_table, lut_table, id, geom)
 
 
 if __name__ == "__main__":
