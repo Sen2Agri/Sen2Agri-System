f03d714c9bc02de7f19e17933825cff7fc0b9ab0
Laurențiu Nicola <lnicola@dend.ro>
2019-06-12 15:22:35 +0300

    Add product details tables

---
+++ database/03-tables/sen4cap/product_details_l4a.sql
@@ -0,0 +1,10 @@
+create table product_details_l4a(
+    product_id int not null references product(id),
+    "NewID" int not null,
+    "CT_decl" int,
+    "CT_pred_1" int,
+    "CT_conf_1" real,
+    "CT_pred_2" int,
+    "CT_conf_2" real,
+    constraint product_details_l4a_pkey primary key(product_id, "NewID")
+);
