b7871af506e81bd8ab752825b29ac9e5f2764ce4
Laurențiu Nicola <lnicola@dend.ro>
2020-03-23 18:05:45 +0200

    Remove unused routes from sen2agri-http-listener

--- sen2agri-http-listener/controller/dashboardcontroller.hpp
+++ sen2agri-http-listener/controller/dashboardcontroller.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #include <httpserver/httprequest.h>
-#include <httpserver/httpresponse.h>
 #include <httpserver/httprequesthandler.h>
+#include <httpserver/httpresponse.h>
 
 #include "persistencemanager.hpp"
 
@@ -12,22 +12,6 @@ class DashboardController : public HttpRequestHandler
     Q_DISABLE_COPY(DashboardController)
 
     PersistenceManagerDBProvider &persistenceManager;
-
-    void getDashboardCurrentJobData(const HttpRequest &request, HttpResponse &response);
-    void getDashboardServerResourceData(const HttpRequest &request, HttpResponse &response);
-    void getDashboardProcessorStatistics(const HttpRequest &request, HttpResponse &response);
-    void getDashboardProductAvailability(const HttpRequest &request, HttpResponse &response);
-    void getDashboardJobTimeline(const HttpRequest &request, HttpResponse &response);
-
-    void getDashboardProducts(const HttpRequest &request, HttpResponse &response);
-    void getDashboardSites(const HttpRequest &request, HttpResponse &response);
-    void getDashboardSentinelTiles(const HttpRequest &request, HttpResponse &response);
-    void getDashboardLandsatTiles(const HttpRequest &request, HttpResponse &response);
-    void getDashboardProcessors(const HttpRequest &request, HttpResponse &response);
-
-    void cancelJob(const HttpRequest &request, HttpResponse &response);
-    void pauseJob(const HttpRequest &request, HttpResponse &response);
-    void resumeJob(const HttpRequest &request, HttpResponse &response);
 
     void notifyOrchestrator();
 
