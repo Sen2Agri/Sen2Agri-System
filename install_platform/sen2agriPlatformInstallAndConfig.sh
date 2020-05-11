60fcc92081476adf0665dd9bad4cb4314e6fa720
Laurențiu Nicola <lnicola@dend.ro>
2020-05-11 13:06:22 +0300

    Don't error out if MACCS or MAJA are not installed

--- install_platform/sen2agriPlatformInstallAndConfig.sh
+++ install_platform/sen2agriPlatformInstallAndConfig.sh
@@ -721,7 +721,10 @@ function find_l1c_processor()
         fi
     else
         echo "Unable to find ${l1c_processor_name} under ${l1c_processor_path}"
-        return 1
+        echo "You might want to cancel the setup and install it, or you can use Sen2Cor products if available."
+        echo "Hit Ctrl-C now to cancel."
+        sleep 20
+        return 0
     fi
 }
 
