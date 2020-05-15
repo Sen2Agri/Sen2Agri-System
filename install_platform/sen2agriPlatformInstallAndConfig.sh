2eebdff2e14d3e17123e5135359aa886f97dd6f8
Laurențiu Nicola <lnicola@dend.ro>
2020-05-15 14:00:58 +0300

    Install yum-utils

--- install_platform/sen2agriPlatformInstallAndConfig.sh
+++ install_platform/sen2agriPlatformInstallAndConfig.sh
@@ -914,7 +914,7 @@ disable_selinux
 disable_firewall
 
 ##install EPEL for dependencies, PGDG for the Postgres client libraries and
-yum -y install epel-release https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+yum -y install epel-release https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm yum-utils
 yum -y update epel-release pgdg-redhat-repo
 yum-config-manager --disable pgdg95
 yum -y install docker docker-compose
