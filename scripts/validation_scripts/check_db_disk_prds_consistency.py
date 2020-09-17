fdbf750140b114bf2a98f7721c6ac238151d2849
Cosmin Udroiu <cosmin.udroiu@c-s.ro>
2020-09-17 14:54:48 +0300

    Added check for db-disk consistency for L1C products.

--- scripts/validation_scripts/check_db_disk_prds_consistency.py
+++ scripts/validation_scripts/check_db_disk_prds_consistency.py
@@ -47,6 +47,7 @@ class Config(object):
         self.site_id = args.site_id
         self.start_date = args.start_date
         self.end_date = args.stop_date
+        self.dwn_hist_check = args.dwn_hist_check
             
         print ("Start date is {}".format(self.start_date))
         print ("Stop date is {}".format(self.end_date))
@@ -79,6 +80,27 @@ def check_products_from_db(config, conn):
             if (not os.path.isdir(prdFullPath)) and (not os.path.isfile(prdFullPath)) : 
                 print("Product {} does not exist on disk!".format(prdFullPath))        
 
+def check_dwn_history_prds(config, conn):
+    with conn.cursor() as cursor:
+        query = SQL(
+            """
+            select full_path, status_id from downloader_history
+            where site_id = {} and product_date between {} and {}
+            order by product_date asc;
+            """
+        )
+        query = query.format(Literal(config.site_id), Literal(config.start_date), Literal(config.end_date))
+        print(query.as_string(conn))
+        cursor.execute(query)
+
+        results = cursor.fetchall()
+        conn.commit()
+
+        for (full_path,status_id) in results:
+            prdFullPath = full_path
+            if ((not os.path.isdir(prdFullPath))) and (not os.path.isfile(prdFullPath)) : 
+                print("Product {} with status id = {} does not exist on disk!".format(prdFullPath, status_id))        
+
 def main():
 
     parser = argparse.ArgumentParser(description='Check if the products in the DB for a site exist on disk')
@@ -88,6 +110,7 @@ def main():
     
     parser.add_argument('-b', '--start_date', help='start-date: yyyy-mm-dd', default=(datetime.datetime.now() - relativedelta(years=5)).strftime('%Y-%m-%d'))
     parser.add_argument('-f', '--stop_date', help='stop_date: yyyy-mm-dd', default=datetime.datetime.now().strftime( "%Y-%m-%d"))
+    parser.add_argument('-d', '--dwn-hist-check', default=0, type=int, help='Downloader history products check. 0 - do not check, 1 - check')
 
     args = parser.parse_args()
     
@@ -99,6 +122,8 @@ def main():
 
     with psycopg2.connect(host=config.host, dbname=config.dbname, user=config.user, password=config.password) as conn:
         check_products_from_db(config, conn)
+        if config.dwn_hist_check == 1 :
+            check_dwn_history_prds(config, conn)
                 
 ###################################################################################################
 if __name__ == '__main__':
