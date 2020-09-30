cf1debd1599e1c5b09533e8b47cbba96a6a6185e
Laurențiu Nicola <lnicola@dend.ro>
2020-09-30 17:00:17 +0300

    Make max retry count configurable, check if site and satellite are enabled, add Sen2Cor support to sp_start_l1_tile_processing

--- database/05-functions/sp_start_l1_tile_processing.sql
+++ database/05-functions/sp_start_l1_tile_processing.sql
@@ -22,6 +22,40 @@ begin
         raise exception 'Please set the transaction isolation level to serializable.' using errcode = 'UE001';
     end if;
 
+    create temporary table if not exists site_config(
+        key,
+        site_id,
+        value
+    ) as
+    select
+        keys.key,
+        site.id,
+        config.value
+    from site
+    cross join (
+        values
+            ('processor.l2a.s2.implementation'),
+            ('processor.l2a.optical.retry-interval'),
+            ('processor.l2a.optical.max-retries'),
+            ('processor.l2a.optical.num-workers'),
+            ('s2.enabled'),
+            ('l8.enabled')
+    ) as keys(key)
+    cross join lateral (
+        select
+            coalesce((
+                select value
+                from config
+                where key = keys.key
+                and config.site_id = site.id
+            ), (
+                select value
+                from config
+                where key = keys.key
+                and config.site_id is null
+            )) as value
+    ) config;
+
     select l1_tile_history.satellite_id,
            l1_tile_history.orbit_id,
            l1_tile_history.tile_id,
@@ -31,10 +65,60 @@ begin
          _tile_id,
          _downloader_history_id
     from l1_tile_history
-    where status_id = 2 -- failed
-      and retry_count < 3
-      and status_timestamp < now() - interval '1 day'
-    order by status_timestamp
+    inner join downloader_history on downloader_history.id = l1_tile_history.downloader_history_id
+    inner join site on site.id = downloader_history.site_id
+    cross join lateral (
+        select
+            (
+                select value :: int as max_retries
+                from site_config
+                where site_config.site_id = downloader_history.site_id
+                  and key = 'processor.l2a.optical.max-retries'
+            ),
+            (
+                select value :: interval as retry_interval
+                from site_config
+                where site_config.site_id = downloader_history.site_id
+                  and key = 'processor.l2a.optical.retry-interval'
+            ),
+            (
+                select value :: boolean as s2_enabled
+                from site_config
+                where site_config.site_id = downloader_history.site_id
+                  and key = 's2.enabled'
+            ),
+            (
+                select value :: boolean as l8_enabled
+                from site_config
+                where site_config.site_id = downloader_history.site_id
+                  and key = 'l8.enabled'
+            )
+    ) config
+    where l1_tile_history.status_id = 2 -- failed
+      and l1_tile_history.retry_count < config.max_retries
+      and l1_tile_history.status_timestamp < now() - config.retry_interval
+      and case downloader_history.satellite_id
+              when 1 then config.s2_enabled
+              when 2 then config.l8_enabled
+              else false
+      end
+      and (
+          site.enabled
+          or exists (
+              select *
+              from downloader_history
+              where downloader_history.status_id = 2 -- downloaded
+                and l1_tile_history.tile_id = any(downloader_history.tiles)
+                and l1_tile_history.orbit_id = downloader_history.orbit_id
+                and exists (
+                    select *
+                    from site
+                    where site.id = downloader_history.site_id
+                      and site.enabled
+                )
+          )
+      )
+    order by l1_tile_history.status_timestamp
     limit 1;
 
     if found then
@@ -68,26 +152,67 @@ begin
             _path,
             _site_id
         from downloader_history
+        inner join site on site.id = downloader_history.site_id
         cross join lateral (
                 select unnest(tiles) as tile_id
             ) tile_ids
-        inner join site on site.id = downloader_history.site_id
-        where not exists (
+        cross join lateral (
+            select
+                (
+                    select value as l2a_implementation
+                    from site_config
+                    where site_config.site_id = downloader_history.site_id
+                    and key = 'processor.l2a.s2.implementation'
+                ),
+                (
+                    select value :: int as max_retries
+                    from site_config
+                    where site_config.site_id = downloader_history.site_id
+                    and key = 'processor.l2a.optical.max-retries'
+                ),
+                (
+                    select value :: boolean as s2_enabled
+                    from site_config
+                    where site_config.site_id = downloader_history.site_id
+                    and key = 's2.enabled'
+                ),
+                (
+                    select value :: boolean as l8_enabled
+                    from site_config
+                    where site_config.site_id = downloader_history.site_id
+                    and key = 'l8.enabled'
+                )
+        ) config
+        where (
+            config.l2a_implementation = 'sen2cor'
+            and downloader_history.satellite_id = 1
+            or not exists (
+                select *
+                from l1_tile_history
+                where (l1_tile_history.satellite_id,
+                    l1_tile_history.orbit_id,
+                    l1_tile_history.tile_id) =
+                    (downloader_history.satellite_id,
+                    downloader_history.orbit_id,
+                    tile_ids.tile_id)
+                and (status_id = 1 or -- processing
+                    retry_count < config.max_retries and status_id = 2 -- failed
+                )
+            )
+        )
+        and not exists (
             select *
             from l1_tile_history
-            where (l1_tile_history.satellite_id,
-                   l1_tile_history.orbit_id,
-                   l1_tile_history.tile_id) =
-                  (downloader_history.satellite_id,
-                   downloader_history.orbit_id,
-                   tile_ids.tile_id)
-              and (status_id = 1 or -- processing
-                   retry_count < 3 and status_id = 2 -- failed
-              )
-              or (l1_tile_history.downloader_history_id, l1_tile_history.tile_id) = (downloader_history.id, tile_ids.tile_id)
-        ) and downloader_history.status_id in (2, 7) -- downloaded, processing
+            where (l1_tile_history.downloader_history_id, l1_tile_history.tile_id) = (downloader_history.id, tile_ids.tile_id)
+        )
+        and downloader_history.status_id in (2, 7) -- downloaded, processing
         and site.enabled
         and downloader_history.satellite_id in (1, 2) -- sentinel2, landsat8
+        and case downloader_history.satellite_id
+                when 1 then config.s2_enabled
+                when 2 then config.l8_enabled
+                else false
+        end
         order by satellite_id,
                 orbit_id,
                 tile_id,
